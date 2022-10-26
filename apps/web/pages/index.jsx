a19a1b1cac7af836d42f21741cc5c7e4890c9742
Kar <ISoNice4572@gmail.com>
2022-10-26 17:30:46 +0800

    chore: Testing pre-commit hooks

---
+++ apps/web/pages/index.jsx
@@ -0,0 +1,10 @@
+import { Button } from "ui";
+
+export default function Web() {
+  return (
+    <div>
+                  <h1>Web12345</h1>
+                  <Button />
+                </div>
+  );
+}
