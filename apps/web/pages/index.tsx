6fb00e5a3487d360c8feafb19d1f3d09b39a9046
Kar <ISoNice4572@gmail.com>
2022-10-26 17:26:13 +0800

    build: :wrench: Fixed some packages installation issues

--- apps/web/pages/index.tsx
+++ apps/web/pages/index.tsx
@@ -1,10 +1,10 @@
-import { Button } from "ui";
+import { Button } from 'ui';
 
 export default function Web() {
   return (
     <div>
-      <h1>Web</h1>
-      <Button />
+              <h1>Web123</h1>
+              <Button />
     </div>
   );
 }
