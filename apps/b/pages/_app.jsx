6f612c5b428410b26a754eed56606e5b7f59f4b1
Acrylic125 <acrylic125email@gmail.com>
2022-10-27 18:09:43 +0800

    chore: :sparkles: team b setup repo

--- apps/b/pages/_app.jsx
+++ apps/b/pages/_app.jsx
@@ -5,7 +5,9 @@ import '../styles/globals.css';
 const MyApp = ({ Component, pageProps }) => <Component {...pageProps} />;
 
 MyApp.propTypes = {
-  pageProps: PropTypes.shape({}),
+  // If getServerSideProps is used, we cannot guarantee the shape of the page props
+  // as all page props are passed down through this component.
+  // pageProps: PropTypes.shape({}),
   Component: PropTypes.elementType,
 };
 
