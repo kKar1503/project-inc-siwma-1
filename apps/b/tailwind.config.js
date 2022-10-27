0c3645c01264030e84fe39f46989c0b8ed352816
Acrylic125 <acrylic125email@gmail.com>
2022-10-27 18:31:37 +0800

    chore: :rocket: add tailwindcss and daisyui

--- apps/b/tailwind.config.js
+++ apps/b/tailwind.config.js
@@ -1,2 +1,8 @@
 /** @type {import('tailwindcss').Config} */
-module.exports = require('@inc/styles/tailwind.config');
+module.exports = {
+  content: ['./pages/**/*.{js,ts,jsx,tsx}', './components/**/*.{js,ts,jsx,tsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+};
