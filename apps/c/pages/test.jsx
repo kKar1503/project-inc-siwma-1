69818048f096b086992e14de3757a816e9bbc7aa
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2022-10-30 04:50:46 +0800

    feat: implemented basic functionality
    todo: find a way to set z index properly and use color scheme

--- apps/c/pages/test.jsx
+++ apps/c/pages/test.jsx
@@ -6,6 +6,11 @@ const Test = () => (
       navigationTabs={{
         tab1: '#',
         tab2: '#2',
+        tab3: '#2',
+        tab4: '#2',
+        tab5: '#2',
+        tab6: '#2',
+        tab7: '#2',
       }}
     />
     {/* Other content to test the hamburger menu */}
