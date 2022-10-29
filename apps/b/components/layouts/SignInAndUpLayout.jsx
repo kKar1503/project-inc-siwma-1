fb4e9606b1f2749dc573bb30824f8dd2cf731f2d
Acrylic125 <acrylic125email@gmail.com>
2022-10-29 18:19:40 +0800

    fix: fix hydration problems and scaling image issue

--- apps/b/components/layouts/SignInAndUpLayout.jsx
+++ apps/b/components/layouts/SignInAndUpLayout.jsx
@@ -18,7 +18,7 @@ const propTypes = {
  */
 const LargeLayout = ({ children }) => (
   <div className="w-screen h-screen flex flex-row justify-center items-center">
-    <Image src="/siwma-bg.jpg" alt="SIWMA Background" className="opacity-60" fill />
+    <Image src="/siwma-bg.jpg" alt="SIWMA Background" className="opacity-60 object-cover" fill />
     <div className="flex flex-col justify-center items-center p-16 w-full h-full">
       <section className="flex flex-col w-full max-w-xl max-h-full bg-base-100 shadow-lg rounded-xl z-10">
         <header className="flex flex-row items-center justify-center p-8">
@@ -45,14 +45,14 @@ LargeLayout.propTypes = propTypes;
  */
 const SmallLayout = ({ children }) => (
   <div className="w-screen h-screen flex">
-    <Image src="/siwma-bg.jpg" alt="SIWMA Banner" fill className="object-cover opacity-60" />
+    <Image src="/siwma-bg.jpg" alt="SIWMA Banner" fill className="opacity-60 object-cover" />
     <section className="flex flex-col w-full h-full z-10">
       <header className="flex flex-row items-center justify-center bg-base-100 py-4 rounded-b-xl">
         <div className="relative h-8 w-full">
           <Image src="/siwma-logo.png" alt="SIWMA Background" fill className="object-contain" />
         </div>
       </header>
-      <div className="relative h-1/4">
+      <div className="relative h-1/3">
         {/* <Image src="/siwma-bg.jpg" alt="SIWMA Banner" fill className="object-cover" /> */}
       </div>
       <div className="flex flex-col gap-4 flex-1 overflow-y-auto bg-base-100 p-8 rounded-t-xl">
@@ -65,15 +65,21 @@ const SmallLayout = ({ children }) => (
 SmallLayout.propTypes = propTypes;
 
 /**
+ * HYDRATION WARNING!
+ * This component is NOT SSR FRIENDLY DUE TO THE NEED FOR IT TO BE RESPONSIVE!
+ * Wrap this component in a dynamic() with no ssr call to prevent it from being rendered on the server.
+ *
  * @type {React.FC<PropTypes.InferProps<typeof propTypes>>}
  */
 const SignInAndUpLayout = ({ children }) => {
-  // Here, we are using the tailwind config. We will fallback to 0px if the config is not found.
-  const isMobile = useMediaQuery({
-    query: `(max-width: ${fullConfig.theme?.screens?.sm ?? '0px'})`,
+  // Danger! Hydration problem! Since the window object is not present in the server, this will always return false.
+  // However, if this is run in the client, it will return true if the screen is smaller than the sm breakpoint.
+  // Thus, we will have a mismatch UI in the server and client, thus a hydration error.
+  const isMobileMediaQuery = useMediaQuery({
+    query: `(max-width: ${fullConfig.theme?.screens?.md ?? '0px'})`,
   });
 
-  if (isMobile) {
+  if (isMobileMediaQuery) {
     return <SmallLayout>{children}</SmallLayout>;
   }
   return <LargeLayout>{children}</LargeLayout>;
