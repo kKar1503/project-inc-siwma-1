7bfa02446dfb8f29691946adf92827b7e72da1f4
Acrylic125 <acrylic125email@gmail.com>
2022-10-29 17:08:27 +0800

    refactor: :art: made eslint happy

--- apps/b/components/layouts/SignInAndUpLayout.jsx
+++ apps/b/components/layouts/SignInAndUpLayout.jsx
@@ -1,72 +1,71 @@
 // TODO: Move this to the ui packages when tailwind is setup.
 
+import PropTypes from 'prop-types';
 import resolveConfig from 'tailwindcss/resolveConfig';
-import tailwindConfig from '../../tailwind.config';
 import Image from 'next/image';
 import { useMediaQuery } from 'react-responsive';
 import React from 'react';
+import tailwindConfig from '../../tailwind.config';
 
 const fullConfig = resolveConfig(tailwindConfig);
 
-/**
- * @type {React.FC<{
- *  children: import('react').ReactNode
- * }>}
- */
-const LargeLayout = ({ children }) => {
-  return (
-    <div className="w-screen h-screen flex flex-row justify-center items-center">
-      <Image src="/siwma-bg.jpg" alt="SIWMA Background" className="opacity-60" fill />
-      <div className="flex flex-col justify-center items-center p-16 w-full h-full">
-        <section className="flex flex-col w-full max-w-xl max-h-full bg-base-100 shadow-lg rounded-xl z-10">
-          <header className="flex flex-row items-center justify-center p-8">
-            <div className="relative h-16 w-full">
-              <Image src="/siwma-logo.png" alt="SIWMA Banner" fill className="object-contain" />
-            </div>
-          </header>
-          <div
-            className="bg-base-200"
-            style={{
-              height: 1,
-            }}
-          />
-          <div className="flex flex-col flex-1 w-full p-8 overflow-y-auto">{children}</div>
-        </section>
-      </div>
-    </div>
-  );
+const propTypes = {
+  children: PropTypes.elementType,
 };
 
 /**
- * @type {React.FC<{
- *  children: import('react').ReactNode
- * }>}
+ * @type {React.FC<PropTypes.InferProps<typeof propTypes>>}
  */
-const SmallLayout = ({ children }) => {
-  return (
-    <div className="w-screen h-screen flex">
-      <Image src="/siwma-bg.jpg" alt="SIWMA Banner" fill className="object-cover opacity-60" />
-      <section className="flex flex-col w-full h-full z-10">
-        <header className="flex flex-row items-center justify-center bg-base-100 py-4 rounded-b-xl">
-          <div className="relative h-8 w-full">
-            <Image src="/siwma-logo.png" alt="SIWMA Background" fill className="object-contain" />
+const LargeLayout = ({ children }) => (
+  <div className="w-screen h-screen flex flex-row justify-center items-center">
+    <Image src="/siwma-bg.jpg" alt="SIWMA Background" className="opacity-60" fill />
+    <div className="flex flex-col justify-center items-center p-16 w-full h-full">
+      <section className="flex flex-col w-full max-w-xl max-h-full bg-base-100 shadow-lg rounded-xl z-10">
+        <header className="flex flex-row items-center justify-center p-8">
+          <div className="relative h-16 w-full">
+            <Image src="/siwma-logo.png" alt="SIWMA Banner" fill className="object-contain" />
           </div>
         </header>
-        <div className="relative h-1/4">
-          {/* <Image src="/siwma-bg.jpg" alt="SIWMA Banner" fill className="object-cover" /> */}
-        </div>
-        <div className="flex flex-col gap-4 flex-1 overflow-y-auto bg-base-100 p-8 rounded-t-xl">
-          {children}
-        </div>
+        <div
+          className="bg-base-200"
+          style={{
+            height: 1,
+          }}
+        />
+        <div className="flex flex-col flex-1 w-full p-8 overflow-y-auto">{children}</div>
       </section>
     </div>
-  );
-};
+  </div>
+);
+
+LargeLayout.propTypes = propTypes;
 
 /**
- * @type {React.FC<{
- *  children: import('react').ReactNode
- * }>}
+ * @type {React.FC<PropTypes.InferProps<typeof propTypes>>}
+ */
+const SmallLayout = ({ children }) => (
+  <div className="w-screen h-screen flex">
+    <Image src="/siwma-bg.jpg" alt="SIWMA Banner" fill className="object-cover opacity-60" />
+    <section className="flex flex-col w-full h-full z-10">
+      <header className="flex flex-row items-center justify-center bg-base-100 py-4 rounded-b-xl">
+        <div className="relative h-8 w-full">
+          <Image src="/siwma-logo.png" alt="SIWMA Background" fill className="object-contain" />
+        </div>
+      </header>
+      <div className="relative h-1/4">
+        {/* <Image src="/siwma-bg.jpg" alt="SIWMA Banner" fill className="object-cover" /> */}
+      </div>
+      <div className="flex flex-col gap-4 flex-1 overflow-y-auto bg-base-100 p-8 rounded-t-xl">
+        {children}
+      </div>
+    </section>
+  </div>
+);
+
+SmallLayout.propTypes = propTypes;
+
+/**
+ * @type {React.FC<PropTypes.InferProps<typeof propTypes>>}
  */
 const SignInAndUpLayout = ({ children }) => {
   // Here, we are using the tailwind config. We will fallback to 0px if the config is not found.
@@ -79,5 +78,7 @@ const SignInAndUpLayout = ({ children }) => {
   }
   return <LargeLayout>{children}</LargeLayout>;
 };
+
+SignInAndUpLayout.propTypes = propTypes;
 
 export default SignInAndUpLayout;
