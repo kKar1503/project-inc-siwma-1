ef4140cde247b236e34dd1c441e60be96ba67613
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2022-10-29 22:22:51 +0800

    feat: converted to arrow function
    Background colors are there only for debugging purposes as it is not completed yet

--- apps/c/components/navigation/hamburgerMenu/hamburgermenu.jsx
+++ apps/c/components/navigation/hamburgerMenu/hamburgermenu.jsx
@@ -1,36 +1,45 @@
-import {useState} from "react";
-import "./HamburgerMenuStyles.module.css"
+import { useState } from 'react';
+import PropTypes from 'prop-types';
+import './HamburgerMenuStyles.module.css';
 
-export default function HamburgerMenu({navigationTabs}) {
+const HamburgerMenu = ({ navigationTabs }) => {
   const [open, setOpen] = useState(false);
-  const content = Object.keys(navigationTabs).map(key => {
+  const content = Object.keys(navigationTabs).map((key) => {
     const value = navigationTabs[key];
     return (
-      <li style={{margin: "5px"}}>
+      <li style={{ margin: '5px' }}>
         <a href={value} key={key}>
-          <div style={{backgroundColor: "gray"}} className={"hamburger-menu-element"}>
+          <div style={{ backgroundColor: 'gray' }} className="hamburger-menu-element">
             {key}
           </div>
         </a>
       </li>
-    )
+    );
   });
   const hamburgerContent = (
-    <div style={{position: "absolute", backgroundColor: "red", width: "12em"}}>
-      <ul style={{listStyle: "none"}}>
-        {content}
-      </ul>
+    <div style={{ position: 'absolute', backgroundColor: 'red', width: '12em' }}>
+      <ul style={{ listStyle: 'none' }}>{content}</ul>
     </div>
-  )
+  );
   return (
     <div>
-      <button className={"btn"} onClick={() => {
-        setOpen(!open)
-      }}>
+      <button
+        className="btn"
+        onClick={() => {
+          setOpen(!open);
+        }}
+      >
         Hamburger
       </button>
 
       {open && hamburgerContent}
     </div>
-  )
-}
+  );
+};
+export default HamburgerMenu;
+HamburgerMenu.propTypes = {
+  navigationTabs: PropTypes.shape({
+    key: PropTypes.string,
+    value: PropTypes.string,
+  }),
+};
--- apps/c/components/navigation/hamburgerMenu/propTypes.js
+++
@@ -1,9 +0,0 @@
-import PropTypes from 'prop-types';
-import HamburgerMenu from './hamburgermenu';
-
-HamburgerMenu.propTypes = {
-  navigationTabs: PropTypes.shape({
-    key: PropTypes.string,
-    value: PropTypes.string,
-  }),
-};
