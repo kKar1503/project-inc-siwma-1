ab735445452651ddfedc93096e4a8d7d503ae4be
MrsDarth <7jgbm234@gmail.com>
2022-10-29 22:38:50 +0800

    build: add chart

---
+++ apps/d/pages/analyticCharts.jsx
@@ -0,0 +1,38 @@
+import Image from 'next/image';
+
+const AnalyticCharts = () => (
+  <div>
+    <h2 className="text-2xl font-semibold">Company</h2>
+    <div className="flex flex-row gap-4">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <p>Graph 1</p>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <p>Graph 2</p>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <p>Graph 3</p>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <p>Graph 4</p>
+      </div>
+    </div>
+    <h2 className="text-2xl font-semibold">Settlements</h2>
+    <div className="flex flex-row gap-4">
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <p>Graph 1</p>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <p>Graph 2</p>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <p>Graph 3</p>
+      </div>
+      <div className="card w-96 bg-base-100 shadow-xl">
+        <p>Graph 4</p>
+      </div>
+    </div>
+  </div>
+);
+
+export default AnalyticCharts;
