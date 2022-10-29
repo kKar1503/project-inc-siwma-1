257082540d50ae2b9588d3a4656eb6f1a3f8cb22
Kar <ISoNice4572@gmail.com>
2022-10-29 13:08:37 +0800

    build: :arrow_up: added tailwind & daisyui dep and added packages for shared config and styling

---
+++ packages/styles/tailwind.config.js
@@ -0,0 +1,11 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ['./pages/**/*.{js,jsx}', './components/**/*.{js,jsx}'],
+  theme: {
+    extend: {},
+  },
+  plugins: [require('daisyui')],
+  daisyui: {
+    themes: ['corporate'],
+  },
+};
