723748e9453e7b61cea9a5c2effc6e3c8e8d18d7
MrsDarth <7jgbm234@gmail.com>
2022-10-30 23:24:06 +0800

    build: added all 6 charts

---
+++ packages/charts/components/settlements/TopSellingItemsChart.jsx
@@ -0,0 +1,43 @@
+import PropTypes from 'prop-types';
+import { VictoryAxis, VictoryBar, VictoryChart, VictoryLabel } from 'victory';
+import Title from '../Title';
+
+const color = '#5CBBCC';
+
+const mockData = [
+  { item: 'Item 1', amount: 360 },
+  { item: 'Item 2', amount: 320 },
+  { item: 'Item 3', amount: 260 },
+  { item: 'Item 4', amount: 200 },
+  { item: 'Item 5', amount: 190 },
+  { item: 'Item 6', amount: 190 },
+].reverse();
+
+const TopSellingItemsChart = ({ data = mockData }) => (
+  <VictoryChart
+    horizontal
+    padding={{ left: 100, bottom: 50, right: 30, top: 50 }}
+    domainPadding={40}
+  >
+    <Title text="Top 6 Selling Items in 2022" />
+    <VictoryAxis dependentAxis style={{ grid: { stroke: '#DADADA' } }} />
+    <VictoryAxis dependentAxis label="Amount" />
+    <VictoryBar data={data} x="item" y="amount" style={{ data: { fill: color } }} />
+    <VictoryAxis
+      style={{ tickLabels: { fontSize: 10 } }}
+      label="Items"
+      axisLabelComponent={<VictoryLabel dy={-30} />}
+    />
+  </VictoryChart>
+);
+
+TopSellingItemsChart.propTypes = {
+  data: PropTypes.arrayOf(
+    PropTypes.shape({
+      company: PropTypes.string,
+      posts: PropTypes.number,
+    })
+  ),
+};
+
+export default TopSellingItemsChart;
--- packages/charts/index.jsx
+++ packages/charts/index.jsx
@@ -1,6 +1,6 @@
-export { default as MostPostsBarChart } from './components/companies/MostPostsBarChart';
-export { default as MostBuyAndSellStackChart } from './components/companies/MostBuyAndSellStackChart';
+export { default as MostBuyAndSellChart } from './components/companies/MostBuyAndSellChart';
 export { default as NumberOfBuyAndSellChart } from './components/companies/NumberOfBuyAndSellChart';
-export { default as MostCommonCategoryChart } from './components/companies/MostCommonCategoryChart';
-
-export const a = 69;
+export { default as MostPostsChart } from './components/companies/MostPostsChart';
+export { default as BestSellingSubCategoriesChart } from './components/settlements/BestSellingSubCategoriesChart';
+export { default as MostCommonCategoryChart } from './components/settlements/MostCommonCategoryChart';
+export { default as TopSellingItemsChart } from './components/settlements/TopSellingItemsChart';
