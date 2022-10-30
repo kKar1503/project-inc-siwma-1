cf78babaea007271605f2f87a614659feaa1e1dd
huishilhs <huishilhs@gmail.com>
2022-10-30 22:01:31 +0800

    build: :lipstick: styling upload card
    arranged the dropzone and the button

--- apps/d/pages/adminUpload.jsx
+++ apps/d/pages/adminUpload.jsx
@@ -1,30 +1,34 @@
 const adminUpload = () => (
-  <div className="flex flex-row-reverse space-x-16 space-x-reverse my-32 mx-10 ...">
-    <div className="card w-1/3 h-80 bg-base-100 shadow-xl">
+  <div className="flex flex-row-reverse space-x-16 space-x-reverse my-32 mx-8 ...">
+    <div className="card w-1/3 h-80 bg-base-100 shadow-xl px-8 py-8 ">
       <div className="max-w-xl">
-        <label className="flex justify-center w-full h-32 px-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
-          <span className="flex items-center space-x-2">
+        <label className="flex justify-center w-full h-40 px-4 mt-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+          <span className="items-center space-x-2">
             <svg
-              xmlns="http://www.w3.org/2000/svg"
-              className="w-6 h-6 text-gray-600"
-              fill="none"
+              className="h-16 w-16 text-black m-auto my-4"
+              width="24"
+              height="24"
               viewBox="0 0 24 24"
-              stroke="currentColor"
               strokeWidth="2"
+              stroke="currentColor"
+              fill="none"
+              strokeLinecap="round"
+              strokeLinejoin="round"
             >
-              <path
-                strokeLinecap="round"
-                strokeLinejoin="round"
-                d="M7 16a4 4 0 01-.88-7.903A5 5 0 1115.9 6L16 6a5 5 0 011 9.9M15 13l-3-3m0 0l-3 3m3-3v12"
-              />
+              {' '}
+              <path stroke="none" d="M0 0h24v24H0z" />{' '}
+              <path d="M4 17v2a2 2 0 0 0 2 2h12a2 2 0 0 0 2 -2v-2" />{' '}
+              <polyline points="7 9 12 4 17 9" /> <line x1="12" y1="4" x2="12" y2="16" />
             </svg>
-            <span className="font-medium text-gray-600">
-              Drop files to Attach, or
-              <span className="text-blue-600 underline">browse</span>
-            </span>
+            <p className="text-xs text-gray-600 text-center my-6">
+              Click to upload or drag and drop SVG, PNG or JPG (MAX. 800 x 400px)
+            </p>
           </span>
           <input type="file" name="file_upload" className="hidden" />
         </label>
+        <button className="btn btn-active btn-ghost rounded-md w-1/2 h-6 mx-20 my-8 normal-case text-base">
+          Create
+        </button>
       </div>
     </div>
     <div className="card w-1/3 h-80 bg-base-100 shadow-xl">
