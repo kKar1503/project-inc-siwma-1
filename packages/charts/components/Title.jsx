a4518bcd5cc39fc022c98fd6cff6b39a5559a5e0
MrsDarth <7jgbm234@gmail.com>
2022-10-31 00:58:14 +0800

    build: change title size

--- packages/charts/components/Title.jsx
+++ packages/charts/components/Title.jsx
@@ -1,8 +1,13 @@
+import PropTypes from 'prop-types';
 import { VictoryLabel } from 'victory';
 
-// eslint-disable-next-line react/jsx-props-no-spreading
-const Title = (props) => (
-  <VictoryLabel x={200} y={20} style={{ fontSize: 20 }} textAnchor="middle" {...props} />
+const Title = ({ text }) => (
+  // eslint-disable-next-line react/jsx-props-no-spreading
+  <VictoryLabel x={200} y={20} style={{ fontSize: 20 }} textAnchor="middle" text={text} />
 );
+
+Title.propTypes = {
+  text: PropTypes.oneOfType([PropTypes.string, PropTypes.arrayOf(PropTypes.string)]),
+};
 
 export default Title;
