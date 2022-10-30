da822ae9390a298d81772616d249b85b8e37e8c7
Amos Choo <tohmenchoo@gmail.com>
2022-10-30 19:09:28 +0800

    build: install victory and fix tailwind error

--- apps/d/pages/analyticCharts.jsx
+++ apps/d/pages/analyticCharts.jsx
@@ -10,26 +10,26 @@ import {
 const AnalyticCharts = () => (
   <div className="grid place-content-center">
     <h2 className="my-2 text-2xl font-semibold ">Company</h2>
-    <div className="my-5 flex flex-wrap flex-direction-row gap-8">
-      <div className="card w-96 bg-base-100 shadow-md">
+    <div className="my-5 flex sm:flex-wrap md:flex-nowrap overflow-auto flex-direction-row gap-8">
+      <div className="card w-96 bg-base-100 shadow-md shrink-0">
         <MostPostsChart />
       </div>
-      <div className="card w-96 bg-base-100 shadow-md">
+      <div className="card w-96 bg-base-100 shadow-md shrink-0">
         <MostBuyAndSellChart />
       </div>
-      <div className="card w-96 bg-base-100 shadow-md">
+      <div className="card w-96 bg-base-100 shadow-md shrink-0">
         <NumberOfBuyAndSellChart />
       </div>
     </div>
     <h2 className="my-2 text-2xl font-semibold ">Settlements</h2>
-    <div className="my-5 flex flex-wrap flex-direction-row gap-8">
-      <div className="card w-96 bg-base-100 shadow-md">
+    <div className="my-5 flex sm:flex-wrap md:flex-nowrap overflow-auto flex-direction-row gap-8">
+      <div className="card w-96 bg-base-100 shadow-md shrink-0">
         <BestSellingSubCategoriesChart />
       </div>
-      <div className="card w-96 bg-base-100 shadow-md">
+      <div className="card w-96 bg-base-100 shadow-md shrink-0">
         <MostCommonCategoryChart />
       </div>
-      <div className="card w-96 bg-base-100 shadow-md">
+      <div className="card w-96 bg-base-100 shadow-md shrink-0">
         <TopSellingItemsChart />
       </div>
     </div>
