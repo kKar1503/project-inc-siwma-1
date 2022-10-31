b7015fd65daac2a0e69cd23b988d83f34a9abf6d
Wu Yining <yyiningsp@gmail.com>
2022-11-01 01:49:46 +0800

    build: add button icons
    change of button texts to icons

--- apps/d/pages/adminUpload.jsx
+++ apps/d/pages/adminUpload.jsx
@@ -131,7 +131,38 @@ const AdminUpload = () => {
                             onFocus={() => handleHover(data.name)}
                             onClick={() => handleClick(data.name)}
                           >
-                            {getData?.type === 'select' && data.selected ? 'x' : '+'}
+                            {getData?.type === 'select' && data.selected ? (
+                              <svg
+                                className="h-4 w-4 text-black-500"
+                                width="24"
+                                height="24"
+                                viewBox="0 0 24 24"
+                                strokeWidth="2"
+                                stroke="currentColor"
+                                fill="none"
+                                strokeLinecap="round"
+                                strokeLinejoin="round"
+                              >
+                                {' '}
+                                <path stroke="none" d="M0 0h24v24H0z" />{' '}
+                                <line x1="18" y1="6" x2="6" y2="18" />{' '}
+                                <line x1="6" y1="6" x2="18" y2="18" />
+                              </svg>
+                            ) : (
+                              <svg
+                                className="h-4 w-4  text-black-500"
+                                fill="none"
+                                viewBox="0 0 24 24"
+                                stroke="currentColor"
+                              >
+                                <path
+                                  strokeLinecap="round"
+                                  strokeLinejoin="round"
+                                  strokeWidth="2"
+                                  d="M12 4v16m8-8H4"
+                                />
+                              </svg>
+                            )}
                           </button>
                         </li>
                       ))}
