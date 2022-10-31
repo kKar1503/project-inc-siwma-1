0dc82829a7f86b5bfd06eb102efba9d9fe73310c
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2022-10-31 21:12:49 +0800

    feat: redid hamburger menu as 2nd hamburger menu (still wip)

--- apps/c/components/navigation/hamburgermenu.jsx
+++ apps/c/components/navigation/hamburgermenu.jsx
@@ -1,189 +1,133 @@
-import { useEffect, useState } from 'react';
-import { bool, func, number, objectOf, string } from 'prop-types';
-import { useRouter } from 'next/router';
-
-// todo use color scheme
-// todo make the menu close when clicking outside it
-// todo make the menu items fill up the empty space
-// ------------------ Colors -----------------
-const IconBackgroundColor = 'white';
-const IconColor = 'black';
-const IconHoverColor = 'white';
-const IconBackgroundHoverColor = 'grey';
-
-const ElementHoverColor = 'blue';
-const ElementDefaultColor = 'grey';
-
-const MenuBackgroundColor = 'red';
+import { useState } from 'react';
+import { node, string } from 'prop-types';
+// ------------------ Layout Configuration ------------------
 
 /**
- * The Icon that is used to open the HamburgerMenu
- * @param open {boolean} - if the menu is open or not
- * @param setOpen {(boolean) => void} - function to set the open state
- * @returns {JSX.Element} - the hamburger button
- * @constructor - the hamburger button
+ * This component is a central location to customize the HamburgerMenu
+ * @constructor
  */
-const HamburgerButtonIcon = ({ open, setOpen }) => {
-  // ------------------ UseStates -----------------
-  const [hover, setHover] = useState(false);
+const LayoutEditor = () => (
+  <>
+    <SidebarHeaderIcon />
 
-  // ------------------ Handles -----------------
-  const onClickHandle = () => {
-    setOpen(!open);
-  };
-  const onHoverHandle = () => {
-    setHover(true);
-  };
-  const onLeaveHandle = () => {
-    setHover(false);
-  };
-  // ------------------ Styles -----------------
-  const style = {
-    backgroundColor: hover ? IconBackgroundHoverColor : IconBackgroundColor,
-    zIndex: 2,
-  };
+    <MenuLineDivider />
 
-  // ------------------ Return -----------------
-  return (
-    <div>
-      <button
-        className="btn"
-        onClick={onClickHandle}
-        onMouseEnter={onHoverHandle}
-        onMouseLeave={onLeaveHandle}
-        style={style}
-      >
-        <svg
-          height={32}
-          width={32}
-          style={{
-            fill: hover ? IconHoverColor : IconColor,
-          }}
-        >
-          <path d="M4 10h24a2 2 0 0 0 0-4H4a2 2 0 0 0 0 4zm24 4H4a2 2 0 0 0 0 4h24a2 2 0 0 0 0-4zm0 8H4a2 2 0 0 0 0 4h24a2 2 0 0 0 0-4z" />
-        </svg>
-      </button>
-    </div>
-  );
-};
-// ------------------ PropTypes ------------------
+    <SidebarSearch />
+    <MenuItem name="Home" redirectLink="/" customIcon={<i className="bi bi-house-door-fill" />} />
+    <MenuItem name="Bookmark" redirectLink="/" customIcon={<i className="bi bi-bookmark-fill" />} />
+    <MenuItem
+      name="Chat box"
+      redirectLink="/"
+      customIcon={<i className="bi bi-chat-left-text-fill" />}
+    >
+      <SubMenuItem name="Social" redirectLink="/" />
+      <SubMenuItem name="Personal" redirectLink="/" />
+      <SubMenuItem name="Friends" redirectLink="/" />
+    </MenuItem>
 
-HamburgerButtonIcon.propTypes = {
-  open: bool,
-  setOpen: func,
-};
+    <MenuLineDivider />
+
+    <SidebarLogout />
+  </>
+);
+
+// -----------------------------------------------
+
+/**
+ * MenuLineDivider is a line divider that is displayed in the menu
+ * This can be used to visually separate items
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const MenuLineDivider = () => <div className="my-4 bg-gray-600 h-[1px]" />;
 
 /**
  * The Item represents each tab in the HamburgerMenu
  * @param name {string} - the name of the tab
+ * @param customIcon {JSX.Element} - the icon of the tab
  * @param redirectLink {string} - the link of the tab
+ * @param children {JSX.Element} - the children of the tab
  * @returns {JSX.Element} - the item
  * @constructor - the item
  */
-const HamburgerMenuItem = ({ name, redirectLink, width }) => {
-  const router = useRouter();
+const MenuItem = ({ name, customIcon, redirectLink, children }) => (
+  <>
+    <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+      {customIcon && <customIcon />}
+      <span className="text-[15px] ml-4 text-gray-200 font-bold">{name}</span>
+      {children && (
+        <span className="text-sm rotate-180" id="arrow">
+          <i className="bi bi-chevron-down" />
+        </span>
+      )}
+    </div>
+    {children && (
+      <div className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu">
+        {children}
+      </div>
+    )}
+  </>
+);
 
-  // ------------------ UseStates -----------------
-  const [hover, setHover] = useState(false);
-
-  // ------------------ Styles -----------------
-  const listItemStyle = {
-    backgroundColor: hover ? ElementHoverColor : ElementDefaultColor,
-    textAlign: 'center',
-    flex: '1 1 0',
-    width: `${width}px`,
-  };
-
-  const spanStyle = {
-    display: 'table-cell',
-    verticalAlign: 'middle',
-  };
-
-  // ------------------ Handles -----------------
-  const onClickHandle = async () => {
-    await router.push(redirectLink);
-  };
-  const onHoverHandle = () => {
-    setHover(true);
-  };
-  const onLeaveHandle = () => {
-    setHover(false);
-  };
-
-  // ------------------ Return -----------------
-  return (
-    <span style={spanStyle}>
-      <button
-        style={listItemStyle}
-        className="btn"
-        onMouseLeave={onLeaveHandle}
-        onMouseEnter={onHoverHandle}
-        onClick={onClickHandle}
-      >
-        {name}
-      </button>
-    </span>
-  );
-};
-
-// ------------------ PropTypes ------------------
-
-HamburgerMenuItem.propTypes = {
-  name: string,
-  redirectLink: string,
-  width: number,
-};
+/**
+ * SubMenuItem is a drawer item that is displayed by a MenuItem
+ * @param name {string} - the name of the item
+ * @param customIcon {JSX.Element} - the icon that is displayed next to the name
+ * @param redirectLink {string} - the link that is redirected to when the item is clicked
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const SubMenuItem = ({ name, customIcon, redirectLink }) => (
+  <>
+    {customIcon && <customIcon />}
+    <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">{name}</h1>
+  </>
+);
 
 /**
  * The ExpandedHamburgerMenu is the menu that is shown when the HamburgerButtonIcon is clicked
- * @param navigationTabs {object} - the tabs that are shown in the menu
- * @param open {boolean} - if the menu is open or not
- * @returns {JSX.Element} - the menu
- * @constructor - the menu
+ * This is the icon that is displayed on the top left of the screen
+ * @returns {JSX.Element}
+ * @constructor
  */
-const ExpandedHamburgerMenu = ({ navigationTabs, open }) => {
-  // todo:
-  // implement a way to close the menu when clicking outside it
-  // implement opening and closing animations
+const SidebarHeaderIcon = () => (
+  <div className="text-gray-100 text-xl">
+    <div className="p-2.5 mt-1 flex items-center">
+      <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-blue-600" />
+      <h1 className="font-bold text-gray-200 text-[15px] ml-3">S I W M A</h1>
+      <i className="bi bi-x cursor-pointer ml-28 lg:hidden" />
+    </div>
+  </div>
+);
 
-  // ------------------ UseStats -----------------
-  const [width, setWidth] = useState(0);
+/**
+ * The sidebar logout is a logout button that is displayed in the sidebar
+ * todo no logout functionality yet
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const SidebarLogout = () => (
+  <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
+    <i className="bi bi-box-arrow-in-right" />
+    <span className="text-[15px] ml-4 text-gray-200 font-bold">Logout</span>
+  </div>
+);
 
-  // ------------------ Styles -----------------
-  const style = {
-    position: 'absolute',
-    backgroundColor: MenuBackgroundColor,
-    width: '100%',
-    transition: 'all 0.5s ease',
-    marginTop: open ? '0em' : '-7em',
-    zIndex: 1,
-    display: 'flex',
-    justifyContent: 'space-around',
-  };
-
-  // ------------------ Mapping -----------------
-  const keys = Object.keys(navigationTabs);
-
-  // ------------------ UseEffect -----------------
-  useEffect(() => {
-    setWidth(window.innerWidth / keys.length);
-  }, [keys]);
-
-  const content = keys.map((name) => {
-    const redirectLink = navigationTabs[name];
-    return <HamburgerMenuItem key={name} name={name} redirectLink={redirectLink} width={width} />;
-  });
-
-  // ------------------ Return -----------------
-  return <div style={style}>{content}</div>;
-};
-
-// ------------------ PropTypes ------------------
-
-ExpandedHamburgerMenu.propTypes = {
-  navigationTabs: objectOf(string),
-  open: bool,
-};
+/**
+ * The Sidebar search is a search bar that is displayed in the sidebar
+ * @returns {JSX.Element}
+ * @constructor
+ */
+const SidebarSearch = () => (
+  <div className="p-2.5 flex items-center rounded-md px-4 duration-300 cursor-pointer bg-gray-700 text-white">
+    <i className="bi bi-search text-sm" />
+    <input
+      type="text"
+      placeholder="Search"
+      className="text-[15px] ml-4 w-full bg-transparent focus:outline-none"
+    />
+  </div>
+);
 
 // The purpose of using NextJS is to keep component related logic within 1 file ~ kar lok
 // Rip my OCD
@@ -191,28 +135,54 @@ ExpandedHamburgerMenu.propTypes = {
 // ------------------ Main Component -----------------
 /**
  * The HamburgerMenu is a hamburger menu
- * @param navigationTabs {Object.<string, string>} - the tabs that are shown in the menu
  * @returns {JSX.Element}
  * @constructor
  */
-const HamburgerMenu = ({ navigationTabs }) => {
+const HamburgerMenu = () => {
   // ------------------ useStates -----------------
   const [open, setOpen] = useState(false);
 
   // ------------------ Return -----------------
   return (
     <div>
-      <HamburgerButtonIcon open={open} setOpen={setOpen} />
-      <ExpandedHamburgerMenu navigationTabs={navigationTabs} open={open} />
+      <body className="bg-blue-600">
+        <span className="absolute text-white text-4xl top-5 left-4 cursor-pointer">
+          <i className="bi bi-filter-left px-2 bg-gray-900 rounded-md" />
+        </span>
+        <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900">
+          <LayoutEditor />
+        </div>
+      </body>
     </div>
   );
 };
 
-// ------------------ PropTypes ------------------
-
-HamburgerMenu.propTypes = {
-  navigationTabs: objectOf(string),
-};
-
 // ------------------ Export ------------------
 export default HamburgerMenu;
+
+// ------------------ PropTypes ------------------
+
+LayoutEditor.propTypes = {};
+
+MenuLineDivider.propTypes = {};
+
+MenuItem.propTypes = {
+  name: string.isRequired,
+  customIcon: node,
+  children: node,
+  redirectLink: string,
+};
+
+SubMenuItem.propTypes = {
+  name: string.required,
+  customIcon: node,
+  redirectLink: string,
+};
+
+SidebarHeaderIcon.propTypes = {};
+
+SidebarLogout.propTypes = {};
+
+SidebarSearch.propTypes = {};
+
+HamburgerMenu.propTypes = {};
--- apps/c/pages/test.jsx
+++ apps/c/pages/test.jsx
@@ -1,18 +1,17 @@
+import HamburgerMenuOld from '../components/navigation/hamburgermenuOld';
 import HamburgerMenu from '../components/navigation/hamburgermenu';
 
 const Test = () => (
   <>
-    <HamburgerMenu
-      navigationTabs={{
-        tab1: '#',
-        tab2: '#2',
-        tab3: '#2',
-        tab4: '#2',
-        tab5: '#2',
-        tab6: '#2',
-        tab7: '#2',
-      }}
-    />
+    {/* <HamburgerMenuOld */}
+    {/*  navigationTabs={{ */}
+    {/*    tab1: '#', */}
+    {/*    tab2: '#2', */}
+    {/*    tab3: '#2', */}
+    {/*    tab4: '#2', */}
+    {/*  }} */}
+    {/* /> */}
+    <HamburgerMenu />
     {/* Other content to test the hamburger menu */}
     Lorem ipsum dolor, sit amet consectetur adipisicing elit. Minus adipisci vero fugit voluptas,
     blanditiis beatae amet ratione reprehenderit ad repellendus! Fuga velit id ad? Architecto
