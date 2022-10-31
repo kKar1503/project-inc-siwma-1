6ee50c44d892da0b168e883b78b87aaa15ea0a78
Amos Choo <tohmenchoo@gmail.com>
2022-11-01 03:04:11 +0800

    refactor: file changs
    had to revert the commit cause something bad happened

--- packages/charts/components/advertisments/NumberOfAdsChartCopy.jsx
+++
@@ -1,6 +0,0 @@
-import { VictoryChart, VictoryBar, VictoryAxis, VictoryLabel, Bar } from 'victory';
-// import { DateTime } from 'luxon';
-
-const NumberOfAdsChart = () => <p>1</p>;
-
-export default NumberOfAdsChart;
