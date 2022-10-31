bf7b3119fd8c3673718328e27236e643f1923f8c
notlega <onghinjourn@gmail.com>
2022-10-31 18:34:41 +0800

    feat: :sparkles: updates page for testing
    splits page in half for duplication of wireframe.
    adds custom colours for debugging purposes

--- apps/c/pages/buySellListing.jsx
+++ apps/c/pages/buySellListing.jsx
@@ -1,3 +1,30 @@
-const buySellListing = () => {};
+import Head from 'next/head';
+import CardBackground from '../components/CardBackground';
+
+const buySellListing = () => (
+  <>
+    <Head>
+      <title>Buy Sell Listing</title>
+    </Head>
+
+    <main>
+      {/* temp navbar */}
+      <div className="navbar bg-slate-700">
+        <button className="btn btn-ghost normal-case text-xl bg-slate-200 rounded-2xl">
+          SIWMA
+        </button>
+      </div>
+
+      <div className="flex justify-center mt-8 gap-40">
+        <div className="flex flex-col">
+          <CardBackground />
+        </div>
+        <div className="flex flex-col">
+          <CardBackground />
+        </div>
+      </div>
+    </main>
+  </>
+);
 
 export default buySellListing;
