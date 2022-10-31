a5759f4de97edd607db4e34a4fd433bdc063f2d7
Bengco Rance <Thinkaze@gmail.com>
2022-10-31 21:17:06 +0800

    feat: :sparkles: main layout done, only missing icons now

--- apps/b/components/AdminFigure.jsx
+++ apps/b/components/AdminFigure.jsx
@@ -2,12 +2,14 @@ import PropTypes from 'prop-types';
 import cx from 'classnames';
 
 const AdminFigure = ({ title, value, icon, color }) => (
-  <div className="flex flex-row w-full h-40 shadow-xl items-center p-6 rounded-2xl space-between justify-between">
-    <div className="flex flex-col mt-4 border h-full justify-center">
-      <p className={cx('text-5xl font-black py-1', color ?? 'text-gray-700')}>{value}</p>
-      <h2 className="text-xl font-semibold text-gray-700 py-1">{title}</h2>
+  <div className="flex flex-row grow h-40 shadow-xl items-center p-6 rounded-2xl space-between justify-between">
+    <div className="flex flex-col mt-4 h-full items-left">
+      <p className={cx('text-5xl font-black py-1 pr-4 select-none', color ?? 'text-gray-700')}>
+        {value}
+      </p>
+      <h2 className="text-lg text-left text-gray-700 py-1 select-none">{title}</h2>
     </div>
-    <div className="flex items-center justify-center w-24 h-24 bg-blue-500">{icon}</div>
+    <div className="flex items-center justify-center w-24 h-24 pl-4 bg-blue-500">{icon}</div>
   </div>
 );
 
