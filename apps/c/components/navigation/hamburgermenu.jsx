a77f3ba28610e51af74fa9c5b505c8a502d5e226
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2022-11-01 07:46:56 +0800

    feat: added basic functionality

--- apps/c/components/navigation/hamburgermenu.jsx
+++ apps/c/components/navigation/hamburgermenu.jsx
@@ -1,5 +1,7 @@
 import { useState } from 'react';
 import { node, string } from 'prop-types';
+import { useRouter } from 'next/router';
+
 // ------------------ Layout Configuration ------------------
 
 /**
@@ -50,24 +52,63 @@ const MenuLineDivider = () => <div className="my-4 bg-gray-600 h-[1px]" />;
  * @returns {JSX.Element} - the item
  * @constructor - the item
  */
-const MenuItem = ({ name, customIcon, redirectLink, children }) => (
-  <>
-    <div className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white">
-      {customIcon && <customIcon />}
-      <span className="text-[15px] ml-4 text-gray-200 font-bold">{name}</span>
-      {children && (
-        <span className="text-sm rotate-180" id="arrow">
-          <i className="bi bi-chevron-down" />
-        </span>
-      )}
-    </div>
-    {children && (
-      <div className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold" id="submenu">
-        {children}
+const MenuItem = ({ name, customIcon, redirectLink, children }) => {
+  // ------------------ useStates -----------------
+  const [open, setOpen] = useState(false);
+
+  // ------------------ Styles -----------------
+  const rotateStyles = {
+    transform: open ? 'rotate(90deg)' : 'rotate(0deg)',
+  };
+
+  const subMenuStyles = {
+    display: open ? 'block' : 'none',
+  };
+
+  // ------------------ Misc -----------------
+  const router = useRouter();
+
+  // ------------------ Handles -----------------
+  const redirect = () => {
+    router.push(redirectLink);
+  };
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
+  // ------------------ Return -----------------
+  return (
+    <>
+      <div
+        onClick={redirect}
+        role="presentation"
+        className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-blue-600 text-white"
+      >
+        {customIcon && <customIcon />}
+        <span className="text-[15px] ml-4 text-gray-200 font-bold">{name}</span>
+        {children && (
+          <span
+            onClick={toggle}
+            role="presentation"
+            className="text-sm rotate-180"
+            style={rotateStyles}
+          >
+            <i className="bi bi-chevron-down" />
+          </span>
+        )}
       </div>
-    )}
-  </>
-);
+      {children && (
+        <div
+          className="text-left text-sm mt-2 w-4/5 mx-auto text-gray-200 font-bold"
+          style={subMenuStyles}
+        >
+          {children}
+        </div>
+      )}
+    </>
+  );
+};
 
 /**
  * SubMenuItem is a drawer item that is displayed by a MenuItem
@@ -77,12 +118,23 @@ const MenuItem = ({ name, customIcon, redirectLink, children }) => (
  * @returns {JSX.Element}
  * @constructor
  */
-const SubMenuItem = ({ name, customIcon, redirectLink }) => (
-  <>
-    {customIcon && <customIcon />}
-    <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">{name}</h1>
-  </>
-);
+const SubMenuItem = ({ name, customIcon, redirectLink }) => {
+  // ------------------ Misc -----------------
+  const router = useRouter();
+
+  // ------------------ Handles -----------------
+  const redirect = () => {
+    router.push(redirectLink);
+  };
+
+  // ------------------ Return -----------------
+  return (
+    <div role="presentation" onClick={redirect}>
+      {customIcon && <customIcon />}
+      <h1 className="cursor-pointer p-2 hover:bg-blue-600 rounded-md mt-1">{name}</h1>
+    </div>
+  );
+};
 
 /**
  * The ExpandedHamburgerMenu is the menu that is shown when the HamburgerButtonIcon is clicked
@@ -138,25 +190,22 @@ const SidebarSearch = () => (
  * @returns {JSX.Element}
  * @constructor
  */
-const HamburgerMenu = () => {
+const HamburgerMenu = () => (
   // ------------------ useStates -----------------
-  const [open, setOpen] = useState(false);
+  // const [open, setOpen] = useState(false);
 
   // ------------------ Return -----------------
-  return (
-    <div>
-      <body className="bg-blue-600">
-        <span className="absolute text-white text-4xl top-5 left-4 cursor-pointer">
-          <i className="bi bi-filter-left px-2 bg-gray-900 rounded-md" />
-        </span>
-        <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900">
-          <LayoutEditor />
-        </div>
-      </body>
-    </div>
-  );
-};
-
+  <div>
+    <body className="bg-blue-600">
+      <span className="absolute text-white text-4xl top-5 left-4 cursor-pointer">
+        <i className="bi bi-filter-left px-2 bg-gray-900 rounded-md" />
+      </span>
+      <div className="sidebar fixed top-0 bottom-0 lg:left-0 p-2 w-[300px] overflow-y-auto text-center bg-gray-900">
+        <LayoutEditor />
+      </div>
+    </body>
+  </div>
+);
 // ------------------ Export ------------------
 export default HamburgerMenu;
 
