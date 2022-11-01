5697218de88fb3f47aa5418845f070fb6e49e09f
Kaung Khant Zaw <mmmike2002@gmail.com>
2022-11-01 12:25:30 +0800

    feat: :sparkles: add admin NavBar
    NavBar for Admin

---
+++ apps/b/components/NavBar.jsx
@@ -0,0 +1,33 @@
+import Image from 'next/image';
+
+const NavBar = () => (
+  <div className="navbar bg-base-100 rounded-lg shadow-lg">
+    <div className="flex-1" />
+    <div className="block border-l border-gray-300 px-4 text-right">
+      <p>System Adminstrator</p>
+      <p className="text-sm font-thin">Admin</p>
+    </div>
+    <div className="dropdown dropdown-end">
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control, jsx-a11y/no-noninteractive-tabindex */}
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+        <div className="w-10 rounded-full">
+          <Image src="https://placeimg.com/80/80/people" />
+        </div>
+      </label>
+      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
+      <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+        <li>
+          <a href="/">Profile</a>
+        </li>
+        <li>
+          <a href="/">Settings</a>
+        </li>
+        <li>
+          <a href="/">Logout</a>
+        </li>
+      </ul>
+    </div>
+  </div>
+);
+
+export default NavBar;
