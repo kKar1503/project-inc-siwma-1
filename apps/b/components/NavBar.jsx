acfdd3b139b1831ad46c29416286184409763580
Kaung Khant Zaw <mmmike2002@gmail.com>
2022-11-01 12:25:30 +0800

    feat: :sparkles: add admin NavBar
    NavBar for Admin

--- apps/b/components/NavBar.jsx
+++ apps/b/components/NavBar.jsx
@@ -1,47 +1,29 @@
 import Image from 'next/image';
-import Link from 'next/link';
 
 const NavBar = () => (
   <div className="navbar bg-base-100 rounded-lg shadow-lg">
-    <div className="flex-none lg:hidden">
-      <label htmlFor="sidebar-drawer" className="btn btn-square btn-ghost">
-        <svg
-          xmlns="http://www.w3.org/2000/svg"
-          fill="none"
-          viewBox="0 0 24 24"
-          className="inline-block w-5 h-5 stroke-current"
-        >
-          <path
-            strokeLinecap="round"
-            strokeLinejoin="round"
-            strokeWidth="2"
-            d="M4 6h16M4 12h16M4 18h16"
-          />
-        </svg>
-      </label>
-    </div>
     <div className="flex-1" />
-    <div className="block border-l border-gray-300 px-4 text-right leading-3">
+    <div className="block border-l border-gray-300 px-4 text-right">
       <p>System Adminstrator</p>
       <p className="text-sm font-thin">Admin</p>
     </div>
     <div className="dropdown dropdown-end">
-      {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
-      <div tabIndex={0} className="btn btn-ghost btn-circle avatar">
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control, jsx-a11y/no-noninteractive-tabindex */}
+      <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
         <div className="w-10 rounded-full">
-          <Image src="/../public/sample-profile-pic.jpg" alt="profile-pic" width={80} height={80} />
+          <Image src="https://placeimg.com/80/80/people" />
         </div>
-      </div>
+      </label>
       {/* eslint-disable-next-line jsx-a11y/no-noninteractive-tabindex */}
       <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
         <li>
-          <Link href="/">Profile</Link>
+          <a href="/">Profile</a>
         </li>
         <li>
-          <Link href="/">Settings</Link>
+          <a href="/">Settings</a>
         </li>
         <li>
-          <Link href="/">Logout</Link>
+          <a href="/">Logout</a>
         </li>
       </ul>
     </div>
