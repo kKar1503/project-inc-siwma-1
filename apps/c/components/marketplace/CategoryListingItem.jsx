52b11495f7d1ef7cd8b1701ff7bb205f5a4aba32
Nabil Ridhwan <nabridhwan@gmail.com>
2022-11-01 12:46:54 +0800

    feat: :sparkles: fixed text overlay ontop of category image

--- apps/c/components/marketplace/CategoryListingItem.jsx
+++ apps/c/components/marketplace/CategoryListingItem.jsx
@@ -2,13 +2,18 @@
 import PropTypes from 'prop-types';
 
 const CategoryListingItem = ({ img, name, href }) => (
-  <div className="card">
+  <div className="card relative">
     <a href={href}>
-      {/* TODO: Implement image and text overlap on top of each other (use relative positioning) */}
       <picture>
+        {/* Actual image */}
         <img src={img} alt={name} className="aspect-square object-cover h-[150px]" />
+
+        {/* Overlay gradient */}
+        <div className="bg-gradient-to-b from-transparent to-black/40 w-full h-full z-10 absolute top-0 left-0" />
       </picture>
-      <p>{name}</p>
+
+      {/* Category text */}
+      <p className="absolute z-10 bottom-0 text-center w-full mb-3 text-white">{name}</p>
     </a>
   </div>
 );
