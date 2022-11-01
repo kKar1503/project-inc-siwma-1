27aeacc71ef0a8e870c4378890ee42905833e6b4
Amos Choo <tohmenchoo@gmail.com>
2022-11-01 12:59:46 +0800

    style: styling
    removed some unused styles and fixed breakpoints

--- apps/d/pages/analyticCharts.jsx
+++ apps/d/pages/analyticCharts.jsx
@@ -10,7 +10,7 @@ import {
 const AnalyticCharts = () => (
   <div className="grid place-content-center">
     <h2 className="my-2 text-2xl font-semibold ">Company</h2>
-    <div className="my-5 flex flex-wrap max-md:flex-nowrap overflow-auto flex-direction-row gap-8">
+    <div className="my-5 flex flex-wrap md:flex-nowrap overflow-auto gap-8">
       <div className="card w-96 bg-base-100 shadow-md shrink-0">
         <MostPostsChart />
       </div>
@@ -22,7 +22,7 @@ const AnalyticCharts = () => (
       </div>
     </div>
     <h2 className="my-2 text-2xl font-semibold ">Settlements</h2>
-    <div className="my-5 flex flex-wrap max-md:flex-nowrap overflow-auto flex-direction-row gap-8">
+    <div className="my-5 flex flex-wrap md:flex-nowrap overflow-auto  gap-8">
       <div className="card w-96 bg-base-100 shadow-md shrink-0">
         <BestSellingSubCategoriesChart />
       </div>
@@ -35,5 +35,36 @@ const AnalyticCharts = () => (
     </div>
   </div>
 );
+
+// const AnalyticCharts = () => (
+//   <div className="flex flex-col items-center ">
+//     <div className="">
+//       <h2 className="my-2 text-2xl font-semibold ">Company</h2>
+//     </div>
+//     <div className="w-full my-5 flex flex-col items-center justify-center sm:flex-row overflow-auto flex-direction-row gap-8 ">
+//       <div className="card w-96 bg-base-100 shadow-md shrink-0">
+//         <MostPostsChart />
+//       </div>
+//       <div className="card w-96 bg-base-100 shadow-md shrink-0">
+//         <MostBuyAndSellChart />
+//       </div>
+//       <div className="card w-96 bg-base-100 shadow-md shrink-0">
+//         <NumberOfBuyAndSellChart />
+//       </div>
+//     </div>
+//     <h2 className="my-2 text-2xl font-semibold ">Settlements</h2>
+//     <div className="my-5 w-full flex flex-col justify-center sm:flex-row overflow-auto flex-direction-row gap-8">
+//       <div className="card w-96 bg-base-100 shadow-md shrink-0">
+//         <BestSellingSubCategoriesChart />
+//       </div>
+//       <div className="card w-96 bg-base-100 shadow-md shrink-0">
+//         <MostCommonCategoryChart />
+//       </div>
+//       <div className="card w-96 bg-base-100 shadow-md shrink-0">
+//         <TopSellingItemsChart />
+//       </div>
+//     </div>
+//   </div>
+// );
 
 export default AnalyticCharts;
