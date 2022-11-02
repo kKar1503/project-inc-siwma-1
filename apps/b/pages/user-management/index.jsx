8f19d325901eec08368eac134bd96f33191c9b91
Bengco Rance <Thinkaze@gmail.com>
2022-11-02 16:44:44 +0800

    feat: :sparkles: added overview page for user management view in admin dashboard (WIP)

--- apps/b/pages/user-management/index.jsx
+++ apps/b/pages/user-management/index.jsx
@@ -1,11 +1,9 @@
-/* eslint-disable no-plusplus */
 import Head from 'next/head';
 import AdminFigure from '../../components/AdminFigure';
-import AdminPageLayout from '../../components/layouts/AdminPageLayout';
+import Sidebar, { adminSidebar } from '../../components/Sidebar/Sidebar';
 import SearchBar from '../../components/SearchBar';
-import NavBar from '../../components/NavBar';
 
-const UserOverview = () => (
+const Home = () => (
   <div className="bg-[#FAFAFA]">
     <Head>
       <title>Team B</title>
@@ -13,164 +11,116 @@ const UserOverview = () => (
       <link rel="icon" href="/favicon.ico" />
     </Head>
     <main>
-      <div className="flex flex-col w-full h-full gap-8 p-8">
-        {/* { replace this with sidebar } */}
-        <NavBar />
+      <Sidebar sidebarList={adminSidebar} selected="Users">
+        <div className="flex flex-col w-full h-full overflow-auto gap-8 p-8">
+          <div className="flex flex-col h-full border gap-8">
+            {/* { replace this with sidebar } */}
+            <div className="bg-base-100 border w-full h-20 shadow-lg rounded-2xl" />
 
-        <div className="flex flex-row gap-8 flex-wrap">
-          <AdminFigure title="Active Companies" value="10" color="text-primary" />
-          <AdminFigure title="Total Users" value="14" color="text-accent" />
-          <AdminFigure title="Pending Invites" value="15" color="text-secondary" />
-        </div>
-
-        <div className="flex flex-row flex-wrap gap-8">
-          <div className="flex flex-[7]">
-            <div className="bg-base-100 w-full h-fit shadow-lg rounded-2xl">
-              <div className="flex flex-row justify-between items-center gap-8 p-6">
-                <div className="flex flex-col">
-                  <h1 className="text-xl font-bold">Registered Users</h1>
-                  <p>Showing 1-5 of 14 users</p>
-                </div>
-                <div className="flex flex-col gap-4">
-                  <SearchBar placeholder="Search by e-mail" />
-                </div>
-              </div>
-              <div className="w-full h-px bg-base-300" />
-              {/* replace with div component */}
-              <div className="flex flex-col justify-between items-center">
-                <div className="flex flex-row justify-between h-8 bg-accent items-center gap-8 px-8 w-full">
-                  <div className="flex flex-row items-center">
-                    <div className="h-px w-20" />
-                    <p className="text-base-100 text-base">User</p>
-                  </div>
-                  <p className="text-base-100">E-mail</p>
-                  <p className="text-base-100">Company</p>
-                  <p className="text-base-100">Mobile Number</p>
-                  <p className="text-base-100">Actions</p>
-                </div>
-                <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full hover:bg-base-200">
-                  <div className="flex flex-row items-center">
-                    <div className="h-9 w-9 mr-11 rounded-full bg-base-300" />
-                    <p>Wo Shao</p>
-                  </div>
-                  <p>WoShao@gmail.com</p>
-                  <p>LI FANG IRONWORKS</p>
-                  <p>+65 9854 7482</p>
-                  <p className="mr-4">O O O</p>
-                </div>
-                <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full hover:bg-base-200">
-                  <div className="flex flex-row items-center">
-                    <div className="h-9 w-9 mr-11 rounded-full bg-base-300" />
-                    <p>Wo Shao</p>
-                  </div>
-                  <p>WoShao@gmail.com</p>
-                  <p>LI FANG IRONWORKS</p>
-                  <p>+65 9854 7482</p>
-                  <p className="mr-4">O O O</p>
-                </div>
-                <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full hover:bg-base-200">
-                  <div className="flex flex-row items-center">
-                    <div className="h-9 w-9 mr-11 rounded-full bg-base-300" />
-                    <p>Wo Shao</p>
-                  </div>
-                  <p>WoShao@gmail.com</p>
-                  <p>LI FANG IRONWORKS</p>
-                  <p>+65 9854 7482</p>
-                  <p className="mr-4">O O O</p>
-                </div>
-                <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full hover:bg-base-200">
-                  <div className="flex flex-row items-center">
-                    <div className="h-9 w-9 mr-11 rounded-full bg-base-300" />
-                    <p>Wo Shao</p>
-                  </div>
-                  <p>WoShao@gmail.com</p>
-                  <p>LI FANG IRONWORKS</p>
-                  <p>+65 9854 7482</p>
-                  <p className="mr-4">O O O</p>
-                </div>
-                <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full hover:bg-base-200">
-                  <div className="flex flex-row items-center">
-                    <div className="h-9 w-9 mr-11 rounded-full bg-base-300" />
-                    <p>Wo Shao</p>
-                  </div>
-                  <p>WoShao@gmail.com</p>
-                  <p>LI FANG IRONWORKS</p>
-                  <p>+65 9854 7482</p>
-                  <p className="mr-4">O O O</p>
-                </div>
-              </div>
-              <div className="w-full h-px bg-base-300" />
-              <div className="flex flex-row justify-end gap-8 p-6">
-                <div className="btn-group">
-                  <button className="btn bg-accent border-accent">1</button>
-                  <button className="btn">2</button>
-                  <button className="btn">3</button>
-                </div>
-              </div>
+            <div className="flex flex-row gap-8">
+              <AdminFigure title="Registered Companies" value="1,234" color="text-primary" />
+              <AdminFigure title="Total Users" value="4,568" color="text-accent" />
+              <AdminFigure title="Pending Invites" value="5,983" color="text-secondary" />
             </div>
-          </div>
 
-          <div className="flex flex-[5] w-full grow-none">
-            <div className="bg-base-100 w-full h-fit shadow-lg rounded-2xl">
-              <div className="flex flex-row justify-between items-center gap-8 p-6">
-                <div className="flex flex-col">
-                  <h1 className="text-xl font-bold">Pending Invites</h1>
-                  <p>Showing 1-5 of 15 invites</p>
+            <div className="flex flex-row gap-8">
+              <div className="flex w-7/12">
+                <div className="bg-base-100 w-full h-fit shadow-lg rounded-2xl">
+                  <div className="flex flex-row justify-between items-center gap-8 p-6">
+                    <div className="flex flex-col">
+                      <h1 className="text-xl font-bold">Registered Users</h1>
+                      <p>Showing 1-8 of 22 users</p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                      <SearchBar placeholder="Search by e-mail" />
+                    </div>
+                  </div>
+                  <div className="w-full h-px bg-base-300" />
+                  {/* replace with table component */}
+                  <div className="flex flex-col justify-between items-center">
+                    <div className="flex flex-row justify-between h-8 bg-secondary items-center gap-8 px-8 w-full">
+                      <div className="flex flex-row items-center">
+                        <div className="h-px w-20" />
+                        <p className="text-base-100">User</p>
+                      </div>
+                      <p className="text-base-100">E-mail</p>
+                      <p className="text-base-100">Company</p>
+                      <p className="text-base-100">Mobile Number</p>
+                      <p className="text-base-100">Actions</p>
+                    </div>
+                    <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full">
+                      <div className="flex flex-row items-center">
+                        <div className="h-9 w-9 mr-11 rounded-full bg-base-300" />
+                        <p>AHH</p>
+                      </div>
+                      <p>AHH</p>
+                      <p>AHH</p>
+                      <p>AHH</p>
+                      <p className="mr-4">AHH</p>
+                    </div>
+                  </div>
+                  <div className="w-full h-px bg-base-300" />
+                  <div className="flex flex-row justify-end gap-8 p-6">
+                    <div className="btn-group">
+                      <button className="btn bg-secondary border-secondary">1</button>
+                      <button className="btn">2</button>
+                      <button className="btn">3</button>
+                    </div>
+                  </div>
                 </div>
-                <div className="flex flex-col gap-4">
-                  <SearchBar placeholder="Search by e-mail" />
-                </div>
               </div>
-              <div className="w-full h-px bg-base-300" />
-              {/* replace with div component */}
-              <div className="flex flex-col justify-between items-center">
-                <div className="flex flex-row justify-between h-8 bg-secondary items-center gap-8 px-8 w-full">
-                  <p className="text-base-100">Company</p>
-                  <p className="text-base-100">E-mail</p>
-                  <p className="text-base-100">Actions</p>
-                </div>
-              </div>
-              <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full hover:bg-base-200">
-                <p>LI FANG IRONWORKS</p>
-                <p>WoShao@gmail.com</p>
-                <p className="mr-4">O O O</p>
-              </div>
-              <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full hover:bg-base-200">
-                <p>LI FANG IRONWORKS</p>
-                <p>WoShao@gmail.com</p>
-                <p className="mr-4">O O O</p>
-              </div>
-              <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full hover:bg-base-200">
-                <p>LI FANG IRONWORKS</p>
-                <p>WoShao@gmail.com</p>
-                <p className="mr-4">O O O</p>
-              </div>
-              <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full hover:bg-base-200">
-                <p>LI FANG IRONWORKS</p>
-                <p>WoShao@gmail.com</p>
-                <p className="mr-4">O O O</p>
-              </div>
-              <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full hover:bg-base-200">
-                <p>LI FANG IRONWORKS</p>
-                <p>WoShao@gmail.com</p>
-                <p className="mr-4">O O O</p>
-              </div>
-              <div className="w-full h-px bg-base-300" />
-              <div className="flex flex-row justify-end gap-8 p-6">
-                <div className="btn-group">
-                  <button className="btn bg-secondary border-secondary">1</button>
-                  <button className="btn">2</button>
-                  <button className="btn">3</button>
+
+              <div className="flex w-5/12">
+                <div className="bg-base-100 w-full h-fit shadow-lg rounded-2xl">
+                  <div className="flex flex-row justify-between items-center gap-8 p-6">
+                    <div className="flex flex-col">
+                      <h1 className="text-xl font-bold">Registered Users</h1>
+                      <p>Showing 1-8 of 22 users</p>
+                    </div>
+                    <div className="flex flex-col gap-4">
+                      <SearchBar placeholder="Search by e-mail" />
+                    </div>
+                  </div>
+                  <div className="w-full h-px bg-base-300" />
+                  {/* replace with table component */}
+                  <div className="flex flex-col justify-between items-center">
+                    <div className="flex flex-row justify-between h-8 bg-secondary items-center gap-8 px-8 w-full">
+                      <div className="flex flex-row items-center">
+                        <div className="h-px w-20" />
+                        <p className="text-base-100">User</p>
+                      </div>
+                      <p className="text-base-100">E-mail</p>
+                      <p className="text-base-100">Company</p>
+                      <p className="text-base-100">Mobile Number</p>
+                      <p className="text-base-100">Actions</p>
+                    </div>
+                    <div className="flex flex-row justify-between h-16 items-center gap-8 px-8 w-full">
+                      <div className="flex flex-row items-center">
+                        <div className="h-9 w-9 mr-11 rounded-full bg-base-300" />
+                        <p>AHH</p>
+                      </div>
+                      <p>AHH</p>
+                      <p>AHH</p>
+                      <p>AHH</p>
+                      <p className="mr-4">AHH</p>
+                    </div>
+                  </div>
+                  <div className="w-full h-px bg-base-300" />
+                  <div className="flex flex-row justify-end gap-8 p-6">
+                    <div className="btn-group">
+                      <button className="btn bg-secondary border-secondary">1</button>
+                      <button className="btn">2</button>
+                      <button className="btn">3</button>
+                    </div>
+                  </div>
                 </div>
               </div>
             </div>
           </div>
         </div>
-      </div>
+      </Sidebar>
     </main>
   </div>
 );
 
-UserOverview.getLayout = (page) => <AdminPageLayout>{page}</AdminPageLayout>;
-
-export default UserOverview;
+export default Home;
