b1054ceda0c87266cdae8eb8a315270e4beeb45a
Bengco Rance <Thinkaze@gmail.com>
2022-11-02 17:50:58 +0800

    feat: working layout added

--- apps/b/pages/user-management/index.jsx
+++ apps/b/pages/user-management/index.jsx
@@ -1,3 +1,4 @@
+/* eslint-disable no-plusplus */
 import Head from 'next/head';
 import AdminFigure from '../../components/AdminFigure';
 import Sidebar, { adminSidebar } from '../../components/Sidebar/Sidebar';
