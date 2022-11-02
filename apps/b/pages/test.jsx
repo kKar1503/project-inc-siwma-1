62f2b2311d3423be01ee60c4e0861de5ede0deb3
Kaung Khant Zaw <mmmike2002@gmail.com>
2022-11-02 15:01:17 +0800

    feat: :sparkles: company register form
    company register modal created with daisy ui modal

---
+++ apps/b/pages/test.jsx
@@ -0,0 +1,12 @@
+import CompanyInvite from '../components/Forms/CompanyInvite';
+
+const test = () => (
+  <div>
+    <label htmlFor="company-invite" className="btn">
+      Open
+    </label>
+    <CompanyInvite />
+  </div>
+);
+
+export default test;
