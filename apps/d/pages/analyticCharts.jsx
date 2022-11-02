2076272e166689583024b03e0c8733a466a09ebe
Amos Choo <tohmenchoo@gmail.com>
2022-11-02 21:11:06 +0800

    style: fixed styling of page
    centered items and made the cards fully visible when overflow

--- apps/d/pages/analyticCharts.jsx
+++ apps/d/pages/analyticCharts.jsx
@@ -8,29 +8,33 @@ import {
 } from '@inc/charts';
 
 const AnalyticCharts = () => (
-  <div className="grid place-content-center">
-    <h2 className="my-2 text-2xl font-semibold ">Company</h2>
-    <div className="my-5 flex flex-wrap md:flex-nowrap overflow-auto gap-8">
-      <div className="card w-96 bg-base-100 shadow-md shrink-0">
-        <MostPostsChart />
-      </div>
-      <div className="card w-96 bg-base-100 shadow-md shrink-0">
-        <MostBuyAndSellChart />
-      </div>
-      <div className="card w-96 bg-base-100 shadow-md shrink-0">
-        <NumberOfBuyAndSellChart />
+  <div className="grid place-content-center mx-5">
+    <h2 className="my-2 text-2xl font-semibold">Company</h2>
+    <div className="overflow-auto">
+      <div className="my-5 flex flex-wrap md:flex-nowrap gap-8 place-content-center md:place-content-start">
+        <div className="card w-96 bg-base-100 shadow-md shrink-0">
+          <MostPostsChart />
+        </div>
+        <div className="card w-96 bg-base-100 shadow-md shrink-0">
+          <MostBuyAndSellChart />
+        </div>
+        <div className="card w-96 bg-base-100 shadow-md shrink-0">
+          <NumberOfBuyAndSellChart />
+        </div>
       </div>
     </div>
-    <h2 className="my-2 text-2xl font-semibold ">Settlements</h2>
-    <div className="my-5 flex flex-wrap md:flex-nowrap overflow-auto  gap-8">
-      <div className="card w-96 bg-base-100 shadow-md shrink-0">
-        <BestSellingSubCategoriesChart />
-      </div>
-      <div className="card w-96 bg-base-100 shadow-md shrink-0">
-        <MostCommonCategoryChart />
-      </div>
-      <div className="card w-96 bg-base-100 shadow-md shrink-0">
-        <TopSellingItemsChart />
+    <h2 className="my-2 text-2xl font-semibold">Settlements</h2>
+    <div className="overflow-auto">
+      <div className="my-5 flex flex-wrap md:flex-nowrap gap-8 place-content-center md:place-content-start">
+        <div className="card w-96 bg-base-100 shadow-md shrink-0">
+          <BestSellingSubCategoriesChart />
+        </div>
+        <div className="card w-96 bg-base-100 shadow-md shrink-0">
+          <MostCommonCategoryChart />
+        </div>
+        <div className="card w-96 bg-base-100 shadow-md shrink-0">
+          <TopSellingItemsChart />
+        </div>
       </div>
     </div>
   </div>
