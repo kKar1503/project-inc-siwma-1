0f67f6812393a9e07999f5e49e0cdd0f574215e0
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2022-11-02 20:18:24 +0800

    feat: added TableButton

---
+++ apps/b/components/Tables/TableButton.jsx
@@ -0,0 +1,25 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import cx from 'classnames';
+
+/* selectedColor is the color of the button when it is selected.
+When it is not selected, it is black.
+*/
+
+const TableButton = ({ index, selectedIndex, setSelectedIndex, selectedColor }) => (
+  <button
+    className={cx('px-4 py-2 text-white', index === selectedIndex ? selectedColor : 'bg-black')}
+    onClick={() => setSelectedIndex(index)}
+  >
+    {index + 1}
+  </button>
+);
+
+TableButton.propTypes = {
+  index: PropTypes.number,
+  selectedIndex: PropTypes.number,
+  setSelectedIndex: PropTypes.func,
+  selectedColor: PropTypes.string,
+};
+
+export default TableButton;
