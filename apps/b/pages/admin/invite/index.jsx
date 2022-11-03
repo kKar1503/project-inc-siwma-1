84997d8b071110c2c285b08f31e53b667ddee838
Garlicvideos <Garlicvideos@gmail.com>
2022-11-03 15:27:37 +0800

    feat: :lipstick: replace placeholder tables with the table component

--- apps/b/pages/admin/invite/index.jsx
+++ apps/b/pages/admin/invite/index.jsx
@@ -1,45 +1,35 @@
 import FileUpload from '../../../components/FileUpload';
 import AdminPageLayout from '../../../components/layouts/AdminPageLayout';
 import NavBar from '../../../components/NavBar';
+import CompanyProfilesPreviewTable from '../../../components/Tables/CompanyProfilesPreviewTable';
+import UserInvitesPreviewTable from '../../../components/Tables/UserInvitesPreviewTable';
 
-// eslint-disable-next-line react/prop-types
-const TestTable = ({ className }) => (
-  <div className={className}>
-    <div className="overflow-x-auto">
-      <table className="table w-full">
-        <thead>
-          <tr>
-            {/* eslint-disable-next-line jsx-a11y/control-has-associated-label */}
-            <th />
-            <th>Name</th>
-            <th>Job</th>
-            <th>Favorite Color</th>
-          </tr>
-        </thead>
-        <tbody>
-          <tr>
-            <th>1</th>
-            <td>Cy Ganderton</td>
-            <td>Quality Control Specialist</td>
-            <td>Blue</td>
-          </tr>
-          <tr>
-            <th>2</th>
-            <td>Hart Hagerty</td>
-            <td>Desktop Support Technician</td>
-            <td>Purple</td>
-          </tr>
-          <tr>
-            <th>3</th>
-            <td>Brice Swyre</td>
-            <td>Tax Accountant</td>
-            <td>Red</td>
-          </tr>
-        </tbody>
-      </table>
-    </div>
-  </div>
-);
+/**
+ * Sample data
+ */
+const userTableData = (id) => ({
+  id,
+  company: 'GUANGZHAO METALWORKS, PTE. LTD',
+  email: 'sallyknox_slfi@gmail.com',
+  mobileNumber: '+65 9832 0293',
+});
+
+const companyTableData = (id) => ({
+  id,
+  company: 'GUANGZHAO METALWORKS, PTE. LTD',
+  website: 'www.guangzhaomw.com',
+});
+
+function populateArray(element, count) {
+  const result = [];
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < count; i++) {
+    result[i] = element(i + 1);
+  }
+
+  return result;
+}
 
 const InvitesPage = () => (
   <div className="flex flex-col flex-1 w-full p-8 gap-8 overflow-auto">
@@ -47,8 +37,12 @@ const InvitesPage = () => (
     <div className="flex flex-col lg:grid lg:grid-flow-row grid-cols-12 gap-8 flex-1">
       <FileUpload className="h-full col-span-4" />
       <div className="flex flex-col col-start-5 col-end-13 gap-8 flex-1 justify-between">
-        <TestTable className="shadow-lg flex-1" />
-        <TestTable className="shadow-lg flex-1" />
+        <div className="bg-base-100 rounded-lg shadow-lg max-h-80 overflow-y-auto">
+          <CompanyProfilesPreviewTable data={populateArray(companyTableData, 15)} />
+        </div>
+        <div className="bg-base-100 rounded-lg shadow-lg max-h-80 overflow-y-auto">
+          <UserInvitesPreviewTable data={populateArray(userTableData, 30)} />
+        </div>
         <div className="flex justify-end">
           <button className="btn btn-primary rounded-lg btn-wide">Confirm</button>
         </div>
