ea7e4c16c6561594bc0e7e4d6e36e13cb0793b78
Eravar <pinghowng@hotmail.com>
2022-11-03 15:45:47 +0800

    chore: fill data

--- apps/b/pages/companies/index.jsx
+++ apps/b/pages/companies/index.jsx
@@ -42,16 +42,58 @@ const registeredCompaniesData = [
     website: 'veryverylongname@veryverylongdomain.com',
     bio: 'Very Very Long Company Bio',
   },
+  {
+    id: 3,
+    profilePicture: pic,
+    company: 'Very Very Long Company Name',
+    website: 'veryverylongname@veryverylongdomain.com',
+    bio: 'Very Very Long Company Bio',
+  },
+  {
+    id: 4,
+    profilePicture: pic,
+    company: 'Very Very Long Company Name',
+    website: 'veryverylongname@veryverylongdomain.com',
+    bio: 'Very Very Long Company Bio',
+  },
+  {
+    id: 5,
+    profilePicture: pic,
+    company: 'Very Very Long Company Name',
+    website: 'veryverylongname@veryverylongdomain.com',
+    bio: 'Very Very Long Company Bio',
+  },
+  {
+    id: 6,
+    profilePicture: pic,
+    company: 'Very Very Long Company Name',
+    website: 'veryverylongname@veryverylongdomain.com',
+    bio: 'Very Very Long Company Bio',
+  },
+  {
+    id: 7,
+    profilePicture: pic,
+    company: 'Very Very Long Company Name',
+    website: 'veryverylongname@veryverylongdomain.com',
+    bio: 'Very Very Long Company Bio',
+  },
+  {
+    id: 8,
+    profilePicture: pic,
+    company: 'Very Very Long Company Name',
+    website: 'veryverylongname@veryverylongdomain.com',
+    bio: 'Very Very Long Company Bio',
+  },
 ];
 const Page = () => (
   <div className="flex flex-col flex-1 w-full p-8 gap-8 overflow-auto">
     <NavBar />
     <div className="flex mb-4 gap-4">
       <div className="flex flex-row w-full border-solid border-2 border-gray h-full">
-        <FileUpload />
+        <FileUpload className="h-full col-span-4" />
       </div>
-      <div className="w-2/3 max-w-fit max-h-fit flex flex-row">
-        <RegisteredCompaniesTable data={registeredCompaniesData} />
+      <div className="w-2/3 w-full h-full max-w-fit max-h-fit flex flex-row">
+        <RegisteredCompaniesTable data={registeredCompaniesData} className="shadow-lg" />
       </div>
     </div>
   </div>
