e4cb4f3f4a3a4f59e2b4e94030661f906458886c
notlega <onghinjourn@gmail.com>
2022-11-03 15:55:52 +0800

    fix: :lipstick: updates styling for layout

--- apps/c/pages/buySellListing.jsx
+++ apps/c/pages/buySellListing.jsx
@@ -15,15 +15,8 @@ const BuySellListing = () => {
       </Head>
 
       <main>
-        {/* temp navbar */}
-        <div className="navbar bg-secondary">
-          <button className="btn btn-ghost normal-case text-xl bg-slate-200 rounded-2xl">
-            SIWMA
-          </button>
-        </div>
-
-        <div className="flex justify-around mt-8">
-          <div className="flex space-y-6 flex-col">
+        <div className="flex justify-around mt-8 mx-32">
+          <div className="flex space-y-6 flex-col w-2/6">
             <CardBackground>
               <label className="flex justify-center w-full h-40 px-4 transition bg-white border-2 border-accent border-dashed rounded-md appearance-none cursor-pointer hover:border-accent-focus focus:outline-none">
                 <span className="flex items-center space-x-4">
@@ -62,7 +55,7 @@ const BuySellListing = () => {
               <Input text="Height" />
             </CardBackground>
           </div>
-          <div className="flex flex-col">
+          <div className="flex flex-col w-3/5">
             <CardBackground>
               <ListingForm />
             </CardBackground>
