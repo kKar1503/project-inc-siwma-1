344e2d1703a3493aa14ed896f7362e516a14793b
MrsDarth <7jgbm234@gmail.com>
2022-11-03 18:25:32 +0800

    build: advertisement page layout

--- packages/charts/components/advertisments/ClickDistribution.jsx
+++ packages/charts/components/advertisments/ClickDistribution.jsx
@@ -11,33 +11,33 @@ const dataset = [
 ];
 
 const ClickDistribution = () => (
-  <VictoryContainer width={435} height={290}>
+  <VictoryContainer width={600} height={300}>
     <VictoryPie
-      padding={{ left: 215, bottom: 100 }}
-      radius={100}
+      padding={{ left: 500, bottom: 100 }}
+      radius={120}
       standalone={false}
       data={dataset}
       x="cat"
       y="post"
       colorScale={colors}
-      innerRadius={120}
-      labelRadius={100}
+      innerRadius={140}
+      labelRadius={120}
       style={{ labels: { fontSize: 0 } }}
     />
     <VictoryLabel
       textAnchor="middle"
       style={{ fontSize: 110, fontWeight: 'bold' }}
-      x={310}
+      x={450}
       y={140}
       text="25"
     />
-    <VictoryLabel textAnchor="middle" style={{ fontSize: 30 }} x={310} y={200} text="Clicks" />
+    <VictoryLabel textAnchor="middle" style={{ fontSize: 30 }} x={450} y={200} text="Clicks" />
     <VictoryLegend
       standalone={false}
       itemsPerRow={5}
       gutter={80}
-      x={0}
-      y={70}
+      x={20}
+      y={50}
       colorScale={colors}
       data={dataset.map(({ cat }) => ({ name: cat, symbol: { type: 'square' } }))}
       style={{
