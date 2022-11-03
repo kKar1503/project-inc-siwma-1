5f7aed8d3fbb4be3b1808fd712c91fe4d44e9361
notlega <onghinjourn@gmail.com>
2022-11-03 15:55:52 +0800

    fix: :lipstick: updates styling for layout

--- apps/c/components/CardBackground.jsx
+++ apps/c/components/CardBackground.jsx
@@ -2,7 +2,7 @@ import React from 'react';
 import PropTypes from 'prop-types';
 
 const CardBackground = ({ children }) => (
-  <div className="card w-auto border-2 bg-neutral-content border-red-500 shadow-xl">
+  <div className="card w-auto bg-neutral-content shadow-xl">
     <div className="card-body">{children}</div>
   </div>
 );
