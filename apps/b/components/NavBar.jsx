bd8fd734b37e3f1629fc7db7e6a2063a8c345cdd
Eravar <pinghowng@hotmail.com>
2022-11-03 15:35:49 +0800

    chore: add navbar

--- apps/b/components/NavBar.jsx
+++ apps/b/components/NavBar.jsx
@@ -3,6 +3,23 @@ import Link from 'next/link';
 
 const NavBar = () => (
   <div className="navbar bg-base-100 rounded-lg shadow-lg">
+    <div className="flex-none lg:hidden">
+      <label htmlFor="sidebar-drawer" className="btn btn-square btn-ghost">
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="inline-block w-5 h-5 stroke-current"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M4 6h16M4 12h16M4 18h16"
+          />
+        </svg>
+      </label>
+    </div>
     <div className="flex-1" />
     <div className="block border-l border-gray-300 px-4 text-right leading-3">
       <p>System Adminstrator</p>
