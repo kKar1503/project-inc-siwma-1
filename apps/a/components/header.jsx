2d0fd3d352fd451b85621215b9113f9348789f2c
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-11-03 19:23:22 +0800

    build: :green_heart: update pnpm -lock

--- apps/a/components/header.jsx
+++ apps/a/components/header.jsx
@@ -95,7 +95,7 @@ const Header = () => (
       <div className="dropdown dropdown-end">
         <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
           <div className="w-10 rounded-full">
-            <img src="https://placeimg.com/80/80/people" alt="Person" />
+            <Image src="https://placeimg.com/80/80/people" alt="Person" />
           </div>
         </label>
         <ul
