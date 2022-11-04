47d27e61756c59205443c71e29f0b0543a734d92
Eravar <pinghowng@hotmail.com>
2022-11-04 13:15:37 +0800

    feat: companies management page

---
+++ apps/b/components/Forms/BulkInvite.jsx
@@ -0,0 +1,21 @@
+import FileUpload from '../FileUpload';
+
+const BulkInvite = () => (
+  <div>
+    <input type="checkbox" id="bulk-invite" className="modal-toggle" />
+    <label htmlFor="bulk-invite" className="modal cursor-pointer">
+      {/* eslint-disable-next-line jsx-a11y/label-has-associated-control */}
+      <label htmlFor="" className="modal-box rounded-xl max-w-4xl">
+        <label
+          htmlFor="bulk-invite"
+          className="text-lg absolute right-4 top-2 hover:cursor-pointer"
+        >
+          ✕
+        </label>
+        <FileUpload />
+      </label>
+    </label>
+  </div>
+);
+
+export default BulkInvite;
