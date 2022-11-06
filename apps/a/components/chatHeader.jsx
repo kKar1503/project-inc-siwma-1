ffa97e12220d57bbecd7e6b43f4f986f2e6fd472
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-11-06 16:44:23 +0800

    fix: :bug: fixing errors that caused checks to fail

--- apps/a/components/chatHeader.jsx
+++ apps/a/components/chatHeader.jsx
@@ -1,4 +1,6 @@
-import { BsThreeDotsVertical } from 'react-icons/Bs';
+/* eslint-disable jsx-a11y/no-noninteractive-tabindex */
+/* eslint-disable jsx-a11y/label-has-associated-control */
+import { BsThreeDotsVertical } from 'react-icons/bs';
 import Image from 'next/image';
 import Link from 'next/link';
 
@@ -6,72 +8,62 @@ const chatHeader = () => (
   <div>
     <main>
       <div>
-        <div className='navbar bg-base-100 rounded-t-md w-[700px] border relative z-1'>
-          <div className='flex-1'>
-            <div className='btn btn-ghost btn-circle avatar'>
-              <div className='rounded-full'>
-                <Image
-                  src='/../public/sample-profile-image.jpg'
-                  width={80}
-                  height={80}
-                />
+        <div className="navbar bg-base-100 rounded-t-md w-[700px] border relative z-1">
+          <div className="flex-1">
+            <div className="btn btn-ghost btn-circle avatar">
+              <div className="rounded-full">
+                <Image src="/../public/sample-profile-image.jpg" width={80} height={80} />
               </div>
             </div>
-            <div className='flex-col pl-5'>
-              <div className='font-bold'>
-                <Link href='/'>Name (Company)</Link>
+            <div className="flex-col pl-5">
+              <div className="font-bold">
+                <Link href="/">Name (Company)</Link>
               </div>
-              <div className='text-xs'>Last seen 10 minutes ago</div>
+              <div className="text-xs">Last seen 10 minutes ago</div>
             </div>
           </div>
-          <div className='flex-none gap-2'>
-            <div className='dropdown dropdown-end relative'>
-              <label tabIndex={0} className='btn btn-ghost btn-circle avatar'>
-                <div className='w-67 rounded-full'>
+          <div className="flex-none gap-2">
+            <div className="dropdown dropdown-end relative">
+              <label tabIndex={0} className="btn btn-ghost btn-circle avatar">
+                <div className="w-67 rounded-full">
                   <BsThreeDotsVertical />
                 </div>
               </label>
               <ul
                 tabIndex={0}
-                className='mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52 absolute'
+                className="mt-3 p-2 shadow menu menu-compact dropdown-content bg-base-100 rounded-box w-52 absolute"
               >
                 <li>
-                  <a>Archive Chat</a>
+                  <a href="http://localhost:3001">Archive Chat</a>
                 </li>
                 <li>
-                  <a>Block User</a>
+                  <a href="http://localhost:3001">Block User</a>
                 </li>
                 <li>
-                  <a>Delete Chat</a>
+                  <a href="http://localhost:3001">Delete Chat</a>
                 </li>
                 <li>
-                  <a>Report User</a>
+                  <a href="http://localhost:3001">Report User</a>
                 </li>
               </ul>
             </div>
           </div>
         </div>
-        <div className='navbar bg-base-100 rounded-b-md w-[700px] border relative block'>
-          <div className='flex-1'>
-            <div className='btn btn-circle btn-ghost avatar'>
-              <div className='w-10 rounded-md'>
-                <Image
-                  src='/../public/sample-product-image.jpg'
-                  width={80}
-                  height={80}
-                />
+        <div className="navbar bg-base-100 rounded-b-md w-[700px] border relative block">
+          <div className="flex-1">
+            <div className="btn btn-circle btn-ghost avatar">
+              <div className="w-10 rounded-md">
+                <Image src="/../public/sample-product-image.jpg" width={80} height={80} />
               </div>
-              <div className='absolute h-3 bg-white w-10 pb-13 mt-6'>
-                <div className='absolute text-[7px] pl-1'>Reserved</div>
+              <div className="absolute h-3 bg-white w-10 pb-13 mt-6">
+                <div className="absolute text-[7px] pl-1">Reserved</div>
               </div>
             </div>
-            <div className='flex-col pl-5'>
+            <div className="flex-col pl-5">
               <div>
-                <Link href='/'>
-                  Mild Steel Channel Hot Rolled (Bar 0.750 x 0.375 x 0.125)
-                </Link>
+                <Link href="/">Mild Steel Channel Hot Rolled (Bar 0.750 x 0.375 x 0.125)</Link>
               </div>
-              <div className='font-bold'>Price</div>
+              <div className="font-bold">Price</div>
             </div>
           </div>
         </div>
