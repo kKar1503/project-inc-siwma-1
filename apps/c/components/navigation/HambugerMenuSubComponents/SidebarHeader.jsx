2b83561425dfbd6d235d4869de27bcfb0921b03f
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2022-11-09 16:19:52 +0800

    refactor: refactored code to in order to satisfy requested changes 19-23, 25 - 26, 29 ,32 and 36
    Used react Icons
    Used tailwind classnamed
    refactored some classnames
    started using classnames package
    refactored file structure

---
+++ apps/c/components/navigation/HambugerMenuSubComponents/SidebarHeader.jsx
@@ -0,0 +1,28 @@
+// ------------------ Imports ------------------
+
+import { func } from 'prop-types';
+import { MdClose } from 'react-icons/md';
+
+/**
+ * The ExpandedHamburgerMenu is the menu that is shown when the HamburgerButtonIcon is clicked
+ * This is the icon that is displayed on the top left of the screen
+ * @type {React.FC<import('prop-types').InferProps<typeof SidebarHeaderIcon.propTypes>>}
+ */
+const SidebarHeaderIcon = ({ closeHandle }) => (
+  <div className="text-gray-100 text-xl">
+    <div className="p-2.5 mt-1 flex items-center">
+      <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-primary-focus" />
+      <h1 className="font-bold text-base-200 text-[15px] ml-3">S I W M A</h1>
+      <i className="bi bi-x cursor-pointer ml-28 lg:hidden" />
+      <div className="ml-32" role="presentation" onClick={closeHandle}>
+        <MdClose />
+      </div>
+    </div>
+  </div>
+);
+
+SidebarHeaderIcon.propTypes = {
+  closeHandle: func,
+};
+
+export default SidebarHeaderIcon;
---
+++ apps/c/components/navigation/HambugerMenuSubComponents/SidebarItem.jsx
@@ -0,0 +1,67 @@
+// ------------------ Imports ------------------
+
+import { useState } from 'react';
+import { node, string } from 'prop-types';
+import { useRouter } from 'next/router';
+import { MdArrowForwardIos } from 'react-icons/md';
+import cx from 'classnames';
+
+/**
+ * The Item represents each tab in the HamburgerMenu
+ * @type {React.FC<import('prop-types').InferProps<typeof SidebarItem.propTypes>>}
+ */
+const SidebarItem = ({ name, customIcon, redirectLink, children }) => {
+  // ------------------ useStates -----------------
+  const [open, setOpen] = useState(false);
+
+  // ------------------ Misc -----------------
+  const router = useRouter();
+
+  // ------------------ Handles -----------------
+
+  const toggle = () => {
+    setOpen(!open);
+  };
+
+  const redirect = () => (children ? toggle() : router.push(redirectLink));
+
+  // ------------------ Return -----------------
+  return (
+    <>
+      <div
+        onClick={redirect}
+        role="presentation"
+        className="p-2.5 mt-3 flex items-center rounded-md px-4 duration-300 cursor-pointer hover:bg-primary-focus text-white"
+      >
+        {customIcon && <customIcon />}
+        <span className="text-[15px] ml-4 text-base-200 font-bold">{name}</span>
+        {children && (
+          <span
+            role="presentation"
+            className={cx('text-sm -rotate-0 ml-48', { 'rotate-90': open })}
+          >
+            <MdArrowForwardIos />
+          </span>
+        )}
+      </div>
+      {children && (
+        <div
+          className={cx('block text-left text-sm mt-2 w-4/5 mx-auto text-base-200 font-bold', {
+            hidden: !open,
+          })}
+        >
+          {children}
+        </div>
+      )}
+    </>
+  );
+};
+
+SidebarItem.propTypes = {
+  name: string.isRequired,
+  customIcon: node,
+  children: node,
+  redirectLink: string,
+};
+
+export default SidebarItem;
