2e716cd3f7dea7fe7b79152b5aaa3776f7e24ef5
Nabil Ridhwan <nabridhwan@gmail.com>
2022-11-10 17:06:32 +0800

    feat: :sparkles: utilize btn-circle class from Daisy UI from button

--- apps/c/components/marketplace/carousel/IconRoundButton.jsx
+++ apps/c/components/marketplace/carousel/IconRoundButton.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
 const IconRoundButton = ({ onClick, icon }) => (
-  <button onClick={onClick} className="btn btn-square btn-md rounded-full shadow-lg text-lg">
+  <button onClick={onClick} className="btn btn-circle btn-md shadow-lg text-lg">
     {icon}
   </button>
 );
