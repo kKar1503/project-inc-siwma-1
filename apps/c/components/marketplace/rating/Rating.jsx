7b6c3fd3d61810409b013f6ee7a69048308f9c47
Nabil Ridhwan <nabridhwan@gmail.com>
2022-11-10 17:05:24 +0800

    style: :lipstick: fix star coloring to be main theme of the app

--- apps/c/components/marketplace/rating/Rating.jsx
+++ apps/c/components/marketplace/rating/Rating.jsx
@@ -1,16 +1,22 @@
 import PropTypes from 'prop-types';
-import { useState } from 'react';
 import FullStar from './FullStar';
 import HalfStar from './HalfStar';
 import RemainingStar from './RemainingStar';
 
+/**
+ * Rating is a component that shows a rating on the screen.
+ * @type {React.FC<import('prop-types').InferProps<typeof propTypes>>}
+ */
+
 const Rating = ({ rating }) => {
-  const [solid] = useState(Math.floor(rating));
-  const [half] = useState(rating % 1 <= 0.4 ? 0 : 1);
+  const solid = Math.floor(rating);
+  const half = rating % 1 <= 0.4 ? 0 : 1;
 
   return (
     <div className="flex items-center gap-2">
       {/* Stars: */}
+
+      {/* TODO: Need a unique-random key */}
       <div className="rating rating-sm rating-half">
         {[...Array(solid)].map(() => (
           <FullStar key="solid" />
--- apps/c/components/marketplace/rating/RemainingStar.jsx
+++ apps/c/components/marketplace/rating/RemainingStar.jsx
@@ -4,14 +4,14 @@ const RemainingStar = () => (
       disabled
       type="radio"
       name="rating-10"
-      className="bg-orange-500/40 mask mask-star-2 mask-half-1"
+      className="bg-primary/40 mask mask-star-2 mask-half-1"
     />
 
     <input
       disabled
       type="radio"
       name="rating-10"
-      className="bg-orange-500/40 mask mask-star-2 mask-half-2"
+      className="bg-primary/40 mask mask-star-2 mask-half-2"
     />
   </>
 );
