6676497afc7f43f07bfb61fc5af654d25dea773d
Acrylic125 <acrylic125email@gmail.com>
2022-11-11 19:15:40 +0800

    feat: add create supabase service client util func

---
+++ apps/b/utils/supabase.js
@@ -0,0 +1,19 @@
+// Disable prefer default export for now. Interface with index.js to prevent any issues
+// when refactoring.
+/* eslint-disable import/prefer-default-export */
+
+import { createClient } from '@supabase/supabase-js';
+
+/**
+ *
+ * DO NOT USE THIS IN THE CLIENT SIDE. THIS IS ONLY FOR SERVER SIDE USE.
+ *
+ * This function creates a service client that bypasses RLS.
+ */
+export function createServiceSupabaseClient() {
+  const supabase = createClient(
+    process.env.NEXT_PUBLIC_SUPABASE_URL,
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY
+  );
+  return supabase;
+}
