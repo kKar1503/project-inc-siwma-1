ddfeaf3551c60b50c273fc4e674f20ca19faf6e0
Acrylic125 <acrylic125email@gmail.com>
2022-11-11 19:15:20 +0800

    refactor: broken down into wrapper forms

--- apps/b/pages/login/index.jsx
+++ apps/b/pages/login/index.jsx
@@ -1,6 +1,32 @@
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import dynamic from 'next/dynamic';
+import React from 'react';
+import { useMutation } from 'react-query';
 import SignInAndUpLayout from '../../components/layouts/SignInAndUpLayout';
 import SignInForm from '../../components/layouts/SignInForm';
+
+/**
+ * To minimise the amount of components needed to be rendered,
+ * and to ensure separation of concerns, we will handle the register
+ * logic here.
+ *
+ * @type {React.FC}
+ */
+const LoginFormWrap = () => {
+  const supabase = useSupabaseClient();
+  const register = useMutation(async ({ email, password }) => {
+    supabase.auth.admin.createUser({
+      email,
+      password,
+    });
+    supabase.auth.signUp({
+      email,
+      password,
+    });
+  });
+
+  return <SignInForm />;
+};
 
 /**
  * @type {import("next").NextPage}
@@ -11,7 +37,7 @@ const Page = () => (
       <h1 className="font-bold text-xl">Login</h1>
       <p>Please sign in to your account.</p>
     </div>
-    <SignInForm />
+    <LoginFormWrap />
   </div>
 );
 
