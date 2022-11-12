0f829979b1f2673300c0bdcc6c8a3b24caebc311
Acrylic125 <acrylic125email@gmail.com>
2022-11-12 19:14:21 +0800

    feat: temp using ums schema

--- packages/utils/supabase.js
+++ packages/utils/supabase.js
@@ -13,7 +13,12 @@ import { createClient } from '@supabase/supabase-js';
 export function createServiceSupabaseClient() {
   const supabase = createClient(
     process.env.NEXT_PUBLIC_SUPABASE_URL,
-    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY
+    process.env.NEXT_PUBLIC_SUPABASE_SERVICE_KEY,
+    {
+      db: {
+        schema: 'ums',
+      },
+    }
   );
   return supabase;
 }
