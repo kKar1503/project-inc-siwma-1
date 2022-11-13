5a80cdbaff8e6cc3f9dc4ff3a1a2841bf6966066
Kar <ISoNice4572@gmail.com>
2022-11-14 01:33:53 +0800

    refactor: :truck: moved team-b web pages

--- apps/web/pages/login.jsx
+++ apps/web/pages/login.jsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
-import SignInAndUpLayout from '../../components/layouts/SignInAndUpLayout';
-import SignUpForm from '../../components/layouts/SignUpForm';
+import SignInAndUpLayout from '../components/layouts/SignInAndUpLayout';
+import SignInForm from '../components/layouts/SignInForm';
 
 /**
  * @type {import("next").NextPage}
@@ -8,10 +8,10 @@ import SignUpForm from '../../components/layouts/SignUpForm';
 const Page = () => (
   <div className="flex flex-col gap-4">
     <div className="flex flex-col">
-      <h1 className="font-bold text-xl">Register</h1>
-      <p>Register your account here.</p>
+      <h1 className="font-bold text-xl">Login</h1>
+      <p>Please sign in to your account.</p>
     </div>
-    <SignUpForm />
+    <SignInForm />
   </div>
 );
 
--- apps/web/pages/register.jsx
+++ apps/web/pages/register.jsx
@@ -1,6 +1,6 @@
 import dynamic from 'next/dynamic';
-import SignInAndUpLayout from '../../components/layouts/SignInAndUpLayout';
-import SignInForm from '../../components/layouts/SignInForm';
+import SignInAndUpLayout from '../components/layouts/SignInAndUpLayout';
+import SignUpForm from '../components/layouts/SignUpForm';
 
 /**
  * @type {import("next").NextPage}
@@ -8,10 +8,10 @@ import SignInForm from '../../components/layouts/SignInForm';
 const Page = () => (
   <div className="flex flex-col gap-4">
     <div className="flex flex-col">
-      <h1 className="font-bold text-xl">Login</h1>
-      <p>Please sign in to your account.</p>
+      <h1 className="font-bold text-xl">Register</h1>
+      <p>Register your account here.</p>
     </div>
-    <SignInForm />
+    <SignUpForm />
   </div>
 );
 
