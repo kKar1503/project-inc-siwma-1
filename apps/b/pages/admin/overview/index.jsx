59cf3e6ce60970437b1007a7896fbb0db06c27ec
Bengco Rance <Thinkaze@gmail.com>
2022-11-13 19:24:49 +0800

    fix: fix table-left issue

---
+++ apps/b/pages/admin/overview/index.jsx
@@ -0,0 +1,167 @@
+import AdminFigure from '../../../components/AdminFigure';
+import AdminPageLayout from '../../../components/layouts/AdminPageLayout';
+import NavBar from '../../../components/NavBar';
+import RegisteredUsersTableWithoutCheckbox from '../../../components/Tables/RegisteredUsersTableWithoutCheckbox';
+import PendingInvitesTableWithoutCheckbox from '../../../components/Tables/PendingInvitesTableWithoutCheckbox';
+
+const inviteTableData = [
+  {
+    id: 1,
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 2,
+    email: 'veryverylongname@veryverylongdomain.com',
+    company: 'Very Very Long Company Name',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 3,
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 4,
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 5,
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 6,
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 7,
+    email: 'veryverylongname@veryverylongdomain.com',
+    company: 'Very Very Long Company Name',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 8,
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 9,
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 10,
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+];
+
+const registeredUsersData = [
+  {
+    id: 1,
+    name: 'John Doe',
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 2,
+    name: 'Veryvery Long Name',
+    email: 'veryverylongname@veryverylongdomain.com',
+    company: 'Very Very Long Company Name',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 3,
+    name: 'John Doe',
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 4,
+    name: 'John Doe',
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 5,
+    name: 'John Doe',
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 6,
+    name: 'John Doe',
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 7,
+    name: 'Veryvery Long Name',
+    email: 'veryverylongname@veryverylongdomain.com',
+    company: 'Very Very Long Company Name',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 8,
+    name: 'John Doe',
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 9,
+    name: 'John Doe',
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+  {
+    id: 10,
+    name: 'John Doe',
+    email: 'john@doe.com',
+    company: 'Company',
+    mobileNumber: '+65 9832 0293',
+  },
+];
+
+const UserOverview = () => (
+  <div className="flex flex-col w-full h-full gap-8 p-8 overflow-auto">
+    {/* { replace this with sidebar } */}
+    <NavBar />
+
+    <div className="flex flex-row gap-8 flex-wrap">
+      <AdminFigure title="Active Companies" value="20" color="text-primary" />
+      <AdminFigure title="Total Users" value="14" color="text-accent" />
+      <AdminFigure title="Pending Invites" value="15" color="text-secondary" />
+    </div>
+
+    <div className="flex flex-row flex-wrap gap-8">
+      <RegisteredUsersTableWithoutCheckbox className="basis-[7]" data={registeredUsersData} />
+
+      <PendingInvitesTableWithoutCheckbox
+        className="basis-[5] w-full grow-none"
+        data={inviteTableData}
+      />
+    </div>
+  </div>
+);
+
+UserOverview.getLayout = (page) => <AdminPageLayout pageName="Overview">{page}</AdminPageLayout>;
+
+export default UserOverview;
