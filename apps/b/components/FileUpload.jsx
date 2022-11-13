a3190abf766e3924c8da3c54b7887a76f285cb75
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2022-11-13 22:22:27 +0800

    feat: correct UI for file uploads

---
+++ apps/b/components/FileUpload.jsx
@@ -0,0 +1,56 @@
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { FiUpload } from 'react-icons/fi';
+import { BsFileEarmarkSpreadsheet } from 'react-icons/bs';
+
+const FileUpload = ({ className }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  return (
+    <div className={cx(className, 'flex flex-col bg-base-100 rounded-lg shadow-lg')}>
+      <div className="p-6">
+        <h2 className="text-lg font-bold">Bulk Add Companies & Invite Users</h2>
+        <p>Import a .xlsx or .csv file below to bulk add company profiles and bulk invite users</p>
+      </div>
+      <hr />
+      <div className="flex-1 p-6">
+        <div className="flex flex-col justify-center items-center border border-primary-content border-dotted h-full p-10 gap-5">
+          <div className="h-20">
+            <input
+              id="fileInput"
+              type="file"
+              onChange={changeHandler}
+              className="hidden"
+              accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet" // Accept only .xlsx files
+            />
+            <label htmlFor="fileInput">
+              {selectedFile == null ? (
+                <FiUpload className="w-full h-full" />
+              ) : (
+                <BsFileEarmarkSpreadsheet className="w-full h-full text-success" />
+              )}
+            </label>
+          </div>
+          {selectedFile != null ? (
+            <p className="text-center">{selectedFile.name}</p>
+          ) : (
+            <p className="text-lg text-center w-3/4">
+              Click to upload or drag and drop .xslx or .csv (MAX. 64MB)
+            </p>
+          )}
+        </div>
+      </div>
+    </div>
+  );
+};
+
+FileUpload.propTypes = {
+  className: PropTypes.string,
+};
+
+export default FileUpload;
