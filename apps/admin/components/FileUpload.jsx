96cdd2fd050ca8313aed91cd666f366435668b1b
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2022-11-13 21:56:35 +0800

    feat: file upload logic

--- apps/admin/components/FileUpload.jsx
+++ apps/admin/components/FileUpload.jsx
@@ -1,21 +1,48 @@
 import cx from 'classnames';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
+import { FiUpload } from 'react-icons/fi';
 
-const FileUpload = ({ className }) => (
-  <div className={cx(className, 'flex flex-col bg-base-100 rounded-lg shadow-lg')}>
-    <div className="p-6">
-      <h2 className="text-lg font-bold">Bulk Add Companies & Invite Users</h2>
-      <p>Import a .xlsx or .csv file below to bulk add company profiles and bulk invite users</p>
-    </div>
-    <hr />
-    <div className="flex-1 p-6">
-      <div className="flex flex-col justify-center items-center border border-primary-content border-dotted h-full p-10 gap-20">
-        <div className="w-20 h-20 bg-base-300" />
-        <p className="text-lg break-all">SIWMA_MEMBERS_FIRST_BATCH.xslx</p>
+const FileUpload = ({ className }) => {
+  const [selectedFile, setSelectedFile] = useState(null);
+
+  const changeHandler = (event) => {
+    setSelectedFile(event.target.files[0]);
+  };
+
+  return (
+    <div className={cx(className, 'flex flex-col bg-base-100 rounded-lg shadow-lg')}>
+      <div className="p-6">
+        <h2 className="text-lg font-bold">Bulk Add Companies & Invite Users</h2>
+        <p>Import a .xlsx or .csv file below to bulk add company profiles and bulk invite users</p>
+      </div>
+      <hr />
+      <div className="flex-1 p-6">
+        <div className="flex flex-col justify-center items-center border border-primary-content border-dotted h-full p-10 gap-5">
+          <div className="h-20">
+            <input
+              id="fileInput"
+              type="file"
+              onChange={changeHandler}
+              className="hidden"
+              accept="application/vnd.openxmlformats-officedocument.spreadsheetml.sheet"
+            />
+            <label htmlFor="fileInput">
+              <FiUpload className="w-full h-full" />
+            </label>
+          </div>
+          {selectedFile != null ? (
+            <p className="text-center">{selectedFile.name}</p>
+          ) : (
+            <p className="text-lg text-center w-3/4">
+              Click to upload or drag and drop .xslx or .csv (MAX. 64MB)
+            </p>
+          )}
+        </div>
       </div>
     </div>
-  </div>
-);
+  );
+};
 
 FileUpload.propTypes = {
   className: PropTypes.string,
