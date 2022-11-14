afc639d41357b49a38fa28987f17cc4344290b4d
RuinedRuins <matthew.ngjb@gmail.com>
2022-11-15 01:34:58 +0800

    build: :art: minor structure additions and changes
    for future reference

--- apps/c/components/category/CategoryCard.jsx
+++ apps/c/components/category/CategoryCard.jsx
@@ -2,14 +2,21 @@
 import PropTypes from 'prop-types';
 
 const CategoryCard = ({ img, name, href }) => (
-  <div className="card shadow-md w-1/6">
+  // can add hover:scale-105 if it looks nicer, i'm not sure if it fits thematically
+  <div className="card shadow-md w-1/6 mr-5 transition ease-in-out hover:-translate-y-1">
     <a href={href}>
       <picture className="flex place-content-center">
         <img className="h-[150px] m-5" src={img} alt={name} />
       </picture>
-
+      {/* horizontal line to seperate img and text */}
+      <hr
+        style={{
+          background: 'grey',
+          height: '2px',
+        }}
+      />
       {/* Listing content */}
-      <div className="p-2 pb-4">
+      <div className="p-2 pb-3">
         <p className="font-bold">{name}</p>
       </div>
     </a>
