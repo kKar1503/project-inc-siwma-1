48e73a9bef15bfc8caaaa4522d20f90c6de536c7
Xavier Teo <xavierteo26@gmail.com>
2022-11-15 16:44:20 +0800

    fix: :bug: bug where selected rows will be rendered as unselected when table pages
    The checkbox would be rendered as unchecked when the row is actually checked

--- apps/admin/components/Tables/RegisteredCompaniesTable.jsx
+++ apps/admin/components/Tables/RegisteredCompaniesTable.jsx
@@ -220,6 +220,14 @@ const RegisteredCompaniesTable = ({ className }) => {
             >
               SUSPEND SELECTED
             </button>
+            <button
+              className="btn btn-primary text-white"
+              onClick={() => {
+                console.log(selectedRows);
+              }}
+            >
+              TEST
+            </button>
           </div>
 
           <div className="flex justify-end bg-none">
