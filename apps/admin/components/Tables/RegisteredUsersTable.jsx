14c5aaaf8de2eaac3da2d466f5d151bf924cb1b5
Kaung Khant Zaw <mmmike2002@gmail.com>
2022-11-16 13:14:36 +0800

    feat: :sparkles: pagination for users and invites table

--- apps/admin/components/Tables/RegisteredUsersTable.jsx
+++ apps/admin/components/Tables/RegisteredUsersTable.jsx
@@ -1,6 +1,7 @@
 import React, { useState } from 'react';
 import { useQuery } from 'react-query';
 import PropTypes from 'prop-types';
+import cx from 'classnames';
 import BaseTable from './BaseTable';
 import SearchBar from '../SearchBar';
 import TableButton from './TableButton';
@@ -15,19 +16,46 @@ const parseData = (data) =>
     profilePicture: pic,
     name: e.fullname,
     email: e.email,
-    company: e.companyid,
+    company: e.companies.name,
     mobileNumber: e.phone,
   }));
 
 const supabase = CreateSupabaseClient();
 
 const RegisteredUsersTable = ({ className }) => {
+  const paginationValues = [1, 2, 3];
+
   const [selectedIndex, setSelectedIndex] = useState(0);
+  const [option, setOption] = useState(paginationValues[0]);
 
   const { data, isLoading } = useQuery({
     queryKey: ['users'],
-    queryFn: async () => supabase.from('users').select('*'),
+    queryFn: async () =>
+      supabase.from('users').select(`id, email, fullname, phone, companies:companyid(name)`),
   });
+
+  const renderTableButtons = () => {
+    const tableButtons = [];
+    if (isLoading) return;
+    const count = data.data.length / option;
+    // eslint-disable-next-line no-plusplus
+    for (let i = 0; i < count; i++) {
+      tableButtons.push(
+        <TableButton
+          index={i}
+          selectedIndex={selectedIndex}
+          setSelectedIndex={setSelectedIndex}
+          selectedColor="bg-primary"
+          className={cx('hover:bg-primary', {
+            'rounded-l-lg': i === 0,
+            'rounded-r-lg': i >= count - 1,
+          })}
+        />
+      );
+    }
+    // eslint-disable-next-line consistent-return
+    return tableButtons;
+  };
 
   return (
     <BaseTable
@@ -35,14 +63,18 @@ const RegisteredUsersTable = ({ className }) => {
         <div className="flex flex-row justify-between items-center">
           <div className="flex flex-col pb-3">
             <h1 className="font-bold text-xl">Registered Users</h1>
-            <h1 className="pr-2">Showing 1 to 10 of 100 entries</h1>
+            <h1 className="pr-2">Showing 1 to 10 of {data?.data.length} entries</h1>
           </div>
           <div className="flex flex-row gap-4">
             <h1 className="mt-3">Show</h1>
-            <select className="select select-bordered w-25">
-              <option>8 per page</option>
-              <option>15 per page</option>
-              <option>50 per page</option>
+            <select
+              value={option}
+              className="select select-bordered w-25"
+              onChange={(e) => setOption(e.target.value)}
+            >
+              <option value={paginationValues[0]}>{paginationValues[0]} per page</option>
+              <option value={paginationValues[1]}>{paginationValues[1]} per page</option>
+              <option value={paginationValues[2]}>{paginationValues[2]} per page</option>
             </select>
             <SearchBar placeholder="Search by name" />
           </div>
@@ -53,33 +85,15 @@ const RegisteredUsersTable = ({ className }) => {
       showCheckbox
       className={className}
       columnKeys={['name', 'email', 'company', 'mobileNumber']}
-      data={isLoading ? undefined : parseData(data.data)}
+      data={
+        isLoading
+          ? undefined
+          : parseData(data.data.slice(selectedIndex * option, (selectedIndex + 1) * option))
+      }
       footer={
         <div className="flex justify-between bg-none">
           <button className="btn btn-primary text-white">DEACTIVATE SELECTED</button>
-          <div className="flex justify-end bg-none">
-            <TableButton
-              index={0}
-              selectedIndex={selectedIndex}
-              setSelectedIndex={setSelectedIndex}
-              selectedColor="bg-primary"
-              className="rounded-l-lg hover:bg-primary"
-            />
-            <TableButton
-              index={1}
-              selectedIndex={selectedIndex}
-              setSelectedIndex={setSelectedIndex}
-              selectedColor="bg-primary"
-              styles="hover:bg-primary"
-            />
-            <TableButton
-              index={2}
-              selectedIndex={selectedIndex}
-              setSelectedIndex={setSelectedIndex}
-              selectedColor="bg-primary"
-              className="rounded-r-lg hover:bg-primary"
-            />
-          </div>
+          <div className="flex justify-end bg-none">{renderTableButtons()}</div>
         </div>
       }
     />
