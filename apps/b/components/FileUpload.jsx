af5b362c9296ce59e5496af8b8ffc4ade03d6333
Karandeep Singh <46585033+ConfuseIous@users.noreply.github.com>
2022-11-16 17:47:28 +0800

    feat: basic file parsing

--- apps/b/components/FileUpload.jsx
+++ apps/b/components/FileUpload.jsx
@@ -3,19 +3,35 @@ import PropTypes from 'prop-types';
 import { useState } from 'react';
 import { FiUpload } from 'react-icons/fi';
 import { BsFileEarmarkSpreadsheet } from 'react-icons/bs';
+import * as XLSX from 'xlsx';
 
 const FileUpload = ({ className }) => {
   const [selectedFile, setSelectedFile] = useState(null);
 
-  const changeHandler = (event) => {
+  const changeHandler = async (event) => {
     setSelectedFile(event.target.files[0]);
+
+    const reader = new FileReader();
+    reader.onload = (evt) => {
+      // evt = on_file_select event
+      /* Parse data */
+      const bstr = evt.target.result;
+      const wb = XLSX.read(bstr, { type: 'binary' });
+      /* Get first worksheet */
+      const wsname = wb.SheetNames[0];
+      const ws = wb.Sheets[wsname];
+      /* Convert array of arrays */
+      const data = XLSX.utils.sheet_to_csv(ws, { header: 1 });
+      console.log('data');
+    };
+    reader.readAsBinaryString(event.target.files[0]);
   };
 
   return (
     <div className={cx(className, 'flex flex-col bg-base-100 rounded-lg shadow-lg')}>
       <div className="p-6">
         <h2 className="text-lg font-bold">Bulk Add Companies & Invite Users</h2>
-        <p>Import a .xlsx or .csv file below to bulk add company profiles and bulk invite users</p>
+        <p>Import a .xlsx file below to bulk add company profiles and bulk invite users</p>
       </div>
       <hr />
       <div className="flex-1 p-6">
