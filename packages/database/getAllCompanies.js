23f88e257c6bb3b81d2d541ca02a8a18413d8435
Xavier Teo <xavierteo26@gmail.com>
2022-11-17 21:02:48 +0800

    refactor: :card_file_box: use supabase auth helpers instead of supabase client
    this implementation does not bypass RLS whereas the previous did

--- packages/database/getAllCompanies.js
+++ packages/database/getAllCompanies.js
@@ -1,13 +1,13 @@
-import supabase from './supabase';
-
 /**
  * Retrieves all companies
+ * @param {{ supabase: object, from: number, to: number, matching: string}}
+ * @param {object} supabase The supabase instance
  * @param {number} from The index to start from
  * @param {number} to The index to retrieve until
  * @param {string} matching Filter out companies with names that are unlike the string
  * @returns Companies that match the criterias given
  */
-const getAllCompanies = ({ from, to, matching }) => {
+const getAllCompanies = ({ supabase, from, to, matching }) => {
   // Construct the base query
   const query = supabase.from('companies').select('*').order('name', { ascending: true });
 
