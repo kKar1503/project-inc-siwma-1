e60f4944d140a264ea780600e95b7500d18a58b5
Xavier Teo <xavierteo26@gmail.com>
2022-11-19 15:49:19 +0800

    feat: :sparkles: add new util functions: arrayToString and hasFileExt

--- packages/utils/index.js
+++ packages/utils/index.js
@@ -1,3 +1,6 @@
 export { default as lazyReference } from './lazyReference';
 
 export * from './supabase';
+
+export { default as hasFileExt } from './hasFileExt';
+export { default as arrayToString } from './arrayToString';
