982edca5e4533a468e59aa71c1707d60cbe091e3
Kar <ISoNice4572@gmail.com>
2022-11-19 20:15:25 +0800

    feat: :package: updated @inc/database package to have enums for db tables

---
+++ packages/database/src/constants/enums.js
@@ -0,0 +1,20 @@
+export default {
+  LISTING_TYPE: {
+    BUY: 1,
+    SELL: 2,
+  },
+  DATATYPE: {
+    STRING: 1,
+    NUMBER: 2,
+    BOOLEAN: 3,
+  },
+  PARAMETER_TYPE: {
+    MEASUREMENT: {
+      WEIGHT: 1,
+      DIMENSION: 2,
+    },
+    TWO_CHOICES: 3,
+    MANY_CHOICES: 4,
+    OPEN_ENDED: 5,
+  },
+};
