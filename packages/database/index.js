420b233ae4fb1f0138d324cf64c4645c7d679cc4
Kar <ISoNice4572@gmail.com>
2022-11-19 20:15:25 +0800

    feat: :package: updated @inc/database package to have enums for db tables

---
+++ packages/database/index.js
@@ -0,0 +1,2 @@
+export { default as Enum } from './src/constants/enums';
+export { default as Database } from './src/constants/database';
---
+++ packages/database/src/constants/database.js
@@ -0,0 +1,23 @@
+export default {
+  RPC: {},
+  TABLES: {
+    LISTING: {
+      LISTING: 'listing',
+      TYPE: 'listing_type',
+      IMAGES: 'listings_images',
+      PARAMETER_VALUES: 'listings_parameters_value',
+      IMAGE_BUCKET: 'listing-image-bucket',
+    },
+    CATEGORY: {
+      CATEGORY: 'category',
+      PARAMETER: 'categories_parameters',
+      IMAGE_BUCKET: 'category-image-bucket',
+    },
+    PARAMETER: {
+      PARAMETER: 'parameter',
+      DATATYPE: 'datatype',
+      TYPE: 'parameter_type',
+      CHOICES: 'parameter_choices',
+    },
+  },
+};
