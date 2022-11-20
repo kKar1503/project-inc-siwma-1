50d2947364db95ff47f0d82ca14a82c768fa0a67
Nabil Ridhwan <nabridhwan@gmail.com>
2022-11-20 10:55:24 +0800

    feat: :sparkles: marketplace and marketplace components related fixes brought over to new branching

--- apps/web/components/marketplace/ProductListingItem.jsx
+++ apps/web/components/marketplace/ProductListingItem.jsx
@@ -1,22 +1,23 @@
 // Import prop types
+import Image from 'next/image';
+import Link from 'next/link';
 import PropTypes from 'prop-types';
 import Rating from './rating/Rating';
 
 const ProductListingItem = ({ img, name, rating, href }) => (
   <div className="card shadow-md">
-    <a href={href}>
-      <picture>
+    <Link href={href}>
+      <div className="aspect-square object-cover h-[150px]">
         {/* ! The reason why the image below is 150px in height is because smaller images will be zoomed in to fit the height (this is so images > 150px will zoom and crop) */}
-        <img className="aspect-square object-cover h-[150px]" src={img} alt={name} />
-      </picture>
+        <Image fill src={img} alt={name} className="object-cover" />
+      </div>
 
       {/* Listing content */}
       <div className="p-2 pb-4">
         <p className="font-bold">{name}</p>
-        {/* TODO: Implement rating from Daisy UI */}
         <Rating rating={rating} />
       </div>
-    </a>
+    </Link>
   </div>
 );
 
--- apps/web/next.config.js
+++ apps/web/next.config.js
@@ -2,4 +2,7 @@ const withTM = require('next-transpile-modules')(['@inc/ui', '@inc/styles']);
 
 module.exports = withTM({
   reactStrictMode: true,
+  images: {
+    domains: ['images.unsplash.com', 'via.placeholder.com'],
+  },
 });
