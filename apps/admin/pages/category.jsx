294d1b23f0a337be8dcc33d421cb1a8194705f5a
Joshua Chow <crhjoshua12@gmail.com>
2022-11-21 23:56:31 +0800

    feat: category_management
    Category Management

---
+++ apps/admin/pages/category.jsx
@@ -0,0 +1,59 @@
+import CreateParam from '../components/CreateParam';
+import EditCat from '../components/EditCat';
+import AdminPageLayout from '../components/layouts/AdminPageLayout';
+import NavBar from '../components/NavBar';
+import ActiveParametersTable from '../components/Tables/ActiveParametersTable';
+import AvailableParametersTable from '../components/Tables/AvailableParametersTable';
+
+/**
+ * Sample data
+ */
+const ActiveParametersData = (id) => ({
+  id,
+  company: 'GUANGZHAO METALWORKS, PTE. LTD',
+  email: 'sallyknox_slfi@gmail.com',
+  mobileNumber: '+65 9832 0293',
+});
+
+const AvailableParametersData = (id) => ({
+  id,
+  company: 'GUANGZHAO METALWORKS, PTE. LTD',
+  website: 'www.guangzhaomw.com',
+});
+
+function populateArray(element, count) {
+  const result = [];
+
+  // eslint-disable-next-line no-plusplus
+  for (let i = 0; i < count; i++) {
+    result[i] = element(i + 1);
+  }
+
+  return result;
+}
+
+const CategoryPage = () => (
+  <div className="w-full p-8 gap-8 overflow-auto xl:max-h-screen">
+    {/* <div className="flex flex-col flex-1 w-full p-8 gap-8 overflow-auto xl:max-h-screen"> */}
+    <NavBar />
+    <div className="flex flex-row w-full gap-8 flex-1 justify-between xl:overflow-hidden pb-4 pt-4">
+      <div className="card bg-base-100 w-full rounded-lg p-4">
+        <EditCat />
+      </div>
+      <div className="card bg-base-100 w-full rounded-lg p-4">
+        <CreateParam />
+      </div>
+    </div>
+    <div className="flex flex-row w-full gap-8 flex-1 justify-between xl:overflow-hidden">
+      <ActiveParametersTable data={populateArray(ActiveParametersData, 10)} />
+      <AvailableParametersTable data={populateArray(AvailableParametersData, 10)} />
+    </div>
+  </div>
+);
+
+/**
+ * Use the layout for Admin Pages
+ */
+CategoryPage.getLayout = (page) => <AdminPageLayout pageName="Invites">{page}</AdminPageLayout>;
+
+export default CategoryPage;
