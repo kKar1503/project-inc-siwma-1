b79c271ca0e5b87454292a6e3487c44ebd2cd6ae
Nabil Ridhwan <nabridhwan@gmail.com>
2022-11-21 12:01:33 +0800

    feat: :sparkles: updated Carousel to be dynamic and also show more improve UX

--- apps/web/components/marketplace/carousel/IconRoundButton.jsx
+++ apps/web/components/marketplace/carousel/IconRoundButton.jsx
@@ -1,7 +1,7 @@
 import PropTypes from 'prop-types';
 
-const IconRoundButton = ({ onClick, icon }) => (
-  <button onClick={onClick} className="btn btn-circle btn-md shadow-lg text-lg">
+const IconRoundButton = ({ onClick, icon, className }) => (
+  <button onClick={onClick} className={`btn btn-circle btn-md shadow-lg text-lg ${className}`}>
     {icon}
   </button>
 );
@@ -9,6 +9,7 @@ const IconRoundButton = ({ onClick, icon }) => (
 IconRoundButton.propTypes = {
   onClick: PropTypes.func,
   icon: PropTypes.node,
+  className: PropTypes.string,
 };
 
 export default IconRoundButton;
