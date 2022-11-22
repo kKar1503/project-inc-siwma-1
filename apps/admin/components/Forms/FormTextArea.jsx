257b9381618a0766773ed99785fcbcd28955eca5
Xavier Teo <xavierteo26@gmail.com>
2022-11-22 17:49:40 +0800

    feat: :sparkles: form components made using react-hook-form
    allows for custom forms to be built using components with native react-hook-form support

---
+++ apps/admin/components/Forms/FormTextArea.jsx
@@ -0,0 +1,56 @@
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+
+/**
+ * Wrapper component for a react hook form textarea input
+ * @type {React.FC<PropTypes.InferProps<typeof propTypes>>}
+ * @returns A input that works with react form hook
+ */
+const FormTextArea = ({
+  register,
+  isErrored,
+  name,
+  label,
+  placeholder,
+  customValidation,
+  required,
+  className,
+  style,
+}) => {
+  // Hooks inputs to using react form hook
+  const hookInput = (inputName, inputLabel, options) =>
+    register(inputName, {
+      required: { value: required, message: `${inputLabel} is required` },
+      maxLength: { value: 255, message: `${inputLabel} can only be 255 characters long` },
+      ...options,
+    });
+
+  return (
+    <textarea
+      className={cx(className, 'textarea textarea-bordered h-32', { 'textarea-error': isErrored })}
+      placeholder={placeholder || label}
+      style={style}
+      {...hookInput(name, label, customValidation)}
+    />
+  );
+};
+
+const propTypes = {
+  register: PropTypes.func.isRequired,
+  isErrored: PropTypes.shape({
+    message: PropTypes.string.isRequired,
+  }),
+  name: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  // We do not know what the shape of the object will be
+  // eslint-disable-next-line react/forbid-prop-types
+  customValidation: PropTypes.object,
+  required: PropTypes.bool,
+  className: PropTypes.string,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+};
+
+FormTextArea.propTypes = propTypes;
+
+export default FormTextArea;
---
+++ apps/admin/components/Forms/FormTextInput.jsx
@@ -0,0 +1,57 @@
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+
+/**
+ * Wrapper component for a react hook form input
+ * @type {React.FC<PropTypes.InferProps<typeof propTypes>>}
+ * @returns A input that works with react form hook
+ */
+const FormInput = ({
+  register,
+  isErrored,
+  name,
+  label,
+  placeholder,
+  customValidation,
+  required,
+  className,
+  style,
+}) => {
+  // Hooks inputs to using react form hook
+  const hookInput = (inputName, inputLabel, options) =>
+    register(inputName, {
+      required: { value: required, message: `${inputLabel} is required` },
+      maxLength: { value: 255, message: `${inputLabel} can only be 255 characters long` },
+      ...options,
+    });
+
+  return (
+    <input
+      type="text"
+      className={cx(className, 'input-group input input-bordered', { 'input-error': isErrored })}
+      style={style}
+      placeholder={placeholder || label}
+      {...hookInput(name, label, customValidation)}
+    />
+  );
+};
+
+const propTypes = {
+  register: PropTypes.func.isRequired,
+  isErrored: PropTypes.shape({
+    message: PropTypes.string.isRequired,
+  }),
+  name: PropTypes.string,
+  label: PropTypes.string,
+  placeholder: PropTypes.string,
+  // We do not know what the shape of the object will be
+  // eslint-disable-next-line react/forbid-prop-types
+  customValidation: PropTypes.object,
+  required: PropTypes.bool,
+  className: PropTypes.string,
+  style: PropTypes.oneOfType([PropTypes.object, PropTypes.array]),
+};
+
+FormInput.propTypes = propTypes;
+
+export default FormInput;
