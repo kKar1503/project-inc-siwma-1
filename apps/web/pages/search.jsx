3e5d6a69ef4b7979efc75bd4d0607d9b66a0ac69
Kar <ISoNice4572@gmail.com>
2022-11-22 13:05:27 +0800

    feat: :construction: wip search bar

---
+++ apps/web/pages/search.jsx
@@ -0,0 +1,9 @@
+import { Search } from '@inc/ui';
+
+const SearchPage = () => (
+  <div className="flex justify-center items-center h-screen">
+    <Search />
+  </div>
+);
+
+export default SearchPage;
---
+++ packages/ui/Search.d.ts
@@ -0,0 +1,9 @@
+import React from 'react';
+
+type ComponentProps = {
+  placeholder: string;
+};
+
+declare const Search: React.FC<ComponentProps>;
+
+export default Search;
