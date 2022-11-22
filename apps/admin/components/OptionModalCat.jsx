e6434ad8a8aecdf94a3848ed9ddde3cbb2005e46
Joshua Chow <crhjoshua12@gmail.com>
2022-11-23 00:06:53 +0800

    style: actions func
    actions func icon added

---
+++ apps/admin/components/OptionModalCat.jsx
@@ -0,0 +1,10 @@
+const ModalCat = () => (
+  <div className="dropdown dropdown-left">
+    <ul className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+      <li>Archive</li>
+      <li>Edit</li>
+    </ul>
+  </div>
+);
+
+export default ModalCat;
