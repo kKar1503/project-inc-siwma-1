ca846c3aab20114f345df0a55cef03a9f0c9c8ea
Bengco Rance <Thinkaze@gmail.com>
2022-11-24 00:12:43 +0800

    feat: add select option to edit user

--- apps/admin/pages/edit-company.jsx
+++ apps/admin/pages/edit-company.jsx
@@ -1,42 +1,36 @@
-import { useMemo, useState } from 'react';
-import PropTypes from 'prop-types';
+import { useState } from 'react';
 import Image from 'next/image';
 import AdminPageLayout from '../components/layouts/AdminPageLayout';
 import NavBar from '../components/NavBar';
 import ToggleEdit from '../components/FormControl/ToggleEdit';
 import ToggleEditArea from '../components/FormControl/ToggleEditArea';
-import TogglePass from '../components/FormControl/TogglePass';
-import ToggleSelect from '../components/FormControl/ToggleSelect';
 
-const EditUser = () => {
+const EditCompany = () => {
   // SET ALL USER DATA HERE (PROBABLY USE THE API CALL TO GET USER DATA)
-  const fullName = 'John Doe';
-  const email = 'E-mail';
-  const companyName = 'Company 1';
-  // get all companies below
-  const companies = ['Company 1', 'Company 2', 'Company 3'];
-  const mobileNumber = '9232 3232';
+  const website = 'John Doe';
+  const companyName = 'Company';
+  const bio = 'the most poggers company in all of existence';
   const profilePic =
     'https://spoxwyiorgijkrqidutq.supabase.co/storage/v1/object/public/companyprofilepictures/example.jpg';
+
+  const comments = 'comments';
 
   const [selectedFile, setSelectedFile] = useState(null);
 
   const changeHandler = (event) => {
     setSelectedFile(event.target.files[0]);
+    console.log(selectedFile);
   };
 
-  useMemo(() => {
-    console.log(selectedFile);
-    // ADD API CALL HERE (UPDATES FIELD TO DB AND UPDATES PROFILE PIC)
-  }, [selectedFile]);
-
   return (
-    <div className="flex flex-col w-full h-full gap-8 p-8 overflow-auto xl:max-h-screen">
+    <div className="flex flex-col w-full h-full gap-8 p-6 overflow-auto xl:max-h-screen">
       <NavBar />
 
-      <div className="flex flex-col grow h-fit shadow-xl rounded-2xl space-between bg-base-100">
-        <div className="flex flex-col px-6 pt-6">
-          <h1 className="font-bold text-xl">Edit User</h1>
+      <div className="flex flex-col grow h-fit shadow-xl rounded-2xl bg-base-100">
+        <div className="flex flex-col p-8 border-b">
+          <h1 className="font-bold text-xl">Edit Company</h1>
+          {/* If you want, you can use the company's name to replace 'company' in the heading below as well */}
+          <h1>Edit company details manually below</h1>
         </div>
         <div className="flex flex-wrap gap-8 p-8">
           <div className="flex flex-col flex-[3] flex-wrap">
@@ -70,24 +64,22 @@ const EditUser = () => {
             <div className="flex flex-col w-full min-w-96 gap-12">
               <div className="flex flex-col gap-4">
                 <div className="flex flex-col md:flex-row gap-4">
-                  <ToggleSelect value={companyName} options={companies} label="Company" />
-                  <ToggleEdit value={fullName} label="Full Name" />
+                  <ToggleEdit value={companyName} label="Company Name" />
+                  <ToggleEdit value={website} label="Website" />
                 </div>
                 <div className="flex flex-col md:flex-row gap-4">
-                  <ToggleEdit value={email} label="E-mail" />
-                  <ToggleEdit value={mobileNumber} label="Mobile Number" />
+                  <ToggleEditArea value={bio} label="Company Bio" maxLength={255} />
                 </div>
-                <TogglePass />
                 <div className="flex flex-col gap-4">
-                  <ToggleEditArea value="Comments" label="Comments" />
+                  <ToggleEditArea value={comments} label="Comments" maxLength={255} />
                 </div>
               </div>
             </div>
           </div>
         </div>
         <div className="flex px-8 pb-8 justify-end">
-          <a href="./users" className="btn btn-primary">
-            Return To Users
+          <a href="./companies" className="btn btn-primary">
+            Return To Companies
           </a>
         </div>
       </div>
@@ -95,6 +87,6 @@ const EditUser = () => {
   );
 };
 
-EditUser.getLayout = (page) => <AdminPageLayout pageName="Users">{page}</AdminPageLayout>;
+EditCompany.getLayout = (page) => <AdminPageLayout pageName="Companies">{page}</AdminPageLayout>;
 
-export default EditUser;
+export default EditCompany;
