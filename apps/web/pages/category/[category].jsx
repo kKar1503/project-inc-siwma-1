b67ae881f50679e29bf182c229ddc4615e824e49
RuinedRuins <matthew.ngjb@gmail.com>
2022-11-23 14:49:27 +0800

    build: :poop: added query to db for dynamic category page
    query takes insanely long time, must fix

--- apps/web/pages/category/[category].jsx
+++ apps/web/pages/category/[category].jsx
@@ -1,18 +1,29 @@
 import { useRouter } from 'next/router';
+import { useQuery } from 'react-query';
+import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import CategoryCard from '../../components/category/CategoryCard';
 import CategoryBanner from '../../components/category/CategoryBanner';
 
 const Category = () => {
   const router = useRouter();
   const { category } = router.query;
+  const supabase = useSupabaseClient();
+  const {
+    data: categoryData,
+    // status: categoryStatus,
+    // isLoading: categoryIsLoading,
+  } = useQuery(['test'], async () =>
+    supabase.rpc('get_category_listing', { catid: router.query.id })
+  );
 
+  console.log(categoryData);
   return (
     <div className="mt-10 ml-28 mr-28 mb-10 h-screen">
       <div className="mb-10 h-1/2">
         <CategoryBanner img="https://images.unsplash.com/photo-1563642900777-eb44ab05eaf8" />
       </div>
       <div className="h-2/3">
-        <p className="font-bold text-2xl">{category} shapes</p>
+        <p className="font-bold text-2xl">{category} listings</p>
         {/* dynamically generate each category when queried */}
         <div className="flex flex-wrap mb-10">
           {/* image temporarily grabbed from metalsupermarkets.com */}
@@ -24,33 +35,11 @@ const Category = () => {
             name="Mild Steel Angle"
             img="https://www.metalsupermarkets.com/wp-content/uploads/2021/03/Mild-Steel-Angle-Metal-Supermarkets-150x150.png"
           />
-          <CategoryCard
-            name="Mild Steel Bar Grating"
-            img="https://www.metalsupermarkets.com/wp-content/uploads/2021/10/Mild-Steel-Bar-Grating-Metal-Supermarkets-150x150.png"
-          />
-          <CategoryCard
-            name="Mild Steel Bar Grating"
-            img="https://www.metalsupermarkets.com/wp-content/uploads/2021/10/Mild-Steel-Bar-Grating-Metal-Supermarkets-150x150.png"
-          />
-          <CategoryCard
-            name="Mild Steel Bar Grating"
-            img="https://www.metalsupermarkets.com/wp-content/uploads/2021/10/Mild-Steel-Bar-Grating-Metal-Supermarkets-150x150.png"
-          />
-          <CategoryCard
-            name="Mild Steel Bar Grating"
-            img="https://www.metalsupermarkets.com/wp-content/uploads/2021/10/Mild-Steel-Bar-Grating-Metal-Supermarkets-150x150.png"
-          />
-          <CategoryCard
-            name="Mild Steel Bar Grating"
-            img="https://www.metalsupermarkets.com/wp-content/uploads/2021/10/Mild-Steel-Bar-Grating-Metal-Supermarkets-150x150.png"
-          />
-          <CategoryCard
-            name="Mild Steel Bar Grating"
-            img="https://www.metalsupermarkets.com/wp-content/uploads/2021/10/Mild-Steel-Bar-Grating-Metal-Supermarkets-150x150.png"
-          />
+          {categoryData?.data?.map((item) => (
+            <CategoryCard name={item.name} img={item.image} key={item.name} />
+          ))}
         </div>
         <p className="font-bold text-2xl mb-5">Popular Brands</p>
-        <p className="font-bold text-2xl">{category} listings</p>
         {/* inserted from marketplace */}
       </div>
     </div>
