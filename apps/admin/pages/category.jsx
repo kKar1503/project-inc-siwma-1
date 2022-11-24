4a2b348adc2382c9a1fa3cbfd68915f28b52cdad
Amos Choo <tohmenchoo@gmail.com>
2022-11-24 23:37:40 +0800

    feat: display data
    display data for avaliable parameters

--- apps/admin/pages/category.jsx
+++ apps/admin/pages/category.jsx
@@ -1,3 +1,4 @@
+import { QueryClientProvider, QueryClient } from 'react-query';
 import ArrowL from '../components/ArrowL';
 import ArrowR from '../components/ArrowR';
 import CreateParam from '../components/CreateParam';
@@ -7,54 +8,32 @@ import NavBar from '../components/NavBar';
 import ActiveParametersTable from '../components/Tables/ActiveParametersTable';
 import AvailableParametersTable from '../components/Tables/AvailableParametersTable';
 
-/**
- * Sample data
- */
-const ActiveParametersData = (id) => ({
-  id,
-  company: 'GUANGZHAO METALWORKS, PTE. LTD',
-  email: 'sallyknox_slfi@gmail.com',
-  mobileNumber: '+65 9832 0293',
-});
-
-const AvailableParametersData = (id) => ({
-  id,
-  company: 'GUANGZHAO METALWORKS, PTE. LTD',
-  website: 'www.guangzhaomw.com',
-});
-
-function populateArray(element, count) {
-  const result = [];
-  for (let i = 0; i < count; i++) {
-    result[i] = element(i + 1);
-  }
-
-  return result;
-}
+const queryClient = new QueryClient();
 
 const CategoryPage = () => (
-  <div className="w-full p-8 gap-8 overflow-auto xl:max-h-screen">
-    {/* <div className="flex flex-col flex-1 w-full p-8 gap-8 overflow-auto xl:max-h-screen"> */}
-    <NavBar />
-    <div className="flex flex-row w-full gap-8 flex-1 justify-between xl:overflow-hidden pb-4 pt-4">
-      <div className="card bg-base-100 shadow-lg w-full rounded-lg p-4">
-        <EditCat />
+  <QueryClientProvider client={queryClient}>
+    <div className="w-full p-8 gap-8 overflow-auto xl:max-h-screen">
+      <NavBar />
+      <div className="flex flex-row w-full gap-8 flex-1 justify-between xl:overflow-hidden pb-4 pt-4">
+        <div className="card bg-base-100 shadow-lg w-full rounded-lg p-4">
+          <EditCat />
+        </div>
+        <div className="card bg-base-100 shadow-lg w-full rounded-lg p-4">
+          <CreateParam />
+        </div>
       </div>
-      <div className="card bg-base-100 shadow-lg w-full rounded-lg p-4">
-        <CreateParam />
-      </div>
-    </div>
-    <div className="flex flex-row w-full gap-8 flex-1 justify-between xl:overflow-hidden items-center">
-      <ActiveParametersTable data={populateArray(ActiveParametersData, 10)} />
-      <div className="flex flex-col">
-        <ArrowR />
-        <br />
-        <ArrowL />
-      </div>
+      <div className="flex flex-row w-full gap-8 flex-1 justify-between xl:overflow-hidden items-center">
+        <ActiveParametersTable />
+        <div className="flex flex-col">
+          <ArrowR />
+          <br />
+          <ArrowL />
+        </div>
 
-      <AvailableParametersTable data={populateArray(AvailableParametersData, 10)} />
+        <AvailableParametersTable />
+      </div>
     </div>
-  </div>
+  </QueryClientProvider>
 );
 
 /**
