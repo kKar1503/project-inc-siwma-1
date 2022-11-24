8565392ed5dbadd4c0424d9da84abc1e1d505f56
Amos Choo <tohmenchoo@gmail.com>
2022-11-24 15:58:46 +0800

    style: used react icons
    use react icons instead of svg for the action buttons

--- apps/admin/components/ActionCat.jsx
+++ apps/admin/components/ActionCat.jsx
@@ -1,24 +1,21 @@
+import { BsThreeDotsVertical, BsFillPencilFill } from 'react-icons/bs';
+import { FaBox } from 'react-icons/fa';
+
 const ActionCat = () => (
   <div className="dropdown dropdown-left">
     <button className="btn btn-ghost">
-      <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 128 512" className="h-5 w-5">
-        <path d="M64 360c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zm0-160c30.9 0 56 25.1 56 56s-25.1 56-56 56s-56-25.1-56-56s25.1-56 56-56zM120 96c0 30.9-25.1 56-56 56S8 126.9 8 96S33.1 40 64 40s56 25.1 56 56z" />
-      </svg>
+      <BsThreeDotsVertical />
     </button>
     <ul className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-40">
       <li>
         <div className="btn btn-ghost w-30">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 512 512" className="w-5">
-            <path d="M410.3 231l11.3-11.3-33.9-33.9-62.1-62.1L291.7 89.8l-11.3 11.3-22.6 22.6L58.6 322.9c-10.4 10.4-18 23.3-22.2 37.4L1 480.7c-2.5 8.4-.2 17.5 6.1 23.7s15.3 8.5 23.7 6.1l120.3-35.4c14.1-4.2 27-11.8 37.4-22.2L387.7 253.7 410.3 231zM160 399.4l-9.1 22.7c-4 3.1-8.5 5.4-13.3 6.9L59.4 452l23-78.1c1.4-4.9 3.8-9.4 6.9-13.3l22.7-9.1v32c0 8.8 7.2 16 16 16h32zM362.7 18.7L348.3 33.2 325.7 55.8 314.3 67.1l33.9 33.9 62.1 62.1 33.9 33.9 11.3-11.3 22.6-22.6 14.5-14.5c25-25 25-65.5 0-90.5L453.3 18.7c-25-25-65.5-25-90.5 0zm-47.4 168l-144 144c-6.2 6.2-16.4 6.2-22.6 0s-6.2-16.4 0-22.6l144-144c6.2-6.2 16.4-6.2 22.6 0s6.2 16.4 0 22.6z" />
-          </svg>
+          <BsFillPencilFill />
           Edit
         </div>
       </li>
       <li>
         <div className="btn btn-ghost w-30">
-          <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512" className="w-5">
-            <path d="M50.7 58.5L0 160H208V32H93.7C75.5 32 58.9 42.3 50.7 58.5zM240 160H448L397.3 58.5C389.1 42.3 372.5 32 354.3 32H240V160zm208 32H0V416c0 35.3 28.7 64 64 64H384c35.3 0 64-28.7 64-64V192z" />
-          </svg>
+          <FaBox />
           Archive
         </div>
       </li>
