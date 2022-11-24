80a391f63958deb8f7a9fdb7f4f7b781f9fd4291
Cat <45095604+catcd1w3r5@users.noreply.github.com>
2022-11-25 06:53:34 +0800

    style: fixed style to look normal in small windows with small widths

--- packages/ui/HambugerMenuSubComponents/SidebarHeader.jsx
+++ packages/ui/HambugerMenuSubComponents/SidebarHeader.jsx
@@ -15,8 +15,12 @@ const SidebarHeaderIcon = ({ closeHandle }) => (
     <div className="p-2.5 mt-1 flex items-center">
       <i className="bi bi-app-indicator px-2 py-1 rounded-md bg-primary-focus" />
       <h1 className="font-bold text-base-200 text-[15px] ml-3">S I W M A</h1>
-      <i className="bi bi-x cursor-pointer ml-28 lg:hidden" />
-      <div className="ml-32" role="presentation" onClick={closeHandle}>
+      {/* <i className="bi bi-x ml-28 lg:hidden" /> */}
+      <div
+        className="ml-32 cursor-pointer inset-y-0 right-0"
+        role="presentation"
+        onClick={closeHandle}
+      >
         <MdClose />
       </div>
     </div>
