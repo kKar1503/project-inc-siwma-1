479b32f14071da8ebffdc9709fdfda97f41e74c8
RuinedRuins <matthew.ngjb@gmail.com>
2022-11-24 12:12:50 +0800

    build: :iphone: category card listings is now responsive

--- apps/web/pages/category/[category].jsx
+++ apps/web/pages/category/[category].jsx
@@ -21,14 +21,14 @@ const Category = () => {
   );
 
   return (
-    <div className="mt-10 ml-28 mr-28 mb-10 h-screen">
+    <div className="m-auto mt-10 mb-10 flex flex-col justify-center w-10/12 h-screen">
       <div className="mb-10 h-1/2">
         <CategoryBanner img="https://images.unsplash.com/photo-1563642900777-eb44ab05eaf8" />
       </div>
       <div className="h-2/3">
         <p className="font-bold text-2xl">{category} listings</p>
         {/* dynamically generate each category when queried */}
-        <div className="flex flex-wrap mb-10">
+        <div className="grid grid-cols-1 md:grid-cols-4 gap-4 sm:grid-cols-2 mb-10">
           {/* image temporarily grabbed from metalsupermarkets.com */}
           <CategoryCard
             name="Carbon Steel Round Tube"
