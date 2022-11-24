c75ba4b24bd413087aed888f96755bb209cf6d1c
RuinedRuins <matthew.ngjb@gmail.com>
2022-11-24 12:12:50 +0800

    build: :iphone: category card listings is now responsive

--- apps/web/components/category/CategoryCard.jsx
+++ apps/web/components/category/CategoryCard.jsx
@@ -3,10 +3,10 @@ import PropTypes from 'prop-types';
 
 const CategoryCard = ({ img, name, href }) => (
   // can add hover:scale-105 if it looks nicer, i'm not sure if it fits thematically
-  <div className="card shadow-md w-1/6 mr-5 mb-5 transition ease-in-out hover:-translate-y-1">
+  <div className="card shadow-md min-w-fit transition ease-in-out hover:-translate-y-1">
     <a href={href}>
-      <picture className="flex place-content-center">
-        <img className="h-[150px] m-5" src={img} alt={name} />
+      <picture className="flex place-content-center h-64">
+        <img className="object-cover w-full h-full" src={img} alt={name} />
       </picture>
       {/* horizontal line to seperate img and text */}
       <hr
