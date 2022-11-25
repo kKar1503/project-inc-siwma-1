0715a08533805bac2f8c8e5ac1e08614951cde59
Amos Choo <tohmenchoo@gmail.com>
2022-11-25 17:42:29 +0800

    feat: dynamic archive button
    archive button data driven

--- apps/admin/components/ActionCat.jsx
+++ apps/admin/components/ActionCat.jsx
@@ -1,7 +1,8 @@
 import { BsThreeDotsVertical, BsFillPencilFill } from 'react-icons/bs';
 import { RiInboxArchiveFill, RiInboxUnarchiveFill } from 'react-icons/ri';
+import PropTypes from 'prop-types';
 
-const ActionCat = () => (
+const ActionCat = ({ data, row }) => (
   <div className="dropdown dropdown-left">
     <button className="btn btn-ghost">
       <BsThreeDotsVertical />
@@ -14,13 +15,13 @@ const ActionCat = () => (
         </div>
       </li>
       <li>
-        <div className="btn btn-ghost w-30">
+        <div className="btn btn-ghost w-30" disabled={data[row].active === 'Active'}>
           <RiInboxArchiveFill />
           Archive
         </div>
       </li>
       <li>
-        <div className="btn btn-ghost w-30">
+        <div className="btn btn-ghost w-30" disabled={data[row].active === 'Disabled'}>
           <RiInboxUnarchiveFill />
           UnArchive
         </div>
@@ -29,4 +30,11 @@ const ActionCat = () => (
   </div>
 );
 
+const propTypes = {
+  row: PropTypes.string,
+  data: PropTypes.arrayOf(PropTypes.object), // eslint-disable-line react/forbid-prop-types
+  // We don't know what the data object will look like, so we can't specify it.
+};
+
+ActionCat.propTypes = propTypes;
 export default ActionCat;
