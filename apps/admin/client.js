0aba83885612fef865de8b3b87953c68db2f90f7
Wu Yining <yyiningsp@gmail.com>
2022-11-26 16:40:04 +0800

    chore: change of database
    change of database url and key

--- apps/admin/client.js
+++ apps/admin/client.js
@@ -1,8 +1,8 @@
 import { createClient } from '@supabase/supabase-js';
 
 const supabase = createClient(
-  `https://iecgehqhgqgpiejchgxu.supabase.co`,
-  `eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImllY2dlaHFoZ3FncGllamNoZ3h1Iiwicm9sZSI6ImFub24iLCJpYXQiOjE2Njc4OTE1NTIsImV4cCI6MTk4MzQ2NzU1Mn0.2fQdXsv9iPZNLeiAaALP87s_tJZ83UalXyMpIOICymg`
+  process.env.NEXT_PUBLIC_SUPABASE_URL,
+  process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY
 );
 
 export default supabase;
