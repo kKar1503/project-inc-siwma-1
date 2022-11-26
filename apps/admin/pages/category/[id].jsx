0a5924ad72ad1b81267d6857468af9f937a50791
Amos Choo <tohmenchoo@gmail.com>
2022-11-26 19:53:37 +0800

    style: display edit category form
    display form

--- apps/admin/pages/category/[id].jsx
+++ apps/admin/pages/category/[id].jsx
@@ -14,14 +14,14 @@ const queryClient = new QueryClient();
 const CategoryPage = () => {
   const router = useRouter();
   const { id } = router.query;
-  console.log(id);
+
   return (
     <QueryClientProvider client={queryClient}>
       <div className="w-full p-8 gap-8 overflow-auto xl:max-h-screen">
         <NavBar />
         <div className="flex flex-row w-full gap-8 flex-1 justify-between xl:overflow-hidden pb-4 pt-4">
           <div className="card bg-base-100 shadow-lg w-full rounded-lg p-4">
-            <EditCat />
+            <EditCat id={id} />
           </div>
           <div className="card bg-base-100 shadow-lg w-full rounded-lg p-4">
             <CreateParam />
