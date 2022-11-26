32c8bd235a59a7638567d31f55ee2874a63c583c
RuinedRuins <matthew.ngjb@gmail.com>
2022-11-26 17:19:38 +0800

    build: :sparkles: init component test page for profile listing
    test page, will remove later
    
    db doesn't have column for author so will display all listings for now

---
+++ apps/web/pages/componenttest.jsx
@@ -0,0 +1,14 @@
+import ProfileListing from '../components/profile/profileListing';
+
+const ComponentTest = () => {
+  console.log('test');
+
+  return (
+    <div>
+      <h1>test</h1>
+      <ProfileListing />
+    </div>
+  );
+};
+
+export default ComponentTest;
