23bacb1cbd92886fc6331d7c9fe38a1603f89814
Xavier Teo <xavierteo26@gmail.com>
2022-11-28 01:06:17 +0800

    feat: :sparkles: retrieve company data using id passed through query param

--- packages/database/index.js
+++ packages/database/index.js
@@ -1,3 +1,4 @@
 export { default as getAllCompanies } from './getAllCompanies';
 export { default as getCompanyCount } from './getCompanyCount';
+export { default as getCompany } from './getCompany';
 export { default as supabase } from './supabase';
