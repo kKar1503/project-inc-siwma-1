435553b53b1fd393452d9f36b4d933a5fac7bf15
Xavier Teo <xavierteo26@gmail.com>
2022-11-27 19:09:40 +0800

    feat: :sparkles: add support for table action menus
    - also split up the base table component into smaller components

---
+++ apps/admin/components/Tables/BaseTable/BaseTableRow.jsx
@@ -0,0 +1,148 @@
+import Image from 'next/image';
+import cx from 'classnames';
+import PropTypes from 'prop-types';
+import { ImCheckmark, ImCross } from 'react-icons/im';
+import { HiDotsVertical } from 'react-icons/hi';
+import React, { useEffect, useRef, useState } from 'react';
+
+/**
+ * Row component for BaseTable
+ * Not the best because a lot of props are passed in lol
+ * @param {object} data Data for the row
+ * @param {boolean} showCheckbox Whether or not to render a checkbox for the row
+ * @param {[string]} columnKeys Keys for each table column
+ * @param {[string]} headings The heading for each table column
+ * @param {[string]} centerColumns The columns to be centered
+ * @param {boolean} selected Whether or not the row is currently selected
+ * @param {React.ReactNode} actionMenu The action menu to display if the action button is clicked on (Action button only renders if this prop is specified; Setting this prop makes the tableRef prop required)
+ * @param {React.Ref} tableRef Reference to the table (Required if the actionMenu prop is set)
+ * @param {()} onChange The function to invoke when the row gets selected/deselected
+ * @type {React.FC<PropTypes.InferProps<typeof propTypes>>}
+ * @returns A table row
+ */
+const BaseTableRow = React.forwardRef(({ data, showCheckbox, columnKeys, headings, centerColumns, selected, actionMenu, onChange }, tableRef) => {
+  // States
+  const [showActionMenu, setShowActionMenu] = useState(false);
+
+  // References
+  const actionMenuRef = useRef();
+
+  // -- Hooks -- //
+  useEffect(() => {
+    // -- Determine where the offset the action menu should have, such that it does not exit the bounds of the table -- //
+    // Retrieve the coordinates of the table component and action menu component
+    const tableCoords = tableRef.current.getBoundingClientRect();
+    const actionMenuCoords = actionMenuRef.current.getBoundingClientRect();
+
+    // Check if the action menu is outside the bounding box of the table
+    if (actionMenuCoords.top < tableCoords.top || actionMenuCoords.right > tableCoords.right || actionMenuCoords.bottom > tableCoords.bottom || actionMenuCoords.left < tableCoords.left) {
+      // It is outside the bounding box
+      // Calculate the required vertical offset for the actionMenu, such that it is within the bounds of the table
+      actionMenuRef.current.style.top = 'auto';
+      actionMenuRef.current.style.bottom = `30px`;
+    }
+  });
+
+  return (
+    <tr>
+      {showCheckbox && (
+        <td>
+          <label>
+            <input
+              type="checkbox"
+              className="checkbox"
+              onChange={(e) => onChange(data, e.currentTarget.checked)}
+              // Give the checkbox a checked state if the current row being rendered is selected
+              defaultChecked={selected}
+            />
+          </label>
+        </td>
+      )}
+      {columnKeys.map((key, index) => (
+        <td key={key}>
+          <div className={cx('flex flex-data')}>
+            {/* Show a profilePicture if one exists and if we're on the first column */}
+            {index === 0 && data.profilePicture && (
+              <div className="w-10 h-10 mr-4">
+                <Image
+                  src={data.profilePicture}
+                  alt="Profile Picture"
+                  layout="fill"
+                  width={100}
+                  height={100}
+                  className="rounded-full aspect-square object-cover"
+                />
+              </div>
+            )}
+            {
+              // Vertically center text and horizontally center text (if required)
+              // Determines if the text should be horizontally centered by checking if the centerColumns array includes the name of the heading this column is being rendered for
+            }
+            <div
+              className={cx('flex align-middle items-center', {
+                'mx-auto': centerColumns
+                  ? centerColumns.includes(headings[index])
+                  : false,
+              })}
+            >
+              {
+                // Render the value of the column (will not render anything if the value is a boolean)
+                data[key]
+              }
+              {/* Render boolean values as a tick or a cross */}
+              {
+                // Render a tick if boolean value is true
+                typeof data[key] === 'boolean' && data[key] && (
+                  <ImCheckmark className="text-success" />
+                )
+              }
+              {
+                // Render a cross if boolean value is false
+                typeof data[key] === 'boolean' && !data[key] && (
+                  <ImCross className="text-error" />
+                )
+              }
+            </div>
+          </div>
+        </td>
+      ))}
+      {
+        // Render the action button if the actionMenu prop is specified
+        actionMenu && (
+          <td>
+            <button
+              type="button"
+              className="flex items-center flex-grow-0 mx-auto text-center gap-2"
+              onClick={() => setShowActionMenu(!showActionMenu)}
+            >
+              <HiDotsVertical />
+            </button>
+            {/* Action Menu */}
+            <div className='relative'>
+              <div className={cx('absolute right-10 top-0 h-fit', { 'invisible': !showActionMenu })} ref={actionMenuRef}>
+                {actionMenu}
+              </div>
+            </div>
+          </td>
+        )
+      }
+    </tr >
+  )
+});
+
+const propTypes = {
+  // We don't know what the shape of the object will be, so we can't specify it
+  // eslint-disable-next-line react/forbid-prop-types
+  data: PropTypes.object.isRequired,
+  showCheckbox: PropTypes.bool,
+  columnKeys: PropTypes.arrayOf(PropTypes.string).isRequired,
+  centerColumns: PropTypes.arrayOf(PropTypes.string),
+  headings: PropTypes.arrayOf(PropTypes.string),
+  selected: PropTypes.bool,
+  actionMenu: PropTypes.node,
+  onChange: PropTypes.func,
+}
+
+BaseTableRow.propTypes = propTypes;
+
+export default BaseTableRow;
--- apps/admin/components/Tables/RegisteredCompaniesTable.jsx
+++ apps/admin/components/Tables/RegisteredCompaniesTable.jsx
@@ -4,10 +4,10 @@ import { useQueries, useQueryClient } from 'react-query';
 import cx from 'classnames';
 import { getAllCompanies, getCompanyCount } from '@inc/database';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
-import BaseTable from './BaseTable';
+import { BaseTable } from './BaseTable';
 import SearchBar from '../SearchBar';
-import TableButton from './TableButton';
 import TablePagination from './TablePagination';
+import TableMenu from './TableMenu';
 
 /**
  * Parses data retrieved from Supabase into a format accepted by the tables
@@ -17,9 +17,7 @@ import TablePagination from './TablePagination';
 function parseData(data) {
   return data.map((e) => ({
     id: e.id,
-    profilePicture: `https://spoxwyiorgijkrqidutq.supabase.co/storage/v1/object/public/companyprofilepictures/${
-      e.image || 'example.jpg'
-    }`,
+    profilePicture: `https://spoxwyiorgijkrqidutq.supabase.co/storage/v1/object/public/companyprofilepictures/${e.image || 'example.jpg'}`,
     company: e.name,
     website: e.website,
     bio: e.bio,
@@ -200,6 +198,28 @@ const RegisteredCompaniesTable = ({ className }) => {
       isLoading={isLoading}
       data={companies}
       onChange={onChangeHandler}
+      actionMenu={
+        <TableMenu>
+          <li>
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M3 12l2-2m0 0l7-7 7 7M5 10v10a1 1 0 001 1h3m10-11l2 2m-2-2v10a1 1 0 01-1 1h-3m-6 0a1 1 0 001-1v-4a1 1 0 011-1h2a1 1 0 011 1v4a1 1 0 001 1m-6 0h6" /></svg>
+              Item 2
+            </button>
+          </li>
+          <li>
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z" /></svg>
+              Item 1
+            </button>
+          </li>
+          <li>
+            <button>
+              <svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path strokeLinecap="round" strokeLinejoin="round" strokeWidth="2" d="M9 19v-6a2 2 0 00-2-2H5a2 2 0 00-2 2v6a2 2 0 002 2h2a2 2 0 002-2zm0 0V9a2 2 0 012-2h2a2 2 0 012 2v10m-6 0a2 2 0 002 2h2a2 2 0 002-2m0 0V5a2 2 0 012-2h2a2 2 0 012 2v14a2 2 0 01-2 2h-2a2 2 0 01-2-2z" /></svg>
+              Item 3
+            </button>
+          </li>
+        </TableMenu>
+      }
       footer={
         <div className="flex justify-between bg-none">
           {/* Company suspension/reinstation */}
