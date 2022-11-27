0de2b3e8585ee5777ca14f0623b94d6562dcf6ef
Amos Choo <tohmenchoo@gmail.com>
2022-11-28 04:44:29 +0800

    feat: change param
    make params active and available

--- apps/admin/components/Arrows.jsx
+++ apps/admin/components/Arrows.jsx
@@ -55,6 +55,51 @@ const Arrows = ({ id }) => {
     setOptions([]);
   };
 
+  // Can use next time?
+  // const useMakeAvailable = async (e) => {
+  //   e.preventDefault();
+  //   const options = [];
+  //   optionData.options.map((item) => options.push(item));
+  //   options.toString();
+  //   return useMutation(
+  //     () =>
+  //       supabase
+  //         .from('categories_parameters')
+  //         .delete()
+  //         .eq('category', id)
+  //         .filter('parameter', 'in', `(${options})`),
+  //     {
+  //       onSuccess: () => {
+  //         queryClient.invalidateQueries({ queryKey: ['activeParameters', 'availableParameters'] });
+  //       },
+  //     }
+  //   );
+  // };
+
+  const makeAvailable = async (e) => {
+    const options = [];
+    optionData.options.map((item) => options.push(item));
+    options.toString();
+    await supabase
+      .from('categories_parameters')
+      .delete()
+      .eq('category', id)
+      .filter('parameter', 'in', `(${options})`);
+    queryClient.invalidateQueries({ queryKey: ['activeParameters'] });
+    queryClient.invalidateQueries({ queryKey: ['categoryParameters'] });
+    paramId();
+  };
+
+  const makeActive = async (e) => {
+    const options = [];
+    optionData.options.map((item) => options.push({ category: id, parameter: item }));
+    await supabase.from('categories_parameters').insert(options);
+
+    queryClient.invalidateQueries({ queryKey: ['activeParameters'] });
+    queryClient.invalidateQueries({ queryKey: ['categoryParameters'] });
+    paramId();
+  };
+
   return (
     <div className="flex flex-col gap-5">
       <button
