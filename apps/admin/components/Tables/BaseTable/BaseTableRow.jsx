d0acc66f91a0f85a60b94efedda55d2f57323a7e
Xavier Teo <xavierteo26@gmail.com>
2022-11-27 19:57:33 +0800

    style: :lipstick: change right offset of table action menu
    makes it look nicer

--- apps/admin/components/Tables/BaseTable/BaseTableRow.jsx
+++ apps/admin/components/Tables/BaseTable/BaseTableRow.jsx
@@ -20,115 +20,124 @@ import React, { useEffect, useRef, useState } from 'react';
  * @type {React.FC<PropTypes.InferProps<typeof propTypes>>}
  * @returns A table row
  */
-const BaseTableRow = React.forwardRef(({ data, showCheckbox, columnKeys, headings, centerColumns, selected, actionMenu, onChange }, tableRef) => {
-  // States
-  const [showActionMenu, setShowActionMenu] = useState(false);
+const BaseTableRow = React.forwardRef(
+  (
+    { data, showCheckbox, columnKeys, headings, centerColumns, selected, actionMenu, onChange },
+    tableRef
+  ) => {
+    // States
+    const [showActionMenu, setShowActionMenu] = useState(false);
 
-  // References
-  const actionMenuRef = useRef();
+    // References
+    const actionMenuRef = useRef();
 
-  // -- Hooks -- //
-  useEffect(() => {
-    // -- Determine where the offset the action menu should have, such that it does not exit the bounds of the table -- //
-    // Retrieve the coordinates of the table component and action menu component
-    const tableCoords = tableRef.current.getBoundingClientRect();
-    const actionMenuCoords = actionMenuRef.current.getBoundingClientRect();
+    // -- Hooks -- //
+    useEffect(() => {
+      // -- Determine where the offset the action menu should have, such that it does not exit the bounds of the table -- //
+      // Retrieve the coordinates of the table component and action menu component
+      const tableCoords = tableRef.current.getBoundingClientRect();
+      const actionMenuCoords = actionMenuRef.current.getBoundingClientRect();
 
-    // Check if the action menu is outside the bounding box of the table
-    if (actionMenuCoords.top < tableCoords.top || actionMenuCoords.right > tableCoords.right || actionMenuCoords.bottom > tableCoords.bottom || actionMenuCoords.left < tableCoords.left) {
-      // It is outside the bounding box
-      // Calculate the required vertical offset for the actionMenu, such that it is within the bounds of the table
-      actionMenuRef.current.style.top = 'auto';
-      actionMenuRef.current.style.bottom = `30px`;
-    }
-  });
+      // Check if the action menu is outside the bounding box of the table
+      if (
+        actionMenuCoords.top < tableCoords.top ||
+        actionMenuCoords.right > tableCoords.right ||
+        actionMenuCoords.bottom > tableCoords.bottom ||
+        actionMenuCoords.left < tableCoords.left
+      ) {
+        // It is outside the bounding box
+        // Calculate the required vertical offset for the actionMenu, such that it is within the bounds of the table
+        actionMenuRef.current.style.top = 'auto';
+        actionMenuRef.current.style.bottom = `30px`;
+      }
+    });
 
-  return (
-    <tr>
-      {showCheckbox && (
-        <td>
-          <label>
-            <input
-              type="checkbox"
-              className="checkbox"
-              onChange={(e) => onChange(data, e.currentTarget.checked)}
-              // Give the checkbox a checked state if the current row being rendered is selected
-              defaultChecked={selected}
-            />
-          </label>
-        </td>
-      )}
-      {columnKeys.map((key, index) => (
-        <td key={key}>
-          <div className={cx('flex flex-data')}>
-            {/* Show a profilePicture if one exists and if we're on the first column */}
-            {index === 0 && data.profilePicture && (
-              <div className="w-10 h-10 mr-4">
-                <Image
-                  src={data.profilePicture}
-                  alt="Profile Picture"
-                  layout="fill"
-                  width={100}
-                  height={100}
-                  className="rounded-full aspect-square object-cover"
-                />
-              </div>
-            )}
-            {
-              // Vertically center text and horizontally center text (if required)
-              // Determines if the text should be horizontally centered by checking if the centerColumns array includes the name of the heading this column is being rendered for
-            }
-            <div
-              className={cx('flex align-middle items-center', {
-                'mx-auto': centerColumns
-                  ? centerColumns.includes(headings[index])
-                  : false,
-              })}
-            >
-              {
-                // Render the value of the column (will not render anything if the value is a boolean)
-                data[key]
-              }
-              {/* Render boolean values as a tick or a cross */}
-              {
-                // Render a tick if boolean value is true
-                typeof data[key] === 'boolean' && data[key] && (
-                  <ImCheckmark className="text-success" />
-                )
-              }
-              {
-                // Render a cross if boolean value is false
-                typeof data[key] === 'boolean' && !data[key] && (
-                  <ImCross className="text-error" />
-                )
-              }
-            </div>
-          </div>
-        </td>
-      ))}
-      {
-        // Render the action button if the actionMenu prop is specified
-        actionMenu && (
+    return (
+      <tr>
+        {showCheckbox && (
           <td>
-            <button
-              type="button"
-              className="flex items-center flex-grow-0 mx-auto text-center gap-2"
-              onClick={() => setShowActionMenu(!showActionMenu)}
-            >
-              <HiDotsVertical />
-            </button>
-            {/* Action Menu */}
-            <div className='relative'>
-              <div className={cx('absolute right-10 top-0 h-fit', { 'invisible': !showActionMenu })} ref={actionMenuRef}>
-                {actionMenu}
+            <label>
+              <input
+                type="checkbox"
+                className="checkbox"
+                onChange={(e) => onChange(data, e.currentTarget.checked)}
+                // Give the checkbox a checked state if the current row being rendered is selected
+                defaultChecked={selected}
+              />
+            </label>
+          </td>
+        )}
+        {columnKeys.map((key, index) => (
+          <td key={key}>
+            <div className={cx('flex flex-data')}>
+              {/* Show a profilePicture if one exists and if we're on the first column */}
+              {index === 0 && data.profilePicture && (
+                <div className="w-10 h-10 mr-4">
+                  <Image
+                    src={data.profilePicture}
+                    alt="Profile Picture"
+                    layout="fill"
+                    width={100}
+                    height={100}
+                    className="rounded-full aspect-square object-cover"
+                  />
+                </div>
+              )}
+              {
+                // Vertically center text and horizontally center text (if required)
+                // Determines if the text should be horizontally centered by checking if the centerColumns array includes the name of the heading this column is being rendered for
+              }
+              <div
+                className={cx('flex align-middle items-center', {
+                  'mx-auto': centerColumns ? centerColumns.includes(headings[index]) : false,
+                })}
+              >
+                {
+                  // Render the value of the column (will not render anything if the value is a boolean)
+                  data[key]
+                }
+                {/* Render boolean values as a tick or a cross */}
+                {
+                  // Render a tick if boolean value is true
+                  typeof data[key] === 'boolean' && data[key] && (
+                    <ImCheckmark className="text-success" />
+                  )
+                }
+                {
+                  // Render a cross if boolean value is false
+                  typeof data[key] === 'boolean' && !data[key] && <ImCross className="text-error" />
+                }
               </div>
             </div>
           </td>
-        )
-      }
-    </tr >
-  )
-});
+        ))}
+        {
+          // Render the action button if the actionMenu prop is specified
+          actionMenu && (
+            <td>
+              <button
+                type="button"
+                className="flex items-center flex-grow-0 mx-auto text-center gap-2"
+                onClick={() => setShowActionMenu(!showActionMenu)}
+              >
+                <HiDotsVertical />
+              </button>
+              {/* Action Menu */}
+              <div className="relative">
+                <div
+                  className={cx('absolute right-[6vw] top-0 h-fit', { invisible: !showActionMenu })}
+                  ref={actionMenuRef}
+                >
+                  {actionMenu}
+                </div>
+              </div>
+            </td>
+          )
+        }
+      </tr>
+    );
+  }
+);
 
 const propTypes = {
   // We don't know what the shape of the object will be, so we can't specify it
@@ -141,7 +150,7 @@ const propTypes = {
   selected: PropTypes.bool,
   actionMenu: PropTypes.node,
   onChange: PropTypes.func,
-}
+};
 
 BaseTableRow.propTypes = propTypes;
 
