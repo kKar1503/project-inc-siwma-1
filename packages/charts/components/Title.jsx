981b5eb3ef564eb8bed3771b64534e738fc7dbae
MrsDarth <7jgbm234@gmail.com>
2022-11-28 05:08:29 +0800

    build: chart hover and data fetch

--- packages/charts/components/Title.jsx
+++ packages/charts/components/Title.jsx
@@ -2,7 +2,6 @@ import PropTypes from 'prop-types';
 import { VictoryLabel } from 'victory';
 
 const Title = ({ text }) => (
-  // eslint-disable-next-line react/jsx-props-no-spreading
   <VictoryLabel x={200} y={20} style={{ fontSize: 20 }} textAnchor="middle" text={text} />
 );
 
--- packages/charts/components/companies/MostPostsChart.jsx
+++ packages/charts/components/companies/MostPostsChart.jsx
@@ -1,26 +1,52 @@
 import PropTypes from 'prop-types';
 import { VictoryAxis, VictoryBar, VictoryChart } from 'victory';
 import Title from '../Title';
+import { BarLine, BarToolTip, focus, reset, unfocus } from '../BarHover';
 
 const colors = ['#2563EB', '#497DEE', '#6D96F2', '#91B0F5'];
 
-const mockData = [
-  { company: 'A&G EQUIPMENT PTE.LTD.', posts: 32 },
-  { company: 'ACE-WELD PTE.LTD.', posts: 25 },
-  { company: 'FUJIN PTE.LTD.', posts: 26 },
-  { company: 'HANWA SINGAPORE (PTE.)LTD.', posts: 23 },
-];
+// const mockData = [
+//   { company: 'A&G EQUIPMENT PTE.LTD.', posts: 32 },
+//   { company: 'ACE-WELD PTE.LTD.', posts: 25 },
+//   { company: 'FUJIN PTE.LTD.', posts: 26 },
+//   { company: 'HANWA SINGAPORE (PTE.)LTD.', posts: 23 },
+// ];
 
-const MostPostsBarChart = ({ data = mockData }) => (
+const MostPostsBarChart = ({ data }) => (
   <VictoryChart domainPadding={50}>
     <Title text="Top 4 Companies With The Most Posts" />
     <VictoryAxis dependentAxis style={{ grid: { stroke: '#DADADA' } }} tickCount={7} />
     <VictoryAxis dependentAxis label="Posts" tickCount={7} />
     <VictoryBar
+      labels={data.map(({ posts }) => posts)}
+      labelComponent={<BarToolTip />}
+      events={[
+        {
+          target: 'data',
+          eventHandlers: {
+            onMouseEnter: () => [
+              {
+                eventKey: 'all',
+                ...unfocus,
+              },
+              focus,
+            ],
+            onMouseLeave: () => [
+              {
+                eventKey: 'all',
+                ...reset,
+              },
+            ],
+          },
+        },
+      ]}
       data={data}
+      dataComponent={<BarLine />}
       x="company"
       y="posts"
-      style={{ data: { fill: (row) => colors[row.index] } }}
+      style={{
+        data: { fill: (row) => colors[row.index], ...(data.length === 1 && { width: 30 }) },
+      }}
     />
     <VictoryAxis style={{ tickLabels: { fontSize: 8 } }} label="Company" />
   </VictoryChart>
