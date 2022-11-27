7885069f885fb8041af6fac6388da74ae8f508aa
Amos Choo <tohmenchoo@gmail.com>
2022-11-28 03:43:23 +0800

    feat: disabled buttons
    disabled arrow buttons

--- apps/admin/components/Arrows.jsx
+++ apps/admin/components/Arrows.jsx
@@ -3,20 +3,24 @@ import { AiOutlineArrowLeft, AiOutlineArrowRight } from 'react-icons/ai';
 import PropTypes from 'prop-types';
 import supabase from '../supabaseClient';
 
-const Arrows = ({ optionData }) => {
+const Arrows = ({ id, optionData }) => {
   const queryClient = useQueryClient();
 
   return (
     <div className="flex flex-col gap-5">
       <button
         className="btn btn-ghost bg-base-100 rounded-lg shadow-lg h-12 w-12 self-center items-center"
-        disabled={optionData !== undefined ? optionData.table === 'Active' : false}
+        disabled={
+          optionData === undefined || (optionData !== undefined && optionData.table === 'Active')
+        }
       >
         <AiOutlineArrowLeft />
       </button>
       <button
         className="btn btn-ghost bg-base-100 rounded-lg shadow-lg h-12 w-12 self-center items-center"
-        disabled={optionData !== undefined ? optionData.table === 'Available' : false}
+        disabled={
+          optionData === undefined || (optionData !== undefined && optionData.table === 'Available')
+        }
       >
         <AiOutlineArrowRight />
       </button>
@@ -25,6 +29,7 @@ const Arrows = ({ optionData }) => {
 };
 
 const propTypes = {
+  id: PropTypes.string,
   optionData: PropTypes.shape({
     options: PropTypes.arrayOf(PropTypes.number),
     table: PropTypes.string,
