5979fd4d73169b56703727b198373d5125c93ca1
Shelby <yeoxhshelby20.21@ichat.sp.edu.sg>
2022-11-27 22:00:24 +0800

    feat: :sparkles: added working real time chat functionality

--- apps/web/components/rtc/ChatBubbles.jsx
+++ apps/web/components/rtc/ChatBubbles.jsx
@@ -1,46 +1,76 @@
-const bluemessages = [
-  { message: 'Hi' },
-  { message: 'I am interested in the item.' },
-  { message: 'Could we negotiate on the cost of the items?' },
-];
+import { useEffect, useState, useRef } from 'react';
+import { createClient } from '@supabase/supabase-js';
 
-const greymessages = [
-  {
-    message:
-      'Hello! What price point would you be comfortable with paying? If a good price is offered, we can deal.',
-  },
-];
+const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL;
+const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY;
+const supabase = createClient(supabaseUrl, supabaseKey);
 
-//
+const ChatBubbles = (messages) => {
+  const [user, setUser] = useState('');
+  const [allMsg, setAllMsg] = useState([]);
+  const bottomRef = useRef(null);
 
-const ChatBubbles = () => (
-  <div>
-    <div className="bg-slate-50 items-center">
-      {bluemessages.map((msg) => (
-        <div className="flex justify-end mx-5">
-          <div className="bg-primary rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl w-auto my-3">
-            <div className="card-body py-4 px-6">
-              <p className="min-w-fit max-w-sm min-[320px]:text-[0.8em] sm:text-[0.8em] md:text-[0.9em] lg:text-[1em]">
-                {msg.message}
-              </p>
+  const getUser = async () => {
+    const {
+      data: { userData },
+    } = await supabase.auth.getUser();
+    console.log(userData);
+    setUser('de47e6c2-0591-44b1-9b2c-7359e9f2fc55');
+  };
+
+  const fetchUserAndMsg = async () => {
+    const { data: content, error } = await supabase.from('messages').select(`
+      profile_uuid,
+      contents (*)
+    `);
+
+    if (error) {
+      console.log(error);
+    } else {
+      console.log(content);
+      setAllMsg(content);
+    }
+  };
+
+  useEffect(() => {
+    getUser();
+    fetchUserAndMsg();
+  }, [messages]);
+
+  useEffect(() => {
+    bottomRef.current?.scrollIntoView({ behavior: 'smooth' });
+  }, [messages, allMsg]);
+
+  return (
+    <div>
+      <div className="bg-slate-50 items-center">
+        {allMsg.map((msg) => (
+          <div
+            className={
+              msg.profile_uuid === user ? 'flex justify-end mx-5' : 'flex justify-start mx-5'
+            }
+            key={msg.contents.content_id}
+          >
+            <div
+              className={
+                msg.profile_uuid === user
+                  ? 'bg-info rounded-tl-3xl rounded-tr-3xl rounded-bl-3xl w-auto my-3 bottom-2'
+                  : 'bg-base-300 rounded-tl-3xl rounded-tr-3xl rounded-br-3xl w-auto my-3 bottom-2'
+              }
+            >
+              <div className="card-body py-4 px-6">
+                <p className="min-w-fit max-w-sm min-[320px]:text-[0.8em] sm:text-[0.8em] md:text-[0.9em] lg:text-[1em]">
+                  {msg.contents.text}
+                </p>
+              </div>
             </div>
           </div>
-        </div>
-      ))}
+        ))}
+      </div>
 
-      {greymessages.map((msg) => (
-        <div className="flex justify-start mx-5">
-          <div className="bg-secondary rounded-tl-3xl rounded-tr-3xl rounded-br-3xl w-auto my-3">
-            <div className="py-4 px-6">
-              <p className="min-w-fit max-w-sm min-[320px]:text-[0.8em] sm:text-[0.8em] md:text-[0.9em] lg:text-[1em]">
-                {msg.message}
-              </p>
-            </div>
-          </div>
-        </div>
-      ))}
+      <div ref={bottomRef} />
     </div>
-  </div>
-);
+  );
+};
 
 export default ChatBubbles;
