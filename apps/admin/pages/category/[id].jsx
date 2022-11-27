fb609e5c675c116ef1adb84d98990ed0cd8c261b
Amos Choo <tohmenchoo@gmail.com>
2022-11-28 01:46:53 +0800

    feat: checkbox check
    check if checkbox is clicked

--- apps/admin/pages/category/[id].jsx
+++ apps/admin/pages/category/[id].jsx
@@ -14,6 +14,9 @@ const queryClient = new QueryClient();
 const CategoryPage = () => {
   const router = useRouter();
   const { id } = router.query;
+  const paramId = (data) => {
+    console.log(data);
+  };
 
   return (
     <QueryClientProvider client={queryClient}>
@@ -28,11 +31,11 @@ const CategoryPage = () => {
           </div>
         </div>
         <div className="flex flex-row w-full gap-8 flex-1 justify-between xl:overflow-hidden items-center">
-          <ActiveParametersTable id={id} />
+          <ActiveParametersTable id={id} paramId={paramId} />
           <div className="flex flex-col">
             <Arrows />
           </div>
-          <AvailableParametersTable id={id} />
+          <AvailableParametersTable id={id} paramId={paramId} />
         </div>
       </div>
     </QueryClientProvider>
