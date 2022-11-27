a1c7bda4a8715522b6525750ec0e1510bf04a1cc
Nabil Ridhwan <nabridhwan@gmail.com>
2022-11-27 17:46:53 +0800

    feat: :sparkles: changed text for BuyBadge and SellBadge to 'buy' and 'sell'

--- apps/web/components/marketplace/listing/BuyBadge.jsx
+++ apps/web/components/marketplace/listing/BuyBadge.jsx
@@ -1,5 +1,5 @@
 const BuyBadge = () => (
-  <div className="badge badge-md badge-success text-white rounded-lg">Buy</div>
+  <div className="badge badge-md badge-success text-white rounded-lg shadow-md">Buy</div>
 );
 
 export default BuyBadge;
