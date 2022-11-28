e64301ca2c580101b5502b90b744282bc42e12df
notlega <onghinjourn@gmail.com>
2022-11-28 18:30:50 +0800

    feat: :sparkles: working react query to get listing
    this query gets the listing by the name of the product

--- apps/web/pages/listing/[id].jsx
+++ apps/web/pages/listing/[id].jsx
@@ -12,6 +12,28 @@ const Listing = () => {
   const [user, setUser] = React.useState(null);
   const [carouselImages, setCarouselImages] = React.useState([]);
 
+  const {
+    data: listingData,
+    isError: listingError,
+    isLoading: listingLoading,
+    status: listingStatus,
+  } = useQuery(
+    ['get_listing_by_name', query.id],
+    () => client.rpc('get_listing_by_name', { listing_name: query.id }),
+    {
+      enabled: isReady,
+      refetchOnMount: false,
+      refetchOnWindowFocus: false,
+      refetchOnReconnect: false,
+    }
+  );
+
+  React.useEffect(() => {
+    if (listingStatus === 'success') {
+      setListing(listingData.data);
+    }
+  }, [listingData, listingStatus]);
+
   return (
     <main>
       <h1>query.id</h1>
