170d9c17b17ca20f549cca995760a139fe45ff69
Kaung Khant Zaw <mmmike2002@gmail.com>
2022-11-28 21:17:13 +0800

    feat: :construction: setting data
    also change schema for supabase api

--- apps/admin/pages/edit-user.jsx
+++ apps/admin/pages/edit-user.jsx
@@ -1,5 +1,5 @@
 import { useMemo, useState } from 'react';
-import { useQueries, useQueryClient } from 'react-query';
+import { QueryClientProvider, useQueries, QueryClient, useQuery } from 'react-query';
 import PropTypes from 'prop-types';
 import Image from 'next/image';
 import { useRouter } from 'next/router';
@@ -12,7 +12,6 @@ import ToggleSelect from '../components/FormControl/ToggleSelect';
 import supabase from './api/supabase';
 
 function parseUserData(data) {
-  console.log(data);
   return {
     ...data,
   };
@@ -31,40 +30,46 @@ function parseCommentData(data) {
 }
 
 const EditUser = () => {
-  const { query } = useRouter();
+  const router = useRouter();
+  const { userid } = router.query;
 
-  const queryClient = useQueryClient();
+  const queryClient = new QueryClient();
 
   // SET ALL USER DATA HERE (PROBABLY USE THE API CALL TO GET USER DATA)
-  const [getUserInfoQuery, getCompaniesQuery, getCommentQuery] = useQueries([
+  const [getUserQuery, getCompaniesQuery, getCommentQuery] = useQueries([
     {
-      queryKey: ['getUserInfo'],
-      queryFn: async () =>
-        supabase.from('users').select(`*, companies:companyid(name)`).eq('id', query.userid),
+      queryKey: ['getUser', userid],
+      queryFn: async () => supabase.from('users').select('*').eq('id', userid),
+      keepPreviousData: true,
     },
     {
       queryKey: ['getCompanies'],
       queryFn: async () => supabase.from('companies').select('*'),
+      keepPreviousData: true,
     },
     {
-      queryKey: ['getComment'],
-      queryFn: async () => supabase.from('users_comments').select('*').eq('id', query.userid),
+      queryKey: ['getComment', userid],
+      queryFn: async () => supabase.from('users_comments').select('*').eq('userid', userid),
+      keepPreviousData: true,
     },
   ]);
 
   const user =
-    getUserInfoQuery.isLoading || !getUserInfoQuery.data
+    getUserQuery.isLoading || !getUserQuery.data.data
       ? {}
-      : parseUserData(getUserInfoQuery.data.data[0]);
+      : parseUserData(getUserQuery.data.data[0]);
 
-  const companies = getCompaniesQuery.isLoading
-    ? []
-    : parseCompanyData(getCompaniesQuery.data.data);
+  console.log(user);
+
+  const companies =
+    getCompaniesQuery.isLoading || !getCompaniesQuery.data.data
+      ? []
+      : parseCompanyData(getCompaniesQuery.data.data);
 
   const comment =
-    getCommentQuery.isLoading || !getCommentQuery.data
+    getCommentQuery.isLoading || !getCommentQuery.data.data
       ? {}
-      : parseCommentData(getCommentQuery.data.data);
+      : parseCommentData(getCommentQuery.data.data[0]);
 
   const profilePic =
     'https://rvndpcxlgtqfvrxhahnm.supabase.co/storage/v1/object/public/company-image-bucket/example.jpg';
@@ -81,74 +86,74 @@ const EditUser = () => {
   }, [selectedFile]);
 
   return (
-    <div className="flex flex-col w-full h-full gap-8 p-8 overflow-auto xl:max-h-screen">
-      <NavBar />
+    <QueryClientProvider client={queryClient}>
+      <div className="flex flex-col w-full h-full gap-8 p-6 overflow-auto xl:max-h-screen">
+        <NavBar />
 
-      <div className="flex flex-col grow h-fit shadow-xl rounded-2xl space-between bg-base-100">
-        <div className="flex flex-col px-6 pt-6">
-          <h1 className="font-bold text-xl">Edit User</h1>
-        </div>
-        <div className="flex flex-wrap gap-8 p-8">
-          <div className="flex flex-col flex-[3] flex-wrap">
-            <div className="flex flex-col justify-center items-center">
-              <div className="avatar aspect-square w-64 rounded-full bg-none items-center justify-center group">
-                <Image
-                  src={profilePic}
-                  alt="profile"
-                  width={200}
-                  height={200}
-                  className="rounded-full"
-                />
-                <input
-                  id="fileInput"
-                  type="file"
-                  onChange={changeHandler}
-                  className="hidden"
-                  accept=".png, .jpg, .jpeg"
-                />
-                <label
-                  htmlFor="fileInput"
-                  className="btn btn-ghost w-full h-full rounded-full items-center hidden justify-center group-hover:flex"
-                >
-                  <span>UPLOAD IMAGE</span>
-                </label>
+        <div className="flex flex-col grow h-fit shadow-xl rounded-2xl bg-base-100">
+          <div className="flex flex-col p-8 border-b">
+            <h1 className="font-bold text-xl">Edit User</h1>
+            {/* If you want, you can use the user's name to replace 'user' in the heading below as well */}
+            <h1>Edit user details manually below</h1>
+          </div>
+          <div className="flex flex-wrap gap-8 p-8">
+            <div className="flex flex-col flex-[3] flex-wrap">
+              <div className="flex flex-col justify-center items-center">
+                <div className="avatar aspect-square w-64 rounded-full bg-none items-center justify-center group">
+                  <Image
+                    src={profilePic}
+                    alt="profile"
+                    width={200}
+                    height={200}
+                    className="rounded-full"
+                  />
+                  <input
+                    id="fileInput"
+                    type="file"
+                    onChange={changeHandler}
+                    className="hidden"
+                    accept=".png, .jpg, .jpeg"
+                  />
+                  <label
+                    htmlFor="fileInput"
+                    className="btn btn-ghost w-full h-full rounded-full items-center hidden justify-center group-hover:flex"
+                  >
+                    <span>UPLOAD IMAGE</span>
+                  </label>
+                </div>
               </div>
             </div>
-          </div>
 
-          <div className="flex-[9] flex-wrap">
-            <div className="flex flex-col w-full min-w-96 gap-12">
-              <div className="flex flex-col gap-4">
-                <div className="flex flex-col md:flex-row gap-4">
-                  <ToggleSelect
-                    value={user ? user.company : ''}
-                    options={companies}
-                    label="Company"
-                  />
-                  <ToggleEdit value={user ? user.fullname : ''} label="Full Name" />
-                </div>
-                <div className="flex flex-col md:flex-row gap-4">
-                  <ToggleEdit value={user ? user.email : ''} label="E-mail" />
-                  <ToggleEdit value={user ? user.phone : ''} label="Mobile Number" />
-                </div>
-                <TogglePass />
+            <div className="flex-[9] flex-wrap">
+              <div className="flex flex-col w-full min-w-96 gap-12">
                 <div className="flex flex-col gap-4">
-                  <ToggleEditArea
-                    value={comment ? comment.comments : 'Write here...'}
-                    label="Comments"
-                  />
+                  <div className="flex flex-col md:flex-row gap-4">
+                    <ToggleSelect value="Softmicro" options={companies} label="Company" />
+                    <ToggleEdit value={user ? user.fullname : ''} label="Full Name" />
+                  </div>
+                  <div className="flex flex-col md:flex-row gap-4">
+                    <ToggleEdit value={user ? user.email : ''} label="E-mail" />
+                    <ToggleEdit value={user ? user.phone : ''} label="Mobile Number" />
+                  </div>
+                  <TogglePass />
+                  <div className="flex flex-col gap-4">
+                    <ToggleEditArea
+                      value={comment ? comment.comments : 'Write here...'}
+                      label="Comments"
+                    />
+                  </div>
                 </div>
               </div>
             </div>
           </div>
-        </div>
-        <div className="flex px-8 pb-8 justify-end">
-          <a href="./users" className="btn btn-primary">
-            Return To Users
-          </a>
+          <div className="flex px-8 pb-8 justify-end">
+            <a href="./users" className="btn btn-primary">
+              Return To Users
+            </a>
+          </div>
         </div>
       </div>
-    </div>
+    </QueryClientProvider>
   );
 };
 
