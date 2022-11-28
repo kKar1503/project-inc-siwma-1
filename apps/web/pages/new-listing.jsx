4961d9ac8478371b30fdd65bf06c9b75f1e3b122
notlega <onghinjourn@gmail.com>
2022-11-28 17:08:30 +0800

    feat: :sparkles: working image uploader and display

--- apps/web/pages/new-listing.jsx
+++ apps/web/pages/new-listing.jsx
@@ -13,6 +13,22 @@ const NewListing = () => {
   const [allCategories, setAllCategories] = React.useState([]);
   const [selectedImages, setSelectedImages] = React.useState([]);
 
+  const onSelectFile = (event) => {
+    const selectedImagesArray = Array.from(event.target.files).map((image) =>
+      URL.createObjectURL(image)
+    );
+
+    setSelectedImages((prevImages) => prevImages.concat(selectedImagesArray));
+
+    // chrome bug fix
+    // eslint-disable-next-line no-param-reassign
+    event.target.value = '';
+  };
+
+  const imageDeleteHandler = (image) => {
+    setSelectedImages((prevImages) => prevImages.filter((img) => img !== image));
+  };
+
   const { data, isLoading, isError, status } = useQuery(
     'get_all_categories',
     async () => client.rpc('get_all_categories'),
@@ -35,10 +51,30 @@ const NewListing = () => {
       <div className="flex justify-around mt-8 mx-32">
         <div className="flex space-y-6 flex-col w-2/6">
           <CardBackground>
+            {/* max 10 images alert */}
+            <div className="alert alert-info shadow-lg">
+              <div>
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  fill="none"
+                  viewBox="0 0 24 24"
+                  className="stroke-current flex-shrink-0 w-6 h-6"
+                >
+                  <path
+                    strokeLinecap="round"
+                    strokeLinejoin="round"
+                    strokeWidth="2"
+                    d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+                  />
+                </svg>
+                <span>Max 10 images</span>
+              </div>
+            </div>
+
             <label className="flex justify-center w-full h-40 px-4 transition bg-white border-2 border-accent border-dashed rounded-md appearance-none cursor-pointer hover:border-accent-focus focus:outline-none">
               <span className="flex items-center space-x-4">
                 <div className="btn btn-ghost normal-case text-xl rounded-2xl bg-accent hover:bg-accent-focus">
-                  Upload Image
+                  Upload Images
                 </div>
                 <span className="text-xs text-gray-600 text-center my-6">
                   or drag it into this box
@@ -47,22 +83,31 @@ const NewListing = () => {
               <input
                 type="file"
                 className="hidden"
-                onClick={(e) => {
-                  setSelectedImages(e.target.files[0]);
-                }}
-                accept="image/*"
+                onChange={onSelectFile}
+                multiple
+                accept="image/png, image/jpeg, image/webp"
               />
             </label>
+
             {selectedImages && (
-              <div className="relative">
-                <Image
-                  alt="image not found"
-                  src={URL.createObjectURL(selectedImages)}
-                  fill
-                  sizes="100%"
-                  className="object-contain"
-                />
-                <button onClick={() => selectedImages(null)}>Remove</button>
+              <div className="flex flex-row justify-between w-full">
+                {selectedImages.map((image) => (
+                  <div
+                    key={image}
+                    className="flex flex-col justify-center items-center relative border-2 border-secondary w-1/3"
+                  >
+                    <Image
+                      alt={image.name}
+                      src={image}
+                      width={500}
+                      height={500}
+                      className="object-contain"
+                    />
+                    <button className="relative" onClick={() => imageDeleteHandler(image)}>
+                      Remove image
+                    </button>
+                  </div>
+                ))}
               </div>
             )}
           </CardBackground>
