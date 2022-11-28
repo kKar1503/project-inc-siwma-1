0cb5404c76c68b3b93b1cac4911eeaec16bbb828
Hein Htet Aung <hhaung5142@gmail.com>
2022-11-28 16:46:14 +0800

    feat: :sparkles: form that toggles between selling and buying listing
    toggled using the radio button component

--- apps/web/components/layouts/ListingForm.jsx
+++ apps/web/components/layouts/ListingForm.jsx
@@ -1,39 +1,17 @@
-import Dropdown from '../Dropdown';
-import Input from '../Input';
-import RadioButton from '../RadioButton';
+import PropTypes from 'prop-types';
 
-const CreateListingInformation = () => (
+import RadioButton from '../RadioButton';
+import SellingForm from './SellingForm';
+import BuyingForm from './BuyingForm';
+
+const CreateListingInformation = ({ options, onChangeValue, typeHandler }) => (
   <form className="p-5">
     {/* Selling/Buying Options */}
-    <div className="flex flex-row justify-between mb-5">
-      <button className="bg-primary hover:bg-primary-focus text-primary-content py-2 px-4 rounded w-2/5">
-        Selling
-      </button>
-      <button className="bg-primary hover:bg-primary-focus text-primary-content py-2 px-4 rounded w-2/5">
-        Buying
-      </button>
-    </div>
+    <RadioButton options={options} onChangeValue={onChangeValue} />
 
-    {/* Category Label */}
-    <Dropdown items={['Bar', 'Angle Bars', 'Big Bricks']} />
-
-    {/* Title Label */}
-    <Input text="Title" />
-
-    {/* Condition Label */}
-    <RadioButton options={['New', 'Used']} />
-
-    {/* Price Label */}
-    <Input text="Price" />
-    <div className="form-control w-1/6">
-      <label className="label cursor-pointer">
-        <span className="label-text">Negotiable</span>
-        <input type="checkbox" className="checkbox checkbox-primary" />
-      </label>
-    </div>
-
-    {/* Description Label */}
-    <Input text="Description" type="textarea" />
+    {/* Selling/Buying Form */}
+    {/* eslint-disable no-nested-ternary */}
+    {typeHandler ? typeHandler === 'Selling' ? <SellingForm /> : <BuyingForm /> : null}
 
     {/* List Now Submit Btn */}
     <div className="flex flex-row justify-end">
@@ -46,5 +24,11 @@ const CreateListingInformation = () => (
     </div>
   </form>
 );
+
+CreateListingInformation.propTypes = {
+  options: PropTypes.arrayOf(PropTypes.string).isRequired,
+  onChangeValue: PropTypes.func.isRequired,
+  typeHandler: PropTypes.string,
+};
 
 export default CreateListingInformation;
