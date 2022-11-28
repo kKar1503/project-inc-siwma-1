175de6311d1e4cb293a0fa4c42849f22c783b1fe
Kaung Khant Zaw <mmmike2002@gmail.com>
2022-11-29 06:18:52 +0800

    feat: :construction: change password feature
    work in progress, not working atm

--- apps/admin/pages/edit-user.jsx
+++ apps/admin/pages/edit-user.jsx
@@ -89,7 +89,7 @@ const EditUser = () => {
   const [email, setEmail] = useState(user ? user.email : '');
   const [company, setCompany] = useState(user ? user.companyid : 0);
   const [phone, setPhone] = useState(user ? user.phone : '');
-  const [password, setPassword] = useState('');
+  const [newPassword, setNewPassword] = useState('');
   const [comment, setComment] = useState(
     commentData && !commentData === {} ? commentData.comments : ''
   );
@@ -128,6 +128,11 @@ const EditUser = () => {
       }),
   });
 
+  const { mutate: editPassword, isError: passwordError } = useMutation({
+    mutationKey: ['updatePassword'],
+    mutationFn: async () => supabase.auth.admin.updateUserById(userid, { password: newPassword }),
+  });
+
   const onClickHandler = () => {
     editUser();
     if (comment !== '' || comment !== commentData.comments) {
@@ -136,6 +141,9 @@ const EditUser = () => {
       } else {
         editComment();
       }
+    }
+    if (newPassword !== '') {
+      editPassword();
     }
   };
 
@@ -194,7 +202,7 @@ const EditUser = () => {
                     <ToggleEdit value={email} label="E-mail" onSave={setEmail} />
                     <ToggleEdit value={phone} label="Mobile Number" onSave={setPhone} />
                   </div>
-                  <TogglePass sendEmail={sendPasswordEmail} />
+                  <TogglePass sendEmail={sendPasswordEmail} onSave={setNewPassword} />
                   <div className="flex flex-col gap-4">
                     <ToggleEditArea value={comment} label="Comments" onSave={setComment} />
                   </div>
