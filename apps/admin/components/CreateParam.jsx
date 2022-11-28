b84c239282107e83ec1ccdd5d69de97975ae7ad5
Joshua Chow <crhjoshua12@gmail.com>
2022-11-29 07:59:30 +0800

    feat: :bug: validation
    validation for creating parameters, creating categories and editing categories

--- apps/admin/components/CreateParam.jsx
+++ apps/admin/components/CreateParam.jsx
@@ -117,6 +117,8 @@ const CreateParam = () => {
             type="text"
             className="input-group input input-bordered"
             placeholder="Parameter Name"
+            pattern="^[A-Za-z]+$"
+            title="Parameter name should only include letters"
             required
           />
         </div>
@@ -129,6 +131,8 @@ const CreateParam = () => {
             type="text"
             className="input-group input input-bordered"
             placeholder="Display Name"
+            pattern="^[A-Za-z]+$"
+            title="Display name should only include letters"
             required
           />
         </div>
