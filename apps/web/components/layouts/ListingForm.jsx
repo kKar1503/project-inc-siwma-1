060292c5d129ccfc646432caeb0ea33ce0152549
notlega <onghinjourn@gmail.com>
2022-11-28 17:48:00 +0800

    feat: :sparkles: includes guides for users to follow

--- apps/web/components/layouts/ListingForm.jsx
+++ apps/web/components/layouts/ListingForm.jsx
@@ -5,24 +5,44 @@ import SellingForm from './SellingForm';
 import BuyingForm from './BuyingForm';
 
 const CreateListingInformation = ({ options, onChangeValue, typeHandler }) => (
-  <form className="p-5">
-    {/* Selling/Buying Options */}
-    <RadioButton options={options} onChangeValue={onChangeValue} />
-
-    {/* Selling/Buying Form */}
-    {/* eslint-disable no-nested-ternary */}
-    {typeHandler ? typeHandler === 'Selling' ? <SellingForm /> : <BuyingForm /> : null}
-
-    {/* List Now Submit Btn */}
-    <div className="flex flex-row justify-end">
-      <button
-        type="submit"
-        className="bg-primary hover:bg-primary-focus text-primary-content py-2 px-4 rounded mt-5"
-      >
-        List Now
-      </button>
+  <>
+    <div className="alert alert-warning shadow-lg">
+      <div>
+        <svg
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          className="stroke-current flex-shrink-0 w-6 h-6"
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            strokeWidth="2"
+            d="M13 16h-1v-4h-1m1-4h.01M21 12a9 9 0 11-18 0 9 9 0 0118 0z"
+          />
+        </svg>
+        <span>All fields need to be filled in!</span>
+      </div>
     </div>
-  </form>
+    <form className="p-5">
+      {/* Selling/Buying Options */}
+      <RadioButton options={options} onChangeValue={onChangeValue} />
+
+      {/* Selling/Buying Form */}
+      {typeHandler && typeHandler === 'Selling' && <SellingForm />}
+      {typeHandler && typeHandler === 'Buying' && <BuyingForm />}
+
+      {/* List Now Submit Btn */}
+      <div className="flex flex-row justify-end">
+        <button
+          type="submit"
+          className="bg-primary hover:bg-primary-focus text-primary-content py-2 px-4 rounded mt-5"
+        >
+          List Now
+        </button>
+      </div>
+    </form>
+  </>
 );
 
 CreateListingInformation.propTypes = {
