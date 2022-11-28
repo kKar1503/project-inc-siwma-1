d06d916e20069a0ff742e7c0982b21326e18b2c7
RuinedRuins <matthew.ngjb@gmail.com>
2022-11-29 01:20:19 +0800

    fix: :zap: added bought badge, changed archive sold and bought color

--- apps/web/components/marketplace/listing/ArchiveBadge.jsx
+++ apps/web/components/marketplace/listing/ArchiveBadge.jsx
@@ -1,5 +1,7 @@
 const ArchiveBadge = () => (
-  <div className="badge badge-md badge-warning text-white rounded-lg shadow-md ml-1">Archived</div>
+  <div className="badge badge-md bg-[#FAA31B] border-none text-white rounded-lg shadow-md ml-1">
+    Archived
+  </div>
 );
 
 export default ArchiveBadge;
