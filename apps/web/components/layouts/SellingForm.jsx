e5722a77d7725be3beca7c65a9b73e266679a333
Hein Htet Aung <hhaung5142@gmail.com>
2022-11-29 14:12:59 +0800

    feat: :zap: created listing inserted to database

--- apps/web/components/layouts/SellingForm.jsx
+++ apps/web/components/layouts/SellingForm.jsx
@@ -1,26 +1,60 @@
 import React from 'react';
+import PropTypes from 'prop-types';
 import Input from '../Input';
 
-const SellingForm = () => (
+const SellingForm = ({
+  name,
+  setName,
+  price,
+  setPrice,
+  description,
+  setDescription,
+  negotiable,
+  setNegotiable,
+}) => (
   <>
     {/* Title Label */}
-    <Input text="Selling Title" />
+    <Input text="Selling Title" value={name} onChange={(e) => setName(e.target.value)} />
 
     {/* Price Label */}
-    <Input text="Price (SGD)" />
+    <Input text="Price (SGD)" value={price} onChange={(e) => setPrice(e.target.value)} />
     <div className="form-control w-1/6 flex flex-row justify-start">
       <label className="label cursor-pointer space-x-4">
         <span className="label-text">Negotiable</span>
-        <input type="checkbox" className="checkbox checkbox-primary" />
+        <input
+          type="checkbox"
+          className="checkbox checkbox-primary"
+          checked={negotiable}
+          onChange={(e) => setNegotiable(e.target.checked)}
+        />
       </label>
     </div>
 
     {/* Quantity Label */}
-    <Input text="Quantity" />
+    {/* <Input text="Quantity" /> */}
 
     {/* Description Label */}
-    <Input text="Description" type="textarea" />
+    <Input
+      text="Description"
+      type="textarea"
+      value={description}
+      onChange={(e) => setDescription(e.target.value)}
+    />
   </>
 );
+
+SellingForm.propTypes = {
+  name: PropTypes.string.isRequired,
+  setName: PropTypes.func.isRequired,
+
+  price: PropTypes.string.isRequired,
+  setPrice: PropTypes.func.isRequired,
+
+  description: PropTypes.string.isRequired,
+  setDescription: PropTypes.func.isRequired,
+
+  negotiable: PropTypes.bool.isRequired,
+  setNegotiable: PropTypes.func.isRequired,
+};
 
 export default SellingForm;
