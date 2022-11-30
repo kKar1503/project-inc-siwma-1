fb31875e19b12d42a68e43e11993cd1b4f6961ae
Nabil Ridhwan <nabridhwan@gmail.com>
2022-11-30 15:42:07 +0800

    feat: :sparkles: updated for fallback image onError and changed size

--- apps/web/components/marketplace/CategoryListingItem.jsx
+++ apps/web/components/marketplace/CategoryListingItem.jsx
@@ -2,28 +2,43 @@
 import Image from 'next/image';
 import Link from 'next/link';
 import PropTypes from 'prop-types';
+import { useState } from 'react';
 
 /**
  * CategoryListingItem is a component that displays a category listing item.
  * @type {React.FC<import('prop-types').InferProps<typeof propTypes>>}
  */
 
-const CategoryListingItem = ({ img, name, href }) => (
-  <Link href={href}>
-    <div className="card relative rounded-md">
-      <div className="aspect-square object-cover h-[150px]">
-        {/* Actual image */}
-        {img && <Image src={img} fill alt={name} className="object-cover" />}
+const CategoryListingItem = ({ img, name, href }) => {
+  const [imgSrc, setImgSrc] = useState(img);
 
-        {/* Overlay gradient */}
-        <div className="bg-gradient-to-b from-transparent to-black/40 w-full h-full z-10 absolute top-0 left-0" />
+  return (
+    <Link href={href}>
+      <div className="card relative rounded-md">
+        <div className="aspect-square object-cover h-[160px]">
+          {/* Actual image */}
+          {img ? (
+            <Image
+              src={imgSrc}
+              fill
+              alt={name}
+              className="object-cover"
+              onError={() => setImgSrc('https://via.placeholder.com/150')}
+            />
+          ) : (
+            <Image src="https://via.placeholder.com/150" fill alt={name} className="object-cover" />
+          )}
+
+          {/* Overlay gradient */}
+          <div className="bg-gradient-to-b from-transparent to-black/40 w-full h-full z-10 absolute top-0 left-0" />
+        </div>
+
+        {/* Category text */}
+        <p className="absolute z-10 bottom-0 text-center w-full mb-3 text-white">{name}</p>
       </div>
-
-      {/* Category text */}
-      <p className="absolute z-10 bottom-0 text-center w-full mb-3 text-white">{name}</p>
-    </div>
-  </Link>
-);
+    </Link>
+  );
+};
 
 CategoryListingItem.propTypes = {
   img: PropTypes.string,
