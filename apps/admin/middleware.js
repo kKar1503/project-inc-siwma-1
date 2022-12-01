1e2bb21781e66bc2a533bef2a5488de11a3287ca
Xavier Teo <xavierteo26@gmail.com>
2022-12-01 20:36:33 +0800

    feat: :sparkles: create middleware to protect admin routes

---
+++ apps/admin/middleware.js
@@ -0,0 +1,58 @@
+/* eslint-disable consistent-return */
+import { NextResponse } from 'next/server';
+import { createMiddlewareSupabaseClient } from '@supabase/auth-helpers-nextjs';
+
+// This function can be marked `async` if using `await` inside
+export async function middleware(req) {
+  // Code based off of https://supabase.com/docs/guides/auth/auth-helpers/nextjs#auth-with-nextjs-middleware
+  // We need to create a response and hand it to the supabase client to be able to modify the response headers.
+  const res = NextResponse.next();
+
+  // Retrieve the pathname from the request
+  const { pathname } = req.nextUrl;
+
+  if (
+    pathname === '/login' || // Exclude the login route
+    pathname === '/register' || // Exclude register route
+    pathname.startsWith('/_next') || // exclude Next.js internals
+    pathname.startsWith('/api') || //  exclude all API routes
+    pathname.startsWith('/static') || // exclude static files
+    pathname.includes('.') // exclude all files in the public folder
+  ) {
+    // Forward request to the page
+    return res;
+  }
+
+  // Create authenticated Supabase Client.
+  const supabase = createMiddlewareSupabaseClient({ req, res });
+
+  // Check if we have a session
+  const {
+    data: { session },
+  } = await supabase.auth.getSession();
+
+  // Check if user is authenticated
+  if (session) {
+    // User is authenticated, check if the user is an admin
+    const { data, error } = await supabase.rpc('is_sysadmin', {
+      user_id: session.user.id,
+    });
+
+    if (error) throw error;
+
+    console.log({ data });
+
+    // Authentication successful, forward request to protected route.
+    return res;
+  }
+
+  // Auth condition not met, redirect to home page.
+  const redirectUrl = req.nextUrl.clone();
+  redirectUrl.pathname = '/login';
+  return NextResponse.redirect(redirectUrl);
+}
+
+// See "Matching Paths" below to learn more
+export const config = {
+  matcher: '/((?!_next|api/auth).*)(.+)',
+};
