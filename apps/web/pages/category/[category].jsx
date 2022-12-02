75856808d1450f5a241dbcc8f799a0c1e7d24d1a
Matthew Ng <matthew.ngjb@gmail.com>
2022-12-03 02:27:59 +0800

    fix: :fire: removed unnecessary code

--- apps/web/pages/category/[category].jsx
+++ apps/web/pages/category/[category].jsx
@@ -1,6 +1,4 @@
 import { useRouter } from 'next/router';
-import { useQuery } from 'react-query';
-import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import CategoryBanner from '../../components/category/CategoryBanner';
 import CategoryListings from '../../components/category/CategoryListings';
 
@@ -10,15 +8,6 @@ import CategoryListings from '../../components/category/CategoryListings';
 const Category = () => {
   const router = useRouter();
   const { category } = router.query;
-  const supabase = useSupabaseClient();
-
-  const {
-    data: categoryData,
-    // status: categoryStatus,
-    // isLoading: categoryIsLoading,
-  } = useQuery(['fetchListings', router.query.id], async () =>
-    supabase.rpc('get_category_listing', { catid: router.query.id })
-  );
 
   return (
     <div className="m-auto my-10 flex flex-col justify-center w-10/12 h-screen">
@@ -27,8 +16,8 @@ const Category = () => {
       </div>
       <div className="h-2/3">
         <p className="font-bold text-2xl">{category} listings</p>
-        {/* dynamically generate each category when queried */}
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 sm:grid-cols-2 mb-10">
+          {/* dynamically generate each category when queried */}
           <CategoryListings CatId={router.query.id} />
         </div>
         <p className="font-bold text-2xl mb-5">Popular Brands</p>
