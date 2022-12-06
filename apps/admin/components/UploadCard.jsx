c1ce2728de89d7e92c0380898b9bf6f972581846
huishilhs <huishilhs@gmail.com>
2022-12-06 19:48:26 +0800

    fix: :bug: fix re-upload & added company link
    added re-upload message, state for upload & added company link

--- apps/admin/components/UploadCard.jsx
+++ apps/admin/components/UploadCard.jsx
@@ -1,17 +1,25 @@
+import { useQuery, useQueries, useQueryClient } from 'react-query';
 import PropType from 'prop-types';
 import { FiUpload } from 'react-icons/fi';
 import { useState } from 'react';
+import { Alert } from '@inc/ui';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
+import crypto from 'crypto';
 
-const UploadCard = ({ id, des }) => {
+const UploadCard = ({ id, des, link, state }) => {
   const [image, setImage] = useState(null);
   const [imageURL, setImageURL] = useState('');
+  const [error, setError] = useState(null);
   const [colourMessage, setColourMessage] = useState('text-center text-green-500 pt-4');
   const [errorMessage, setErrorMessage] = useState('');
+  const [displayAlert, setDisplayAlert] = useState(null);
   const supabase = useSupabaseClient();
+  const queryClient = useQueryClient();
 
   const checkFile = async (e) => {
-    if (e.target.files[0].type === 'image/png' || e.target.files[0].type === 'image/jpeg') {
+    if (e.target.files[0] === undefined) {
+      e.target.files = null;
+    } else if (e.target.files[0].type === 'image/png' || e.target.files[0].type === 'image/jpeg') {
       setImage(e.target.files[0]);
       setErrorMessage('Please click "Create file"');
       setColourMessage('text-center text-green-500 pt-4');
@@ -22,72 +30,114 @@ const UploadCard = ({ id, des }) => {
     }
   };
 
-  const handleUpload = async (e) => {
+  const changeUUID = (uuid) => {
+    const parts = [];
+    parts.push(uuid.slice(0, 8));
+    parts.push(uuid.slice(8, 12));
+    parts.push(uuid.slice(12, 16));
+    parts.push(uuid.slice(16, 20));
+    parts.push(uuid.slice(20, 32));
+    return parts.join('-');
+  };
+
+  const addAdvertisement = async (e) => {
     e.preventDefault();
 
-    if (image) {
-      const { data, error } = await supabase.storage
-        .from('advertisement')
-        .upload(`${image.name}`, image);
+    const { data, status } = await supabase
+      .from('advertisements')
+      .insert({
+        company_id: id,
+        description: des,
+        link,
+      })
+      .select('id');
 
-      if (error) {
-        console.log(error);
-      }
+    if (status === 409) {
+      setDisplayAlert(true);
+      setTimeout(() => {
+        setDisplayAlert(false);
+      }, 4000);
+    } else {
+      const randomUUID = crypto.randomBytes(32).toString('hex');
+      const newUUID = changeUUID(randomUUID);
 
-      if (data) {
-        setImageURL(data);
-      }
+      await supabase.storage.from('advertisement').upload(newUUID, image);
+      const { error: message } = await supabase
+        .from('advertisements')
+        .update({ image: newUUID })
+        .eq('id', data[0].id);
+      setDisplayAlert(true);
+      setError('');
+      setImage(null);
+      setTimeout(() => {
+        setDisplayAlert(false);
+      }, 4000);
     }
 
-    const { data, error } = await supabase.from('advertisements').upsert({
-      company_id: id,
-      image: image.name,
-      description: des,
-    });
-
-    if (error) {
-      console.log(error);
-    }
-
-    if (data) {
-      console.log(data);
-    }
+    queryClient.invalidateQueries({ queryKey: ['ads'] });
   };
 
   return (
-    <div className="max-w-xl">
-      <label className="flex justify-center w-full h-40 px-4 mt-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
-        <span className="items-center space-x-2">
-          <FiUpload className="h-16 w-16 text-black m-auto my-4" />
-          <p className="text-xs text-gray-600 text-center my-6">
-            Click to upload or drag and drop PNG or JPG (MAX. 1200px x 900px)
-          </p>
-          {errorMessage !== '' && <p className={colourMessage}>{errorMessage}</p>}
-        </span>
-        <input
-          type="file"
-          name="file_upload"
-          accept="image/*"
-          className="hidden"
-          disabled={id === '' || des === ''}
-          onChange={(e) => checkFile(e)}
-        />
-      </label>
-      <div className="flex items-center justify-center">
-        <button
-          className="btn btn-ghost rounded-md w-full h-6 my-8 normal-case text-base btn btn-outline btn-primary"
-          disabled={errorMessage === '' || errorMessage === 'Only image file is allowed'}
-          onClick={handleUpload}
-        >
-          Create
-        </button>
+    <div>
+      <div className="card max-sm:w-full h-80 bg-base-100 shadow-xl px-8 py-8 ">
+        <div className="max-w-xl">
+          <form
+            onSubmit={async (e) => {
+              await addAdvertisement(e);
+            }}
+          >
+            <label className="flex justify-center w-full h-40 px-4 mt-4 transition bg-white border-2 border-gray-300 border-dashed rounded-md appearance-none cursor-pointer hover:border-gray-400 focus:outline-none">
+              <span className="items-center space-x-2">
+                {image !== null ? (
+                  <div>
+                    <p className="text-xl text-gray-600 text-center my-6 px-20">Image Name:</p>
+                    <p className="text-xl text-cyan-600 text-center my-6">{image.name}</p>
+                  </div>
+                ) : (
+                  <div>
+                    <FiUpload className="h-16 w-16 text-black m-auto my-4" />
+                    <p className="text-xs text-gray-600 text-center my-6">
+                      Click to upload or drag and drop PNG or JPG (MAX. 1200px x 900px)
+                    </p>
+                  </div>
+                )}
+                {errorMessage !== '' && <p className={colourMessage}>{errorMessage}</p>}
+              </span>
+              <input
+                type="file"
+                name="file_upload"
+                accept="image/*"
+                className="hidden"
+                disabled={!state}
+                onChange={(e) => checkFile(e)}
+              />
+            </label>
+            <div className="flex items-center justify-center">
+              <button
+                className="btn btn-ghost rounded-md w-full h-6 my-8 normal-case text-base btn btn-outline btn-primary"
+                disabled={errorMessage === '' || errorMessage === 'Only image file is allowed'}
+                type="submit"
+              >
+                Create
+              </button>
+            </div>
+          </form>
+        </div>
       </div>
+      {displayAlert && error && (
+        <Alert level="error" message="Duplicate Advertisement found" className="mt-4" />
+      )}
+      {displayAlert && error === '' && (
+        <Alert level="success" message="Advertisement successfully added" className="mt-4" />
+      )}
     </div>
   );
 };
 
 UploadCard.propTypes = {
+  state: PropType.bool.isRequired,
   id: PropType.number.isRequired,
   des: PropType.string.isRequired,
+  link: PropType.string.isRequired,
 };
 export default UploadCard;
--- apps/admin/pages/AdminUpload.jsx
+++ apps/admin/pages/AdminUpload.jsx
@@ -8,8 +8,9 @@ import AdminPageLayout from '../components/layouts/AdminPageLayout';
 
 const AdminUpload = () => {
   const [userInput, setUserInput] = useState('');
+  const [handleSubmit, sethandleSubmit] = useState(false);
   const [selected, setSelected] = useState(0);
-  const [companyData, setCompanyData] = useState(null);
+  const [link, setLink] = useState('');
   const [description, setDescription] = useState('');
   const supabase = useSupabaseClient();
 
@@ -73,25 +74,48 @@ const AdminUpload = () => {
         </div>
         <div className="card w-1/3  max-sm:w-full h-80 bg-base-100 shadow-xl mr-10 max-sm:mb-10">
           <div className="max-w-xl">
-            <h1 className="ml-4 leading-loose font-bold text-xl mt-6">
-              Description of Advertisement
-            </h1>
-            <div className="flex justify-center">
-              <textarea
-                type="text"
-                name="description"
-                placeholder="Description"
-                value={description}
-                disabled={selected === 0}
-                className="w-11/12 h-40 px-4 mt-4 text-base placeholder-gray-500 break-normal border rounded-lg focus:shadow-outline"
-                onChange={(e) => setDescription(e.target.value)}
-              />
+            <div className="flex justify-center m-4">
+              <form
+                onSubmit={(event) => {
+                  event.preventDefault();
+                  sethandleSubmit(true);
+                }}
+              >
+                <label htmlFor="Description" className="text-lg">
+                  Description
+                </label>
+                <textarea
+                  type="text"
+                  name="description"
+                  // placeholder="Description"
+                  value={description}
+                  disabled={selected === 0}
+                  className="w-11/12 h-20 mx-4 mb-10 text-base placeholder-gray-500 break-normal border rounded-lg focus:shadow-outline"
+                  onChange={(e) => setDescription(e.target.value)}
+                />
+                <label htmlFor="Link" className="text-lg">
+                  Company&#39;s Link
+                </label>
+                <input
+                  type="url"
+                  name="link"
+                  id="link"
+                  // placeholder="Description"
+                  value={link}
+                  disabled={selected === 0}
+                  className="w-11/12 h-10 mx-4 text-base placeholder-gray-500 break-normal border rounded-lg focus:shadow-outline"
+                  onChange={(e) => setLink(e.target.value)}
+                />
+                <div className="mx-36 mt-3">
+                  <button className="btn btn-outline btn-primary" type="submit">
+                    save
+                  </button>
+                </div>
+              </form>
             </div>
           </div>
         </div>
-        <div className="card w-1/3  max-sm:w-full h-80 bg-base-100 shadow-xl px-8 py-8 ">
-          <UploadCard id={selected} des={description} />
-        </div>
+        <UploadCard id={selected} des={description} link={link} state={handleSubmit} />
       </div>
     </div>
   );
