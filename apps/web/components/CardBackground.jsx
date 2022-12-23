03fc10226e229c9b9b8c56b9528579b9ab9a8509
notlega <onghinjourn@gmail.com>
2022-12-23 18:11:23 +0800

    docs: :memo: introduces new documentation for the card background component

--- apps/web/components/CardBackground.jsx
+++ apps/web/components/CardBackground.jsx
@@ -1,17 +1,46 @@
-import React from 'react';
 import PropTypes from 'prop-types';
 
-const CardBackground = ({ children }) => (
+/**
+ * A component that gives a grey background to the children elements
+ * @param {JSX.Element} children The children elements to be rendered inside the CardBackground
+ * @param {string} className The className to be applied to the body of the CardBackground
+ * @returns {JSX.Element}
+ */
+const CardBackground = ({ children, className }) => (
   <div className="card w-auto bg-neutral-content shadow-xl">
-    <div className="card-body">{children}</div>
+    <div className={`card-body space-y-2 ${className}`}>{children}</div>
   </div>
 );
 
 CardBackground.propTypes = {
-  children: PropTypes.oneOfType([
-    PropTypes.arrayOf(PropTypes.element),
-    PropTypes.element.isRequired,
-  ]),
+  /**
+   * The children elements to be rendered inside the CardBackground
+   * @default null
+   * @type {JSX.Element}
+   * @example
+   * <CardBackground>
+   * <h1>CardBackground</h1>
+   * </CardBackground>
+   * // The above example will render a CardBackground with a h1 tag
+   * // inside it
+   * // The children can be used to add any custom elements to the CardBackground
+   * // body
+   */
+  children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node.isRequired]),
+  /**
+   * The className to be applied to the body of the CardBackground
+   * @default ''
+   * @type {string}
+   * @example
+   * <CardBackground className="p-4">
+   *  <h1>CardBackground</h1>
+   * </CardBackground>
+   * // The above example will render a CardBackground with a padding of 4
+   * // on all sides
+   * // The className can be used to add any custom styling to the CardBackground
+   * // body
+   */
+  className: PropTypes.string,
 };
 
 export default CardBackground;
