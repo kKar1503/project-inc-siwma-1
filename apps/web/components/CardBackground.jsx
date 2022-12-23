143e2f01d4435c90f8d9dcf7e687b6b3ac09a0e7
notlega <onghinjourn@gmail.com>
2022-12-24 02:56:28 +0800

    refactor: :art: improves jsdoc to have proper types

--- apps/web/components/CardBackground.jsx
+++ apps/web/components/CardBackground.jsx
@@ -2,9 +2,8 @@ import PropTypes from 'prop-types';
 
 /**
  * A component that gives a grey background to the children elements
- * @param {JSX.Element} children The children elements to be rendered inside the CardBackground
- * @param {string} className The className to be applied to the body of the CardBackground
- * @returns {JSX.Element}
+ *
+ * @type {React.FC<import('prop-types').InferProps<typeof propTypes>>}
  */
 const CardBackground = ({ children, className }) => (
   <div className="card w-auto bg-neutral-content shadow-xl">
@@ -12,14 +11,13 @@ const CardBackground = ({ children, className }) => (
   </div>
 );
 
-CardBackground.propTypes = {
+const propTypes = {
   /**
    * The children elements to be rendered inside the CardBackground
    * @default null
-   * @type {JSX.Element}
    * @example
    * <CardBackground>
-   * <h1>CardBackground</h1>
+   *   <h1>CardBackground</h1>
    * </CardBackground>
    * // The above example will render a CardBackground with a h1 tag
    * // inside it
@@ -27,13 +25,13 @@ CardBackground.propTypes = {
    * // body
    */
   children: PropTypes.oneOfType([PropTypes.arrayOf(PropTypes.node), PropTypes.node.isRequired]),
+
   /**
    * The className to be applied to the body of the CardBackground
    * @default ''
-   * @type {string}
    * @example
    * <CardBackground className="p-4">
-   *  <h1>CardBackground</h1>
+   *   <h1>CardBackground</h1>
    * </CardBackground>
    * // The above example will render a CardBackground with a padding of 4
    * // on all sides
@@ -42,5 +40,7 @@ CardBackground.propTypes = {
    */
   className: PropTypes.string,
 };
+
+CardBackground.propTypes = propTypes;
 
 export default CardBackground;
