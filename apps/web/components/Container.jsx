312492fbd2ad053b8e3af08519f3dc4d70c8f7b7
Nabil Ridhwan <nabridhwan@gmail.com>
2023-01-04 11:12:31 +0800

    feat: :sparkles: add upper limit to Container width for large devices (max width: 1024px)

--- apps/web/components/Container.jsx
+++ apps/web/components/Container.jsx
@@ -1,7 +1,9 @@
 // Import prop types
 import PropTypes from 'prop-types';
 
-const Container = ({ children }) => <div className="mx-2 md:mx-5">{children}</div>;
+const Container = ({ children }) => (
+  <div className="mx-2 md:mx-5 max-w-[1024px] lg:mx-auto">{children}</div>
+);
 
 Container.propTypes = {
   children: PropTypes.node,
