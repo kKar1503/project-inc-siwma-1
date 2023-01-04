fe50e87b3b8d174541d7d1107ffc162bf7f5125b
Nabil Ridhwan <nabridhwan@gmail.com>
2023-01-04 15:24:21 +0800

    feat: :lipstick: changed container padding for desktop (for Carousel buttons)

--- apps/web/components/Container.jsx
+++ apps/web/components/Container.jsx
@@ -2,7 +2,7 @@
 import PropTypes from 'prop-types';
 
 const Container = ({ children }) => (
-  <div className="px-2 lg:mx-auto lg:max-w-[1440px]">{children}</div>
+  <div className="px-2 md:px-6 lg:mx-auto lg:max-w-[1440px]">{children}</div>
 );
 
 Container.propTypes = {
