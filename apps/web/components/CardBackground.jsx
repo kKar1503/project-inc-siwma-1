8d723ca57e57f64d445dc30de8b2f8f6760ae53e
Nabil Ridhwan <nabridhwan@gmail.com>
2023-01-06 16:42:14 +0800

    feat: :sparkles: remove width and change shadow for CardBackground

--- apps/web/components/CardBackground.jsx
+++ apps/web/components/CardBackground.jsx
@@ -1,13 +1,14 @@
-import React from 'react';
 import PropTypes from 'prop-types';
+import React from 'react';
 
-const CardBackground = ({ children }) => (
-  <div className="card w-auto bg-neutral-content shadow-xl">
+const CardBackground = ({ className, children }) => (
+  <div className={`card bg-neutral-content shadow-xl ${className}`}>
     <div className="card-body">{children}</div>
   </div>
 );
 
 CardBackground.propTypes = {
+  className: PropTypes.string,
   children: PropTypes.oneOfType([
     PropTypes.arrayOf(PropTypes.element),
     PropTypes.element.isRequired,
