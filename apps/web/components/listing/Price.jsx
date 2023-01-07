1a58b250072d091a59f0924a79558c2f8925cf78
Nabil Ridhwan <nabridhwan@gmail.com>
2023-01-07 12:20:50 +0800

    feat: :sparkles: add unit price in Price component

--- apps/web/components/listing/Price.jsx
+++ apps/web/components/listing/Price.jsx
@@ -1,8 +1,14 @@
 import PropTypes from 'prop-types';
 
-const Price = ({ price }) => <h1 className="text-3xl font-bold">S${price}</h1>;
+const Price = ({ price, unitPrice }) => (
+  <h1 className="text-3xl font-bold">
+    S${price}
+    {unitPrice && <span className="text-2xl font-normal">/unit</span>}
+  </h1>
+);
 
 Price.propTypes = {
+  unitPrice: PropTypes.bool.isRequired,
   price: PropTypes.number.isRequired,
 };
 
