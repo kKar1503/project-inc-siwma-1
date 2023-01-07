102e06e5f52f25e3bfac340321a8d2a600f4f619
Joshua Chow <crhjoshua12@gmail.com>
2023-01-08 04:15:11 +0800

    build: forgot to add
    forgot to add earlier

---
+++ apps/admin/components/Tables/BaseTableParam/index.js
@@ -0,0 +1,3 @@
+export { default as BaseTableParam } from './BaseTableParam';
+export { default as BaseTableRow } from './BaseTableRow';
+export { default as BaseTableHeader } from './BaseTableHeader';
