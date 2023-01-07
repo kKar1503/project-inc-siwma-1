9ed74c89c136255732989dd8f3c2eb2193443ed8
Joshua Chow <crhjoshua12@gmail.com>
2023-01-08 04:12:30 +0800

    fix: wrong table name
    change table name from registered users to available parameters

--- apps/admin/components/Tables/AvailableParametersTable.jsx
+++ apps/admin/components/Tables/AvailableParametersTable.jsx
@@ -207,7 +207,7 @@ const ExistingCategories = ({ className, id }) => {
       header={
         <div className="flex flex-row justify-between items-center">
           <div className="flex flex-col pb-3">
-            <h1 className="font-bold text-xl">Registered Users</h1>
+            <h1 className="font-bold text-xl">Available Parameters</h1>
             <h1 className="pr-2">
               Showing {selectedIndex * option + 1} to{' '}
               {(selectedIndex + 1) * option > availParamCount
