db679fb6edafc279a2e92e90916247bca9cb3f3d
Acrylic125 <acrylic125email@gmail.com>
2023-01-08 16:22:43 +0800

    feat: using auth guards

--- apps/admin/pages/_app.jsx
+++ apps/admin/pages/_app.jsx
@@ -1,10 +1,15 @@
 import PropTypes from 'prop-types';
 import '@inc/styles/globals.css';
-import { SessionContextProvider, } from '@supabase/auth-helpers-react';
-// import { SessionContextProvider, useSupabaseClient } from '@supabase/auth-helpers-react';
-import { useState } from 'react';
+import { SessionContextProvider } from '@supabase/auth-helpers-react';
+import { useEffect, useState } from 'react';
 import { createBrowserSupabaseClient } from '@supabase/auth-helpers-nextjs';
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { useRouter } from 'next/router';
+import Error404 from '../components/fallbacks/Error404';
+import AuthenticationGuard from '../components/auth/AuthenticationGuard';
+import AuthorizationGuard from '../components/auth/AuthorizationGuard';
+import AppUserContext from '../contexts/AppUserContext';
+import AppUserProvider from '../components/auth/UserProvider';
 
 const queryClient = new QueryClient();
 
@@ -15,6 +20,18 @@ const propTypes = {
   pageProps: PropTypes.any,
   Component: PropTypes.elementType,
 };
+
+const DisallowNonAuthenticatedFallback = () => {
+  const router = useRouter();
+  useEffect(() => {
+    router.push(`/login?redirect=${router.asPath}`);
+  }, [router]);
+  return <div>Not allowed</div>;
+};
+
+const DisallowAuthenticatedFallback = () => <div>Fallback</div>;
+
+const ForbiddenFallback = () => <div>Forbidden</div>;
 
 /**
  * TODO: NextJS 13 Future migrations with layouts.
@@ -30,11 +47,23 @@ const MyApp = ({ Component, pageProps }) => {
   const [supabase] = useState(() => createBrowserSupabaseClient());
   // Use the layout defined at the page level, if available
   const getLayout = Component.getLayout || ((page) => page);
+  const { roles, allowAuthenticated, allowNonAuthenticated } = Component;
 
   return (
     <SessionContextProvider supabaseClient={supabase} initialSession={pageProps.initialSession}>
       <QueryClientProvider client={queryClient}>
-        {getLayout(<Component {...pageProps} />)}
+        <AppUserProvider>
+          <AuthenticationGuard
+            disallowAuthenticatedFallback={<Error404 />}
+            disallowNonAuthenticatedFallback={<DisallowNonAuthenticatedFallback />}
+            allowAuthenticated={allowAuthenticated}
+            allowNonAuthenticated={allowNonAuthenticated}
+          >
+            <AuthorizationGuard roles={roles} fallback={<Error404 />}>
+              {getLayout(<Component {...pageProps} />)}
+            </AuthorizationGuard>
+          </AuthenticationGuard>
+        </AppUserProvider>
       </QueryClientProvider>
     </SessionContextProvider>
   );
