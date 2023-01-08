981edf6ab3ea5d0b448131310b05644dc3459a3c
Acrylic125 <acrylic125email@gmail.com>
2023-01-08 16:22:23 +0800

    feat: App user context

---
+++ apps/admin/contexts/AppUserContext.jsx
@@ -0,0 +1,31 @@
+import React, { createContext } from 'react';
+
+/**
+ * @type {Context<{
+ *  isLoading: boolean;
+ *  isAuthenticated: boolean;
+ *  supabaseUser: import('@supabase/supabase-js').User | null;
+ *  appUser: {
+ *      id: string;
+ *      email: string;
+ *      fullname: string;
+ *      created_at: string;
+ *      updated_at: string;
+ *      phone: string;
+ *      contact: number;
+ *      bio?: string;
+ *      companyId?: number;
+ *      permissions: number;
+ *      enabled: boolean;
+ *      image?: string;
+ *  } | null
+ * }>}
+ */
+const AppUserContext = createContext({
+  supabaseUser: null,
+  appUser: null,
+  isLoading: false,
+  isAuthenticated: false,
+});
+
+export default AppUserContext;
