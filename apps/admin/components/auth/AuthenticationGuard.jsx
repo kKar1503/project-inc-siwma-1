2c36bd9359a31ddcf4c7e2ddcef8649c782addc4
Acrylic125 <acrylic125email@gmail.com>
2023-01-08 16:21:49 +0800

    feat: AuthenticationGuard

--- apps/admin/components/auth/AuthenticationGuard.jsx
+++ apps/admin/components/auth/AuthenticationGuard.jsx
@@ -1,26 +1,47 @@
+import { useContext } from 'react';
 import PropTypes from 'prop-types';
-import { useUser } from '@supabase/auth-helpers-react';
+import AppUserContext from '../../contexts/AppUserContext';
 
 /**
  *
  * @type {React.FC<PropTypes.InferProps<typeof propTypes>>}
  */
-const AuthenticationGuard = ({ children, fallback, allowAuthenticated, allowNonAuthenticated }) => {
-  // This will be resolved on the client side.
-  const user = useUser();
-  const isAuthenticated = !!user;
-  if (!allowAuthenticated && isAuthenticated) {
-    return fallback ?? null;
+const AuthenticationGuard = ({
+  children,
+  disallowAuthenticatedFallback,
+  disallowNonAuthenticatedFallback,
+  loader,
+  allowAuthenticated,
+  allowNonAuthenticated,
+}) => {
+  // useUser() uses useSessionContext() internally and returns the user object.
+  // We want to ensure that the user object is derived from a single source (The context),
+  // so if the useUser() hook were to change, there won't be any unexpected issues.
+  const { isLoading, isAuthenticated } = useContext(AppUserContext);
+
+  if (allowAuthenticated && allowNonAuthenticated) {
+    return children;
   }
+  if (isLoading) {
+    return loader ?? null;
+  }
+
+  // If we don't allow authenticated users, and the user is authenticated, we return the fallback.
+  if (!allowAuthenticated && isAuthenticated) {
+    return disallowAuthenticatedFallback ?? null;
+  }
+  // If we don't allow non-authenticated users, and the user is not authenticated, we return the fallback.
   if (!allowNonAuthenticated && !isAuthenticated) {
-    return fallback ?? null;
+    return disallowNonAuthenticatedFallback ?? null;
   }
   return children;
 };
 
 const propTypes = {
   children: PropTypes.node.isRequired,
-  fallback: PropTypes.node,
+  loader: PropTypes.node,
+  disallowAuthenticatedFallback: PropTypes.node,
+  disallowNonAuthenticatedFallback: PropTypes.node,
   allowAuthenticated: PropTypes.bool,
   allowNonAuthenticated: PropTypes.bool,
 };
