8286b01d7ae3899c1ffd61800097ca8542adbe17
Matthew Ng <matthew.ngjb@gmail.com>
2023-01-10 01:35:19 +0800

    feat: :sparkles: image now shows in {category} page

--- apps/web/pages/category/[category].jsx
+++ apps/web/pages/category/[category].jsx
@@ -1,5 +1,6 @@
 import { useRouter } from 'next/router';
 import { useQuery } from 'react-query';
+import { useState } from 'react';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import CategoryCard from '../../components/category/CategoryCard';
 import ProductListingItem from '../../components/marketplace/listing/ProductListingItem';
@@ -8,7 +9,35 @@ import CategoryBanner from '../../components/category/CategoryBanner';
 // supabase.rpc('get_category_listing', { catid: router.query.id })
 // supabase.from('listing').select('*').eq('category', catId);
 
+async function getListingFirstImage(listings, supabaseClient) {
+  if (!listings) return null;
+
+  const listingDataWithImages = listings.map(async (item) => {
+    // Let imageLink be null first, we will set a value if there is an image
+    let imageLink = null;
+    if (item.image) {
+      // Get image from supabase storage
+
+      const imageResult = await supabaseClient.storage
+        .from('listing-image-bucket')
+        .getPublicUrl(item.image);
+
+      imageLink = imageResult.data.publicUrl;
+    }
+
+    return {
+      ...item,
+      imageUrl: imageLink,
+    };
+  });
+
+  const resolvedListingDataWithImages = await Promise.all(listingDataWithImages);
+
+  return resolvedListingDataWithImages;
+}
+
 const Category = () => {
+  const [listingData, setListingData] = useState([]);
   const router = useRouter();
   const { category } = router.query;
   const supabase = useSupabaseClient();
@@ -17,8 +46,16 @@ const Category = () => {
     data: categoryData,
     // status: categoryStatus,
     // isLoading: categoryIsLoading,
-  } = useQuery(['fetchListings', router.query.id], async () =>
-    supabase.rpc('get_category_listing', { catid: router.query.id })
+  } = useQuery(
+    ['fetchListings', router.query.id],
+    async () => supabase.rpc('get_category_listings', { item_offset: 0, catid: router.query.id }),
+    {
+      onSuccess: async (apiData) => {
+        const { data } = apiData;
+        const listingsWithImageUrls = await getListingFirstImage(data, supabase);
+        setListingData(listingsWithImageUrls);
+      },
+    }
   );
 
   return (
@@ -31,7 +68,7 @@ const Category = () => {
         {/* dynamically generate each category when queried */}
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 sm:grid-cols-2 mb-10">
           {/* image temporarily grabbed from metalsupermarkets.com */}
-          {categoryData?.data?.map(
+          {listingData?.map(
             ({
               name,
               imageUrl,
