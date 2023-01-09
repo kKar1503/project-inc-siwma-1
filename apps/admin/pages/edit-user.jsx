146cf02b32ac9dcf7bfa623e98611922fa237584
Kaung Khant Zaw <mmmike2002@gmail.com>
2023-01-09 23:58:17 +0800

    feat: :construction: integrating changePassword

--- apps/admin/pages/edit-user.jsx
+++ apps/admin/pages/edit-user.jsx
@@ -1,6 +1,5 @@
-import { useEffect } from 'react';
 import { useRouter } from 'next/router';
-import { useQueries, QueryClient, useQuery } from 'react-query';
+import { useQueries, useQueryClient } from 'react-query';
 import { useSupabaseClient } from '@supabase/auth-helpers-react';
 import { getAllCompanies, getUser } from '@inc/database';
 import { EditUserFormWrap } from '../components/forms/userEdit';
@@ -10,30 +9,31 @@ import NavBar from '../components/NavBar';
 const EditUser2 = () => {
   const router = useRouter();
   const { userid } = router.query;
-  const supabase = useSupabaseClient();
 
-  const queryClient = new QueryClient();
+  const supabase = useSupabaseClient();
+  const queryClient = useQueryClient();
 
   const queries = useQueries([
     {
       queryKey: ['getUser', { id: userid }],
       queryFn: async () => getUser({ supabase, userid, getAdminContent: true }),
+      refetchInterval: 6000,
+      keepPreviousData: true,
     },
     {
       queryKey: ['getCompanies'],
       queryFn: async () => getAllCompanies({ supabase }),
+      refetchInterval: 6000,
+      keepPreviousData: true,
     },
   ]);
 
   const refreshQuery = () => {
-    queryClient.invalidateQueries({ queryKey: ['getUser', { id: userid }] });
-    queryClient.invalidateQueries({ queryKey: ['getCompanies'] });
+    queryClient.invalidateQueries();
   };
 
   const isLoading = queries.some((e) => e.isLoading);
   const [getUserQuery, getCompaniesQuery] = queries;
-
-  console.log('user', isLoading ? {} : getUserQuery.data.data);
 
   return (
     <div className="flex flex-col w-full h-full gap-8 p-6 overflow-auto xl:max-h-screen">
@@ -49,7 +49,7 @@ const EditUser2 = () => {
           onSuccessChange={() => {
             refreshQuery();
           }}
-          userQuery={getUserQuery.data}
+          userQueryData={getUserQuery.data}
           companiesQueryData={getCompaniesQuery.data}
           isLoading={isLoading}
         />
