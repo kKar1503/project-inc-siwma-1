3e2dfd332c45aea5a971bc4a2385905f36a62bc1
Kai Jun <85498628+Xu3huapiaopiao@users.noreply.github.com>
2023-01-09 14:27:22 +0800

    edit

--- apps/admin/pages/ads/[id].jsx
+++ apps/admin/pages/ads/[id].jsx
@@ -42,6 +42,6 @@ const CategoryPage = () => {
 /**
  * Use the layout for Admin Pages
  */
-CategoryPage.getLayout = (page) => <AdminPageLayout pageName="Category">{page}</AdminPageLayout>;
+CategoryPage.getLayout = (page) => <AdminPageLayout pageName="Advertisement">{page}</AdminPageLayout>;
 
 export default CategoryPage;
