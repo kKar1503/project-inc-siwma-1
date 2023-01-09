7229e8934bead1a570616224b6ab00ec3306d271
Matthew Ng <matthew.ngjb@gmail.com>
2023-01-09 16:43:54 +0800

    refactor: :fire: removed hardcoded values and adjusted layout to look cleaner

--- apps/web/pages/category/[category].jsx
+++ apps/web/pages/category/[category].jsx
@@ -22,27 +22,19 @@ const Category = () => {
 
   return (
     <div className="m-auto my-10 flex flex-col justify-center w-10/12 h-screen">
-      <div className="mb-10 h-1/2">
+      <div className="h-1/4">
         <CategoryBanner img="https://images.unsplash.com/photo-1563642900777-eb44ab05eaf8" />
       </div>
-      <div className="h-2/3">
+      <div>
         <p className="font-bold text-2xl">{category} listings</p>
         {/* dynamically generate each category when queried */}
         <div className="grid grid-cols-1 md:grid-cols-4 gap-4 sm:grid-cols-2 mb-10">
           {/* image temporarily grabbed from metalsupermarkets.com */}
-          <CategoryCard
-            name="Carbon Steel Round Tube"
-            img="https://www.metalsupermarkets.com/wp-content/uploads/2022/09/mild-steel-round-tube-DOM-150x150.webp"
-          />
-          <CategoryCard
-            name="Mild Steel Angle"
-            img="https://www.metalsupermarkets.com/wp-content/uploads/2021/03/Mild-Steel-Angle-Metal-Supermarkets-150x150.png"
-          />
           {categoryData?.data?.map((item) => (
             <CategoryCard name={item.name} img={item.image} key={item.name} />
           ))}
         </div>
-        <p className="font-bold text-2xl mb-5">Popular Brands</p>
+        {/* <p className="font-bold text-2xl mb-5">Popular Brands</p> */}
         {/* inserted from marketplace */}
       </div>
     </div>
