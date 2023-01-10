19ca0e297f5632e89d0bc70f90d5c40275679f90
Kaung Khant Zaw <mmmike2002@gmail.com>
2023-01-10 16:43:29 +0800

    fix: :bug: user invite modal not opening

--- apps/admin/pages/users.jsx
+++ apps/admin/pages/users.jsx
@@ -1,4 +1,6 @@
 import { QueryClientProvider, QueryClient } from 'react-query';
+import { useState } from 'react';
+import Link from 'next/link';
 import NavBar from '../components/NavBar';
 import UserInvite from '../components/Modals/UserInvite';
 import RegisteredUsersTable from '../components/Tables/RegisteredUsersTable';
@@ -11,41 +13,59 @@ import AdminPageLayout from '../components/layouts/AdminPageLayout';
 
 const queryClient = new QueryClient();
 
-const Page = () => (
-  <QueryClientProvider client={queryClient}>
-    <div className="flex flex-col flex-1 w-full p-8 gap-8 overflow-auto">
-      <NavBar />
-      <div className="grid grid-cols-2 gap-8">
-        <div className="rounded-lg card bg-base-100 shadow-lg">
-          <div className="card-body">
-            <h1 className="font-bold text-xl">Create an individual invite</h1>
-            <h1 className="pr-2 pb-4">Invite an individual user to the system</h1>
-            <div className="card-actions justify-center">
-              <label htmlFor="user-invite" className="btn btn-primary btn-outline grow">
-                Send Invite
-              </label>
-            </div>
-          </div>
-        </div>
-        <div className="rounded-lg card bg-base-100 shadow-lg">
-          <div className="card-body">
-            <h1 className="font-bold text-xl">Bulk invite users</h1>
-            <h1 className="pr-2 pb-4">Invite multiple users at once through a file import</h1>
-            <div className="card-actions justify-center">
-              <button className="btn btn-primary btn-outline grow">Send Invites</button>
-            </div>
-          </div>
-        </div>
-      </div>
+const Page = () => {
+  const [isOpen, setIsOpen] = useState(false);
 
-      <div className="flex flex-col h-full gap-8">
-        <PendingInvitesTable />
-        <RegisteredUsersTable />
+  const openModal = () => {
+    setIsOpen(true);
+  };
+
+  const closeModal = () => {
+    setIsOpen(false);
+  };
+
+  const refreshQuery = () => {
+    queryClient.invalidateQueries({ queryKey: ['getUsers'] });
+    queryClient.invalidateQueries({ queryKey: ['getUserCount'] });
+  };
+  return (
+    <QueryClientProvider client={queryClient}>
+      <div className="flex flex-col flex-1 w-full p-8 gap-8 overflow-auto">
+        <NavBar />
+        <div className="grid grid-cols-2 gap-8">
+          <div className="rounded-lg card bg-base-100 shadow-lg">
+            <div className="card-body">
+              <h1 className="font-bold text-xl">Create an individual invite</h1>
+              <h1 className="pr-2 pb-4">Invite an individual user to the system</h1>
+              <div className="card-actions justify-center">
+                <button onClick={openModal} className="btn btn-primary btn-outline grow">
+                  Send Invite
+                </button>
+              </div>
+            </div>
+          </div>
+          <div className="rounded-lg card bg-base-100 shadow-lg">
+            <div className="card-body">
+              <h1 className="font-bold text-xl">Bulk invite users</h1>
+              <h1 className="pr-2 pb-4">Invite multiple users at once through a file import</h1>
+              <div className="card-actions justify-center">
+                <Link href="invite/" className="w-full btn btn-outline btn-primary">
+                  Bulk Invite Users
+                </Link>
+              </div>
+            </div>
+          </div>
+        </div>
+
+        <div className="flex flex-col h-full gap-8">
+          <PendingInvitesTable />
+          <RegisteredUsersTable />
+        </div>
+        <UserInvite isOpen={isOpen} onRequestClose={closeModal} onSuccess={refreshQuery} />
       </div>
-      <UserInvite />
-    </div>
-  </QueryClientProvider>
-);
+    </QueryClientProvider>
+  );
+};
 
 Page.getLayout = (page) => <AdminPageLayout pageName="Users">{page}</AdminPageLayout>;
 
