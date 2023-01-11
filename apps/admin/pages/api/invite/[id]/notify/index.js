749f05f0d082a867a308e25dde77215cba4d1ec4
Acrylic125 <acrylic125email@gmail.com>
2023-01-11 10:01:07 +0800

    feat: invite notify EP

--- apps/admin/pages/api/invite/[id]/notify/index.js
+++ apps/admin/pages/api/invite/[id]/notify/index.js
@@ -1,7 +1,7 @@
 import { Log } from '@inc/utils';
 import { createServiceSupabaseClient } from '@inc/utils/supabase';
 import { createServerSupabaseClient } from '@supabase/auth-helpers-nextjs';
-import client from '../../../../../smtp/mailtrap';
+import sibClient from '../../../../../smtp/sib';
 
 /**
  *
@@ -9,15 +9,19 @@ import client from '../../../../../smtp/mailtrap';
  * @param {import("next").NextApiResponse} res
  */
 export default async function handler(req, res) {
-  //   if (req.method !== 'POST') {
-  //     res.status(405).end();
-  //     return;
-  //   }
+  if (req.method !== 'POST') {
+    res.status(405).json({
+      error: 'Method not allowed',
+    });
+    return;
+  }
 
   const $inviteId = req.query.id;
   const inviteId = $inviteId instanceof Array ? $inviteId[0] : $inviteId;
   if (!inviteId) {
-    res.status(400).end();
+    res.status(400).json({
+      error: 'Invite ID is required',
+    });
     return;
   }
 
@@ -43,12 +47,12 @@ export default async function handler(req, res) {
     });
     return;
   }
-  //   if (appUser.permissions !== 1) {
-  //     res.status(403).json({
-  //       error: 'You do not have permission to perform this action',
-  //     });
-  //     return;
-  //   }
+  if (appUser.permissions !== 1) {
+    res.status(403).json({
+      error: 'You do not have permission to perform this action',
+    });
+    return;
+  }
 
   const serviceSupabase = createServiceSupabaseClient();
   const { data: invite, error: inviteError } = await serviceSupabase
@@ -67,47 +71,48 @@ export default async function handler(req, res) {
     return;
   }
 
-  const { success } = await client.send({
-    from: {
-      email: 'acrylic125email@gmail.com',
-      name: 'Acrylic125',
-    },
+  const apiInstance = new sibClient.TransactionalEmailsApi();
+  let sendSmtpEmail = new sibClient.SendSmtpEmail(); // SendSmtpEmail | Values to send a transactional email
+  sendSmtpEmail = {
     to: [
       {
         email: invite.email,
+        name: invite.name,
       },
     ],
-    subject: 'Hello from Mailtrap',
-    html: `
-    <!doctype html>
-    <html>
-      <head>
-        <meta http-equiv="Content-Type" content="text/html; charset=UTF-8">
-      </head>
-      <body style="font-family: sans-serif;">
-        <div style="display: block; margin: auto; max-width: 600px;" class="main">
-          <h1 style="font-size: 18px; font-weight: bold; margin-top: 20px">Congrats for sending test email with Mailtrap!</h1>
-          <p>Inspect it using the tabs you see above and learn how this email can be improved.</p>
-          <img alt="Inspect with Tabs" src="cid:welcome.png" style="width: 100%;">
-          <p>Now send your email using our fake SMTP server and integration of your choice!</p>
-          <p>Good luck! Hope it works.</p>
-        </div>
-        <!-- Example of invalid for email html/css, will be detected by Mailtrap: -->
-        <style>
-          .main { background-color: white; }
-          a:hover { border-left-width: 1em; min-height: 2em; }
-        </style>
+    sender: {
+      email: process.env.SIB_SENDER_EMAIL,
+      name: process.env.SIB_SENDER_NAME,
+    },
+    subject: 'SIWMA Invite',
+    htmlContent: `
+      <!DOCTYPE html>
+      <html>
+      <body>
+        <h1>You have received an invite!</h1>
+        <p>Click on the following link to create your account.</p>
+        <a href="{{params.redirectUrl}}">Create Account</a>
       </body>
-    </html>
-  `,
-  });
+      </html>
+      `,
+    params: {
+      redirectUrl: `http://localhost:3000/register?token=${invite.token}`,
+    },
+    headers: {
+      // Do not forget to set your custom headers here
+      'X-Mailin-custom': '',
+    },
+  };
 
-  if (!success) {
-    res.status(500).json({
-      error: 'Failed to send invite',
+  try {
+    await apiInstance.sendTransacEmail(sendSmtpEmail);
+    res.status(200).json({
+      message: 'Invite EMail sent successfully.',
     });
-    return;
+  } catch (err) {
+    Log({}, err);
+    res.status(500).json({
+      error: 'Something went wrong when sending the invite EMail.',
+    });
   }
-
-  res.status(204).end();
 }
